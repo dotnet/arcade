5b10ddf735a36df6a214d5ca4df21c3240695bb8
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-08-30 11:59:01 -0700

    Respond to PR feedback.

--- TargetFrameworkMoniker.cs
+++ TargetFrameworkMoniker.cs
@@ -10,16 +10,26 @@ namespace Xunit
     [Flags]
     public enum TargetFrameworkMoniker
     {
-        Net45 = 1,
-        Net451 = 2,
-        Net452 = 4,
-        Net46 = 8,
-        Net461 = 16,
-        Net462 = 32,
-        Net463 = 64,
-        Netcore50 = 128,
-        Netcore50aot = 256,
-        Netcoreapp1_0 = 512,
-        Netcoreapp1_1 = 1024
+        Net45 = 0x1,
+        Net451 = 0x2,
+        Net452 = 0x4,
+        Net46 = 0x8,
+        Net461 = 0x10,
+        Net462 = 0x20,
+        Net463 = 0x40,
+
+        Netcore50 = 0x80,
+        Netcore50aot = 0x100,
+
+        Netcoreapp1_0 = 0x200,
+        Netcoreapp1_1 = 0x400,
+
+        NetFramework = Net45 | Net451 | Net452 | Net46 | Net461 | Net462 | Net463,
+        NetFramework45 = Net45 | Net451 | Net452,
+        NetFramework46 = Net46 | Net461 | Net462,
+
+        Netcoreapp = Netcoreapp1_0 | Netcoreapp1_1,
+
+        NetcoreUwp = Netcore50 | Netcore50aot
     }
 }
