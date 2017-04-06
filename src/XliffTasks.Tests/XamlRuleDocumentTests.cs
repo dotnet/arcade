795e8a9c5ba47f1391c75bd0e786a6d8145a1408
Nick Guerrera <nicholg@microsoft.com>
2017-04-06 15:44:42 -0700

    Add CPS xaml rule document translation

---
+++ src/XliffTasks.Tests/XamlRuleDocumentTests.cs
@@ -0,0 +1,64 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace XliffTasks.Tests
+{
+    public class CpsRuleTranslationTests
+    {
+        [Fact]
+        public void BasicLoadAndTranslate()
+        {
+            string source =
+@"<Rule Name=""MyRule"" DisplayName=""My rule display name"" PageTemplate=""generic"" Description=""My rule description"" xmlns=""http://schemas.microsoft.com/build/2009/properties"">
+  <Rule.Categories>
+    <Category Name=""MyCategory"" DisplayName=""My category display name"" />
+  </Rule.Categories>
+  <EnumProperty Name=""MyEnumProperty"" DisplayName=""My enum property display name"" Category=""MyCategory"" Description=""Specifies the source file will be copied to the output directory."">
+    <EnumValue Name=""First"" DisplayName=""Do the first thing"" />
+    <EnumValue Name=""Second"" DisplayName=""Do the second thing"" />
+    <EnumValue Name=""Third"" DisplayName=""Do the third thing"" />
+  </EnumProperty>
+  <BoolProperty Name=""MyBoolProperty"" Description=""My bool property description."" />
+</Rule>";
+
+            var translations = new Dictionary<string, string>
+            {
+                ["Rule|MyRule|DisplayName"] = "AAA",
+                ["Rule|MyRule|Description"] = "BBB",
+                ["Category|MyCategory|DisplayName"] = "CCC",
+                ["EnumProperty|MyEnumProperty|DisplayName"] = "DDD",
+                ["EnumProperty|MyEnumProperty|Description"] = "EEE",
+                ["EnumValue|MyEnumProperty.First|DisplayName"] = "FFF",
+                ["EnumValue|MyEnumProperty.Second|DisplayName"] = "GGG",
+                ["EnumValue|MyEnumProperty.Third|DisplayName"] = "HHH",
+                ["BoolProperty|MyBoolProperty|Description"] = "III",
+                
+            };
+
+            string expectedTranslation =
+@"<Rule Name=""MyRule"" DisplayName=""AAA"" PageTemplate=""generic"" Description=""BBB"" xmlns=""http://schemas.microsoft.com/build/2009/properties"">
+  <Rule.Categories>
+    <Category Name=""MyCategory"" DisplayName=""CCC"" />
+  </Rule.Categories>
+  <EnumProperty Name=""MyEnumProperty"" DisplayName=""DDD"" Category=""MyCategory"" Description=""EEE"">
+    <EnumValue Name=""First"" DisplayName=""FFF"" />
+    <EnumValue Name=""Second"" DisplayName=""GGG"" />
+    <EnumValue Name=""Third"" DisplayName=""HHH"" />
+  </EnumProperty>
+  <BoolProperty Name=""MyBoolProperty"" Description=""III"" />
+</Rule>";
+
+            var document = new XamlRuleDocument();
+            var writer = new StringWriter();
+            document.Load(new StringReader(source));
+            document.Translate(translations);
+            document.Save(writer);
+
+            Assert.Equal(expectedTranslation, writer.ToString());
+        }
+    }
+}
---
+++ src/XliffTasks/XamlRuleDocument.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace XliffTasks
+{
+    /// <summary>
+    /// A <see cref="TranslatableDocument"/> for files in CPS rule .xaml format
+    /// See https://msdn.microsoft.com/en-us/library/ekyft91f(v=vs.100).aspx
+    /// </summary>
+    internal sealed class XamlRuleDocument : TranslatableXmlDocument
+    {
+        protected override IEnumerable<TranslatableNode> GetTranslatableNodes()
+        {
+            foreach (var element in Document.Descendants())
+            {
+                foreach (var attribute in element.Attributes())
+                {
+                    if (XmlName(attribute) != "DisplayName" && XmlName(attribute) != "Description")
+                    {
+                        continue;
+                    }
+
+                    yield return new TranslatableXmlAttribute(
+                        id: GenerateId(attribute),
+                        source: attribute.Value,
+                        note: "",
+                        attribute: attribute);
+                }
+            }
+        }
+
+        private static string GenerateId(XAttribute attribute)
+        {
+            XElement parent = attribute.Parent;
+
+            if (XmlName(parent) == "EnumValue")
+            {
+                XElement grandparent = parent.Parent;
+                return $"{XmlName(parent)}|{AttributedName(grandparent)}.{AttributedName(parent)}|{XmlName(attribute)}";
+            }
+
+            return $"{XmlName(parent)}|{AttributedName(parent)}|{XmlName(attribute)}";
+        }
+
+        private static string XmlName(XElement element) => element.Name.LocalName;
+
+        private static string XmlName(XAttribute attribute) => attribute.Name.LocalName;
+
+        private static string AttributedName(XElement element) => element.Attribute("Name").Value;
+    }
+}
