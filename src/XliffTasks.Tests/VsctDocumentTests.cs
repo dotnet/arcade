cedcd8c521f0972cf79371b6570a233b9b87e70e
Nick Guerrera <nicholg@microsoft.com>
2017-04-06 15:11:10 -0700

    Add vsct document translation

---
+++ src/XliffTasks.Tests/VsctDocumentTests.cs
@@ -0,0 +1,72 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace XliffTasks.Tests
+{
+    public class VsctTranslationTests
+    {
+        [Fact]
+        public void BasicLoadAndTranslate()
+        {
+            string source =
+@"<CommandTable xmlns=""http://schemas.microsoft.com/VisualStudio/2005-10-18/CommandTable"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <Commands package=""guidTestPackage"">
+    <Menus>
+      <Menu id=""menuidOne"">
+        <Strings>
+          <MenuText>Some menu text</MenuText>
+          <ButtonText>Some button text</ButtonText>
+        </Strings>
+      </Menu>
+      <Menu id=""menuidTwo"">
+        <Strings>
+          <MenuText>More menu text</MenuText>
+          <ButtonText>More button text</ButtonText>
+        </Strings>
+      </Menu>
+    </Menus>
+  </Commands>
+</CommandTable>";
+
+            var translations = new Dictionary<string, string>
+            {
+                ["menuidOne|MenuText"] = "Texte de menu",
+                ["menuidOne|ButtonText"] = "Texte de button",
+                ["menuidTwo|MenuText"] = "Plus de texte de menu",
+                ["menuidTwo|ButtonText"] = "Plus de texte de button",
+            };
+
+            string expectedTranslation =
+@"<CommandTable xmlns=""http://schemas.microsoft.com/VisualStudio/2005-10-18/CommandTable"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <Commands package=""guidTestPackage"">
+    <Menus>
+      <Menu id=""menuidOne"">
+        <Strings>
+          <MenuText>Texte de menu</MenuText>
+          <ButtonText>Texte de button</ButtonText>
+        </Strings>
+      </Menu>
+      <Menu id=""menuidTwo"">
+        <Strings>
+          <MenuText>Plus de texte de menu</MenuText>
+          <ButtonText>Plus de texte de button</ButtonText>
+        </Strings>
+      </Menu>
+    </Menus>
+  </Commands>
+</CommandTable>";
+
+            var document = new VsctDocument();
+            var writer = new StringWriter();
+            document.Load(new StringReader(source));
+            document.Translate(translations);
+            document.Save(writer);
+
+            Assert.Equal(expectedTranslation, writer.ToString());
+        }
+    }
+}
---
+++ src/XliffTasks/VsctDocument.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace XliffTasks
+{
+    /// <summary>
+    /// A <see cref="TranslatableDocument"/> for files in .vsct format.
+    /// See https://msdn.microsoft.com/en-us/library/bb164699.aspx
+    /// </summary>
+    internal sealed class VsctDocument : TranslatableXmlDocument
+    {
+        protected override IEnumerable<TranslatableNode> GetTranslatableNodes()
+        {
+            foreach (var strings in Document.Descendants(Document.Root.Name.Namespace + "Strings"))
+            {
+                string id = strings.Parent.Attribute("id").Value;
+
+                foreach (var child in strings.Elements())
+                {
+                    XName name = child.Name;
+
+                    if (name.LocalName == "CanonicalName")
+                    {
+                        // CanonicalName is never localized.
+                        // LocCanonicalName can be used to specify a localized alternative.
+                        // See https://msdn.microsoft.com/en-us/library/bb491712.aspx
+                        continue;
+                    }
+
+                    yield return new TranslatableXmlElement(
+                        id: $"{id}|{name.LocalName}",
+                        source: child.Value,
+                        note: "",
+                        element: child);
+                }
+            }
+        }
+    }
+}
