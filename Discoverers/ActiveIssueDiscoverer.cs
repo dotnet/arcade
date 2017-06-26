d8e1e2a1e16a5b8a76a1332c255214c01b9ca124
Dan Moseley <danmose@microsoft.com>
2017-06-25 22:34:26 -0700

    add UapNotUapAot to TFM enum (#1583)
    * add UapNotUapAot

--- Discoverers/ActiveIssueDiscoverer.cs
+++ Discoverers/ActiveIssueDiscoverer.cs
@@ -53,7 +53,7 @@ namespace Xunit.NetCore.Extensions
                     yield return new KeyValuePair<string, string>(XunitConstants.Category, XunitConstants.NonNetfxTest);
                 if (frameworks.HasFlag(TargetFrameworkMonikers.Netcoreapp))
                     yield return new KeyValuePair<string, string>(XunitConstants.Category, XunitConstants.NonNetcoreappTest);
-                if (frameworks.HasFlag(TargetFrameworkMonikers.Uap))
+                if (frameworks.HasFlag(TargetFrameworkMonikers.UapNotUapAot))
                     yield return new KeyValuePair<string, string>(XunitConstants.Category, XunitConstants.NonUapTest);
                 if (frameworks.HasFlag(TargetFrameworkMonikers.UapAot))
                     yield return new KeyValuePair<string, string>(XunitConstants.Category, XunitConstants.NonUapAotTest);
