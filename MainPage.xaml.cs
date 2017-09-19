d30efa766bfca9785777e9533a3bbe4771cd5f80
Santiago Fernandez Madero <safern@microsoft.com>
2017-09-18 17:35:00 -0700

    Improve UAP Runner logging and enable showprogress flag (#1667)
    * Improve UAP Runner logging and enable showprogress flag
    
    * PR Feedback and update stdout file location
    
    * Fix typo and add new line in output

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -15,22 +14,40 @@ namespace XUnit.Runner.Uap
         public MainPage()
         {
             this.InitializeComponent();
-            log = new StringBuilder();
+            Application.Current.UnhandledException += OnUnhandledException;
         }
 
-        internal static StringBuilder log;
-
-        public void UpdateLog(string text)
-        {
-            log.AppendLine(text);
-        }
+        internal static StreamWriter log;
 
         private void RunXunitTestsInDirectory(object sender, RoutedEventArgs e)
         {
             // Run tests for assemblies in current directory
             XunitTestRunner runner = new XunitTestRunner();
+            log = Helpers.GetFileStreamWriterInLocalStorageAsync("stdout.txt").GetAwaiter().GetResult();
 
-            Task.Run(() => runner.RunTests(App.LaunchArgs.Arguments, log, UpdateLog));
+            Task.Run(() => runner.RunTests(App.LaunchArgs.Arguments, log));
+        }
+
+        static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.Exception as Exception;
+
+            if (log == null)
+            {
+                log = Helpers.GetFileStreamWriterInLocalStorageAsync("stdout.txt").GetAwaiter().GetResult();
+            }
+
+            if (ex != null)
+            {
+                log.WriteLine(ex.ToString());
+            }
+            else
+            {
+                log.WriteLine("Error of unknown type thrown in application domain");
+            }
+
+            log.Dispose();
+            Application.Current.Exit();
         }
     }
 }
