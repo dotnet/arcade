2f4806481cac2c88175a057810756c8630dc771c
Christopher Costa <chcosta@microsoft.com>
2018-02-26 09:35:01 -0800

    Add netci.groovy for CI

---
+++ netci.groovy
@@ -0,0 +1,73 @@
+// Groovy Script: http://www.groovy-lang.org/syntax.html
+// Jenkins DSL: https://github.com/jenkinsci/job-dsl-plugin/wiki
+
+import jobs.generation.Utilities;
+
+static getJobName(def opsysName, def configName) {
+  return "${opsysName}_${configName}"
+}
+
+static addArchival(def job, def filesToArchive, def filesToExclude) {
+  def doNotFailIfNothingArchived = false
+  def archiveOnlyIfSuccessful = false
+
+  Utilities.addArchival(job, filesToArchive, filesToExclude, doNotFailIfNothingArchived, archiveOnlyIfSuccessful)
+}
+
+static addGithubPRTriggerForBranch(def job, def branchName, def jobName) {
+  def prContext = "prtest/${jobName.replace('_', '/')}"
+  def triggerPhrase = "(?i)^\\s*(@?dotnet-bot\\s+)?(re)?test\\s+(${prContext})(\\s+please)?\\s*\$"
+  def triggerOnPhraseOnly = false
+
+  Utilities.addGithubPRTriggerForBranch(job, branchName, prContext, triggerPhrase, triggerOnPhraseOnly)
+}
+
+static addXUnitDotNETResults(def job, def configName) {
+  def resultFilePattern = "**/artifacts/${configName}/TestResults/*.xml"
+  def skipIfNoTestFiles = false
+    
+  Utilities.addXUnitDotNETResults(job, resultFilePattern, skipIfNoTestFiles)
+}
+
+static addBuildSteps(def job, def projectName, def opsysName, def configName, def isPR) {
+  def buildJobName = getJobName(opsysName, configName)
+  def buildFullJobName = Utilities.getFullJobName(projectName, buildJobName, isPR)
+
+  job.with {
+    steps {
+      batchFile(""".\\build\\CIBuild.cmd -configuration ${configName} -prepareMachine""")
+    }
+  }
+}
+
+[true, false].each { isPR ->
+  ['windows'].each { opsysName ->
+    ['debug', 'release'].each { configName ->
+      def projectName = GithubProject
+
+      def branchName = GithubBranchName
+
+      def filesToArchive = "**/artifacts/${configName}/**"
+      def filesToExclude = "**/artifacts/${configName}/obj/**"
+
+      def jobName = getJobName(opsysName, configName)
+      def fullJobName = InternalUtilities.getFullJobName(projectName, jobName, isPR)
+      def myJob = job(fullJobName)
+
+     Internal Utilities.standardJobSetup(myJob, projectName, isPR, "*/${branchName}")
+
+      if (isPR) {
+        addGithubPRTriggerForBranch(myJob, branchName, jobName)
+      } else {
+        Utilities.addGithubPushTrigger(myJob)
+      }
+      
+      addArchival(myJob, filesToArchive, filesToExclude)
+      addXUnitDotNETResults(myJob, configName)
+
+      Utilities.setMachineAffinity(myJob, 'Windows_NT', 'latest-dev15-3')
+
+      addBuildSteps(myJob, projectName, opsysName, configName, isPR)
+    }
+  }
+}
