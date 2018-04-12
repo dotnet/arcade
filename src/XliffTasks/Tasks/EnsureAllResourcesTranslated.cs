d09ed4f743c9fbd713a7a8ba08b76f1b13e55abd
Tom Meschter <tom.meschter@microsoft.com>
2018-04-12 13:57:09 -0700

    Add a target to ensure all resources are translated
    Add an `EnsureAllResourcesTranslated` task and target that verifies all `trans-unit` items are in a `translated` state. If not, MSBuild errors are logged, failing the build.
    
    Fixes #53.

---
+++ src/XliffTasks/Tasks/EnsureAllResourcesTranslated.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using System.IO;
+using XliffTasks.Model;
+
+namespace XliffTasks.Tasks
+{
+    public sealed class EnsureAllResourcesTranslated : XlfTask
+    {
+        [Required]
+        public ITaskItem[] Sources { get; set; }
+
+        [Required]
+        public string[] Languages { get; set; }
+
+        protected override void ExecuteCore()
+        {
+            foreach (var item in Sources)
+            {
+                string sourceDocumentPath = item.GetMetadataOrDefault(MetadataKey.SourceDocumentPath, item.ItemSpec);
+
+                foreach (var language in Languages)
+                {
+                    string xlfPath = GetXlfPath(sourceDocumentPath, language);
+                    XlfDocument xlfDocument;
+
+                    try
+                    {
+                        xlfDocument = LoadXlfDocument(xlfPath);
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // If the file doesn't exist, we don't need to worry about it having
+                        // untranslated resources.
+                        continue;
+                    }
+
+                    int untranslatedResourceCount = xlfDocument.GetUntranslatedResourceCount();
+                    if (untranslatedResourceCount > 0)
+                    {
+                        Log.LogError($"Xliff file '{xlfPath}' has {untranslatedResourceCount} untranslated resources.");
+                    }
+                }
+            }
+        }
+    }
+}
