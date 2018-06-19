70692b92248b7e692433ab20fd029bf222458e7f
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2018-06-19 12:53:50 -0700

    Not throwing if a Mirror commit is already present in the table (#264)
    * Not throwing if same commit exists
    
    * Exceptions caught and logging added

--- src/Microsoft.DotNet.GitSync.CommitManager/Program.cs
+++ src/Microsoft.DotNet.GitSync.CommitManager/Program.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using CommandLine;
 using Microsoft.WindowsAzure.Storage.Table;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace Microsoft.DotNet.GitSync.CommitManager
 {
@@ -15,6 +18,7 @@ namespace Microsoft.DotNet.GitSync.CommitManager
         private const string _cloudTableName = "CommitHistory";
         private static Table s_table { get; set; }
         private static Dictionary<string, List<string>> s_repos { get; set; } = new Dictionary<string, List<string>>();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public static async Task Main(string[] args)
         {
@@ -30,6 +34,12 @@ namespace Microsoft.DotNet.GitSync.CommitManager
 
         private static async Task SetupAsync(string username, string key)
         {
+            LoggingConfiguration config = new LoggingConfiguration();
+            ConsoleTarget consoleTarget = new ConsoleTarget();
+            consoleTarget.Layout = @"${date:format=HH\:mm\:ss}  ${level:uppercase=true}  ${message} ${exception:format=tostring}";
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, consoleTarget);
+            LogManager.Configuration = config;
+
             s_repos.Add("corefx", new List<string> { "coreclr", "corert" });
             s_repos.Add("coreclr", new List<string> { "corefx", "corert" });
             s_repos.Add("corert", new List<string> { "coreclr", "corefx" });
@@ -47,8 +57,20 @@ namespace Microsoft.DotNet.GitSync.CommitManager
                 {
                     CommitEntity entry = new CommitEntity(sourceRepo, repo, commitId, branch);
                     TableOperation insertOperation = TableOperation.Insert(entry);
-                    await s_table.CommitTable.ExecuteAsync(insertOperation);
-                    Console.WriteLine($"Commit {commitId} added to table to get mirrored from {sourceRepo} to {repo}");
+
+                    try
+                    {
+                        await s_table.CommitTable.ExecuteAsync(insertOperation);
+                        logger.Info($"Commit {commitId} added to table to get mirrored from {sourceRepo} to {repo}");
+                    }
+                    catch (WindowsAzure.Storage.StorageException)
+                    {
+                        logger.Warn($"The commit {commitId} already exists in {repo}");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Warn($"Insert Operation for commit {commitId} for {repo}\n" + ex.Message);
+                    }
                 }
             }
         }
