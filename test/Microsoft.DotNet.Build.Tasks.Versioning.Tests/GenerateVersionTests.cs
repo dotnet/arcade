bcda69149c9c30e04bdbf20574da60139b89aa8a
Cesar Soares Lucas <disoares@microsoft.com>
2018-06-20 15:49:31 -0700

    Fixing compilation error.

--- test/Microsoft.DotNet.Build.Tasks.Versioning.Tests/GenerateVersionTests.cs
+++ test/Microsoft.DotNet.Build.Tasks.Versioning.Tests/GenerateVersionTests.cs
@@ -11,33 +11,7 @@ namespace Microsoft.DotNet.Build.Tasks.Versioning.Tests
 {
     public class GenerateVersionTests
     {
-        private readonly ITestOutputHelper _output;
 
-        public GenerateVersionTests(ITestOutputHelper output)
-        {
-            _output = output;
-        }
-
-        [Fact]
-        public void BasicTestDefaults()
-        {
-            DateTime BaselineDate = new DateTime(1996, 4, 1, 0, 0, 0, DateTimeKind.Utc);
-            DateTime BuildDate = DateTime.UtcNow;
-
-            int months = (BuildDate.Year - BaselineDate.Year) * 12 + BuildDate.Month - BaselineDate.Month;
-
-            string expectedDate = string.Format("{0}{1}", months.ToString("D"), BuildDate.Day.ToString("D2"));
-
-            var task = new GenerateVersionComponents
-            {
-                BuildEngine = new MockEngine()
-            };
-
-            Assert.True(task.Execute());
-            Assert.Equal("0", task.GeneratedRevision);
-            Assert.Equal(expectedDate, task.GeneratedShortDate);
-            Assert.Equal("", task.GeneratedShortSha);
-        }
     }
 }
 
