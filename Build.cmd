ea7337c1d85bc8b61c0ac0ea40e8b5cdaea501ce
Tomas Matousek <tomat@microsoft.com>
2018-08-09 16:30:05 -0700

    Update infrastructure to Arcade 63208-02

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile %~dp0eng\common\Build.ps1 -restore -build %*
+powershell -ExecutionPolicy ByPass -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
 exit /b %ErrorLevel%
