67ef3db3160b7f1b06ef8e103e311beeb49ce2fa
Alex Perovich <alperovi@microsoft.com>
2018-08-16 15:07:07 -0700

    Update TFMs and add comments.

--- src/Microsoft.DotNet.Helix/Sdk/SendHelixJob.cs
+++ src/Microsoft.DotNet.Helix/Sdk/SendHelixJob.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
@@ -8,29 +9,65 @@ namespace Microsoft.DotNet.Helix.Sdk
 {
     public class SendHelixJob : HelixTask
     {
+        /// <summary>
+        ///   The 'source' value reported to helix
+        /// </summary>
+        /// <remarks>
+        ///   This value is used to filter and sort jobs on Mission Control
+        /// </remarks>
         [Required]
         public string Source { get; set; }
 
+        /// <summary>
+        ///   The 'type' value reported to helix
+        /// </summary>
+        /// <remarks>
+        ///   This value is used to filter and sort jobs on Mission Control
+        /// </remarks>
         [Required]
         public string Type { get; set; }
 
+        /// <summary>
+        ///   The 'build' value reported to helix
+        /// </summary>
+        /// <remarks>
+        ///   This value is used to filter and sort jobs on Mission Control
+        /// </remarks>
         [Required]
         public string Build { get; set; }
 
+        /// <summary>
+        ///   The helix queue this job should run on
+        /// </summary>
         [Required]
         public string TargetQueue { get; set; }
 
+        /// <summary>
+        ///   When the task finishes, the correlation id of the job that has been created
+        /// </summary>
         [Output]
         public string JobCorrelationId { get; set; }
 
         /// <summary>
-        /// A set of directories that will be zipped up and sent as Correlation Payloads for the helix job.
+        ///   A set of directories that will be zipped up and sent as Correlation Payloads for the helix job.
         /// </summary>
+        /// <remarks>
+        ///   Metadata Used:
+        ///     FullPath - This path is required to be a directory and will be zipped up and used as a correlation payload
+        /// </remarks>
         public ITaskItem[] CorrelationPayloads { get; set; }
 
         /// <summary>
-        /// A set of work items that will run in the helix job.
+        ///   A set of work items that will run in the helix job.
         /// </summary>
+        /// <remarks>
+        ///   Required Metadata:
+        ///     Identity - The WorkItemName
+        ///     Command - The command that is invoked to execute the work item
+        ///   Optional Metadata:
+        ///     PayloadDirectory - A directory that will be zipped up and sent as the Work Item payload
+        ///     Timeout - A <see cref="System.TimeSpan"/> string that specifies that Work Item execution timeout
+        /// </remarks>
         public ITaskItem[] WorkItems { get; set; }
 
         protected override async Task ExecuteCore()
@@ -103,7 +140,7 @@ namespace Microsoft.DotNet.Helix.Sdk
             var timeoutString = workItem.GetMetadata("Timeout");
             if (!string.IsNullOrEmpty(timeoutString))
             {
-                if (TimeSpan.TryParse(timeoutString, out TimeSpan timeout))
+                if (TimeSpan.TryParse(timeoutString, CultureInfo.InvariantCulture, out TimeSpan timeout))
                 {
                     wi = wi.WithTimeout(timeout);
                 }
