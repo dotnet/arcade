921a7a0b012290f6caae32836fa4c668b27f5ac5
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2018-08-22 16:03:59 -0700

    Logging Filtered, new model(trained with more data), new ML.net version (#484)
    * new model version and threshold change
    
    * feedback addressed and prefi added

--- src/Microsoft.DotNet.Github.IssueLabeler/Models/Predictor.cs
+++ src/Microsoft.DotNet.Github.IssueLabeler/Models/Predictor.cs
@@ -20,9 +20,8 @@ namespace Microsoft.DotNet.GitHub.IssueLabeler
       
             float[] probabilities = prediction.Probabilities;
             float maxProbability = probabilities.Max();
-            logger.LogInformation($"Label {prediction.Area} for {issue.ID} is predicted with confidence {maxProbability.ToString()}");
-
-            return maxProbability > 0.8 ? prediction.Area : null;
+            logger.LogInformation($"# {maxProbability.ToString()} {prediction.Area} for #{issue.ID} {issue.Title}");
+            return maxProbability > 0.7 ? prediction.Area : null;
         }
     }
 }
