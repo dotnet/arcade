c86df2da16bd9bf9257e4f475051e78066d9c459
Tomáš Matoušek <tmat@users.noreply.github.com>
2018-08-31 18:06:10 -0700

    Clean up build tasks (#571)
    * Clean up build tasks.
    
    Removes unnecessary dependencies from SignTool task.
    Use eng/BuildTask.targets in SignTool project.
    Avoid publishing when building for desktop fx. This is unnecessary and causes double-writes.
    
    * Share AssemblyResolution.cs

--- src/Common/AssemblyResolution.cs
+++ src/Common/AssemblyResolution.cs
@@ -5,11 +5,10 @@
 #if NET461
 
 using System;
-using System.Collections.Immutable;
 using System.IO;
 using System.Reflection;
 
-namespace Microsoft.DotNet.SignTool
+namespace Microsoft.DotNet
 {
     internal static class AssemblyResolution
     {
@@ -22,13 +21,26 @@ namespace Microsoft.DotNet.SignTool
         {
             var name = new AssemblyName(args.Name);
 
-            if (name.Name.Equals("System.Collections.Immutable", StringComparison.OrdinalIgnoreCase))
+            if (!name.Name.Equals("System.Collections.Immutable", StringComparison.OrdinalIgnoreCase))
             {
-                var sci = Assembly.LoadFile(Path.Combine(Path.GetDirectoryName(typeof(AssemblyResolution).Assembly.Location), "System.Collections.Immutable.dll"));
-                if (name.Version <= sci.GetName().Version)
-                {
-                    return sci;
-                }
+                return null;
+            }
+
+            var fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "System.Collections.Immutable.dll");
+
+            Assembly sci;
+            try
+            {
+                sci = Assembly.LoadFile(fullPath);
+            }
+            catch
+            {
+                return null;
+            }
+
+            if (name.Version <= sci.GetName().Version)
+            {
+                return sci;
             }
 
             return null;
