d66254c82505c7bea60aa119eac133e655b0316b
Matt Mitchell <mmitche@microsoft.com>
2018-09-12 12:47:27 -0700

    Darc WIP

---
+++ src/Microsoft.DotNet.Darc/src/Darc/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Darc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
