a19a19cf9bc89e0a89f932c8603f36f4e03fa865
Alex Perovich <alperovi@microsoft.com>
2018-09-13 16:12:57 -0700

    Add support for Pre and Post commands in Helix Sdk (#767)

---
+++ src/Microsoft.DotNet.Helix/Sdk/CommandPayload.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.DotNet.Helix.Sdk
+{
+    internal class CommandPayload : IDisposable
+    {
+        private static readonly Encoding s_utf8NoBom = new UTF8Encoding(false);
+
+        private readonly SendHelixJob _task;
+
+        private readonly Lazy<DirectoryInfo> _directoryInfo = new Lazy<DirectoryInfo>(CreateDirectory);
+
+        public DirectoryInfo Directory => _directoryInfo.Value;
+
+        private static DirectoryInfo CreateDirectory()
+        {
+            var dir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N")));
+            dir.Create();
+            return dir;
+        }
+
+        public CommandPayload(SendHelixJob task)
+        {
+            _task = task;
+        }
+
+        public string AddCommandFile(IEnumerable<string> commands)
+        {
+            var contents = new StringBuilder();
+            string name;
+            if (_task.IsPosixShell)
+            {
+                name = $"scripts/{Guid.NewGuid():N}/execute.sh";
+                contents.Append("#!/bin/sh\n");
+                foreach (var command in commands)
+                {
+                    contents.Append(command + "\n");
+                }
+            }
+            else
+            {
+                name = $"scripts\\{Guid.NewGuid():N}\\execute.cmd";
+                foreach (var command in commands)
+                {
+                    contents.Append(command + "\r\n");
+                }
+            }
+
+            var scriptFile = new FileInfo(Path.Combine(Directory.FullName, name));
+            scriptFile.Directory.Create();
+            File.WriteAllText(scriptFile.FullName, contents.ToString(), s_utf8NoBom);
+            return name;
+        }
+
+        public bool TryGetPayloadDirectory(out string directory)
+        {
+            if (_directoryInfo.IsValueCreated)
+            {
+                directory = Directory.FullName;
+                return true;
+            }
+
+            directory = null;
+            return false;
+        }
+
+        public void Dispose()
+        {
+            if (_directoryInfo.IsValueCreated)
+            {
+                _directoryInfo.Value.Delete(true);
+            }
+        }
+    }
+}
