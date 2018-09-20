8b3fa6197f370e6813402ded3615aaa715099d9c
Jon Fortescue <jofortes@microsoft.com>
2018-09-19 21:17:38 -0700

    Update waiter

--- src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
+++ src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
@@ -13,14 +13,45 @@ namespace Microsoft.DotNet.Helix.Sdk
     public class HelixWait : HelixTask
     {
         /// <summary>
-        /// 
+        /// An array of Helix Job IDs to be waited on
         /// </summary>
         [Required]
         public string[] JobIds { get; set; }
 
-        protected override Task ExecuteCore()
+        /// <summary>
+        /// The number of work items sent to Helix
+        /// </summary>
+        [Required]
+        public int NumberOfWorkItems { get; set; }
+
+        protected async override Task ExecuteCore()
         {
-            throw new NotImplementedException();
+            bool finished = false;
+            while (!finished)
+            {
+                finished = await WorkItemsFinished();
+            }
+        }
+
+        private async Task<bool> WorkItemsFinished()
+        {
+            foreach (string jobId in JobIds)
+            {
+                var workItems = await HelixApi.Aggregate.WorkItemSummaryMethodAsync(new string[] { "job.build" }, filtername: jobId);
+                if (workItems.Count < 1)
+                {
+                    Log.LogError($"No work itmes found for job {jobId}");
+                    return true;
+                }
+                int? pass, fail;
+                workItems[0].Data.WorkItemStatus.TryGetValue("pass", out pass);
+                workItems[0].Data.WorkItemStatus.TryGetValue("fail", out fail);
+                if ((pass ?? 0 + fail ?? 0) < NumberOfWorkItems)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
