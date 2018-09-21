665a79388d636143bfc1fcc025cbf84c65ecd80c
Jon Fortescue <jofortes@microsoft.com>
2018-09-20 17:54:05 -0700

    Correct error logging and API

--- src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
+++ src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Microsoft.DotNet.Helix.Client;
@@ -37,6 +38,8 @@ namespace Microsoft.DotNet.Helix.Sdk
             while (!finished)
             {
                 finished = await WorkItemsFinished();
+
+                await Task.Delay(10000); // Don't overload that Helix API
             }
         }
 
@@ -44,18 +47,20 @@ namespace Microsoft.DotNet.Helix.Sdk
         {
             for (int i = 0; i < JobIds.Length; i++)
             {
-                var workItems = await HelixApi.Aggregate.WorkItemSummaryMethodAsync(new string[] { "job.build" }, filtername: JobIds[i]);
-                if (workItems.Count < 1)
+                var workItemSummary = await HelixApi.Aggregate.JobSummaryMethodAsync(new string[] { "job.source" }, 1, filtername:JobIds[i]);
+                if (workItemSummary.Count < 1)
                 {
-                    Log.LogError($"No work itmes found for job {JobIds[i]}");
+                    Log.LogError($"Job {JobIds[i]} not found -- something has gone terribly wrong");
                     return true;
                 }
+
                 int? pass, fail;
-                workItems[0].Data.WorkItemStatus.TryGetValue("pass", out pass);
-                workItems[0].Data.WorkItemStatus.TryGetValue("fail", out fail);
+                workItemSummary[0].Data.WorkItemStatus.TryGetValue("pass", out pass);
+                workItemSummary[0].Data.WorkItemStatus.TryGetValue("fail", out fail);
+
                 if ((fail ?? 0) > 0)  // If a work item has failed, we don't need to wait anymore and can just state that a workitem has fireballed
                 {
-                    WorkItemStatus = "##vso[task.logissue type=error;]One or more work items failed. See Mission Control for more information.";
+                    Log.LogError("One or more work items failed. See Mission Control for more information.");
                     return true;
                 }
                 else if ((pass ?? 0) < WorkItemCounts[i])  // if the workitems haven't finished, we need to keep waiting
@@ -65,10 +70,10 @@ namespace Microsoft.DotNet.Helix.Sdk
                 else  // if they have finished, we should check to see if any of the tests failed. if they have, we can stop early
                 {
                     int? testFailures;
-                    workItems[0].Data.Analysis[0].Status.TryGetValue("fail", out testFailures);
+                    workItemSummary[0].Data.Analysis[0].Status.TryGetValue("fail", out testFailures);
                     if ((testFailures ?? 0) > 0)
                     {
-                        WorkItemStatus = "##vso[task.logissue type=error;]One or more tests have failed. See Mission Control for more information.";
+                        Log.LogError("One or more tests have failed. See Mission Control for more information.");
                         return true;
                     }
                 }
