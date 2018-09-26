9947790739e2e4a448fcdacc30a23ae60e64ae66
Matt Mitchell <mmitche@microsoft.com>
2018-09-26 10:12:47 -0700

    Add Id in output

--- src/Microsoft.DotNet.Darc/src/Darc/Operations/GetSubscriptionsOperation.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Operations/GetSubscriptionsOperation.cs
@@ -55,6 +55,7 @@ namespace Microsoft.DotNet.Darc.Operations
                                              $"{subscription.SourceRepository}{subscription.Channel}{subscription.TargetRepository}{subscription.TargetBranch}"))
                 {
                     Console.WriteLine($"{subscription.SourceRepository} ({subscription.Channel.Name}) ==> '{subscription.TargetRepository}' ('{subscription.TargetBranch}')");
+                    Console.WriteLine($"  Id: {subscription.Id}");
                     Console.WriteLine($"  Trigger: {subscription.Policy.UpdateFrequency}");
                     Console.WriteLine($"  Merge Policy: {subscription.Policy.MergePolicy}");
                     Console.WriteLine($"  Last Build: {(subscription.LastAppliedBuild != null ? subscription.LastAppliedBuild.BuildNumber : "N/A")}");
