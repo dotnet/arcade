3779bf2204fe440829bc55a38812b824d4aba99d
Cesar Soares Lucas <disoares@microsoft.com>
2018-09-27 10:27:36 -0700

    Fixing correct name for the certificates.

--- src/Microsoft.DotNet.SignTool/src/SignToolConstants.cs
+++ src/Microsoft.DotNet.SignTool/src/SignToolConstants.cs
@@ -10,7 +10,7 @@ namespace Microsoft.DotNet.SignTool
 
         // These certificate are special because they are used when we want 
         // to sign a file that is already signed.
-        public const string Certificate_Microsoft3rdPartyAppComponentDual = "Microsoft3rdPartyAppComponentDual";
-        public const string Certificate_Microsoft3rdPartyAppComponentSha2 = "Microsoft3rdPartyAppComponentSha2";
+        public const string Certificate_Microsoft3rdPartyAppComponentDual = "3PartyDual";
+        public const string Certificate_Microsoft3rdPartyAppComponentSha2 = "3PartySHA2";
     }
 }
