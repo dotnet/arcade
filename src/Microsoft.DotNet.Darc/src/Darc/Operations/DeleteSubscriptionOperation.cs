d2792fcee43a3501596241b3a967787a90fb3727
Matt Mitchell <mmitche@microsoft.com>
2018-10-03 08:21:37 -0700

    Add header and sort usings

--- src/Microsoft.DotNet.Darc/src/Darc/Operations/DeleteSubscriptionOperation.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Operations/DeleteSubscriptionOperation.cs
@@ -1,12 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Microsoft.DotNet.Darc.Helpers;
 using Microsoft.DotNet.Darc.Options;
 using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.Maestro.Client.Models;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.Darc.Operations
