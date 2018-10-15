d28ab7dac629055e0da05c8b7806d7746112fcc8
Cesar Soares Lucas <disoares@microsoft.com>
2018-10-15 16:26:44 -0700

    Adding license header.

--- src/SignCheck/Microsoft.SignCheck/Interop/RevocationChecks.cs
+++ src/SignCheck/Microsoft.SignCheck/Interop/RevocationChecks.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.SignCheck.Interop
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.SignCheck.Interop
 {
     public enum RevocationChecks
     {
--- src/SignCheck/Microsoft.SignCheck/Verification/PortableExecutableVerifier.cs
+++ src/SignCheck/Microsoft.SignCheck/Verification/PortableExecutableVerifier.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.SignCheck.Interop.PortableExecutable;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.SignCheck.Interop.PortableExecutable;
 using Microsoft.SignCheck.Logging;
 
 namespace Microsoft.SignCheck.Verification
