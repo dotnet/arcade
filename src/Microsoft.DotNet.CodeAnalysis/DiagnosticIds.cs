67607d0c9ee2c9fa554e0e40127d022cb13def9e
Santiago Fernandez Madero <safern@microsoft.com>
2018-10-17 16:55:07 -0700

    Move code analyzers to arcade and add analyzer features to reference them in Tools.props and filter analyzers

---
+++ src/Microsoft.DotNet.CodeAnalysis/DiagnosticIds.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.DotNet.CodeAnalysis
+{
+    internal enum DiagnosticIds
+    {
+        BCL0001, // MemberMustExist
+
+        // AppContext related 
+        BCL0010, // AppContextDefaultNotInitializedToTrueDiagnostic
+        BCL0011, // AppContextDefaultUsedUnexpectedIfStatement
+        BCL0012, // DefaultValueDefinedOutsideIfCondition
+
+        BCL0015, // PinvokeCallCheck
+
+        BCL0020, // ResourceUsageCheck
+    }
+}
