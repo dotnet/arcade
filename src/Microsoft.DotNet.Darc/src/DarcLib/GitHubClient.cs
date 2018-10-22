df19e69bf23981e01721294cfed6e7936f5f797d
juanam <juanam@microsoft.com>
2018-10-22 15:45:14 -0700

    Working solution

--- src/Microsoft.DotNet.Darc/src/DarcLib/GitHubClient.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/GitHubClient.cs
@@ -159,7 +159,7 @@ namespace Microsoft.DotNet.DarcLib
                 string baseCommitSha = await Client.Repository.Commit.GetSha1(owner, repo, branch);
                 Octokit.Commit baseCommit = await Client.Git.Commit.Get(owner, repo, baseCommitSha);
                 TreeResponse baseTree = await Client.Git.Tree.Get(owner, repo, baseCommit.Tree.Sha);
-                TreeResponse newTree = await CreateGitHubTreeAsync(owner, repo, branch, filesToCommit, baseTree);
+                TreeResponse newTree = await CreateGitHubTreeAsync(owner, repo, filesToCommit, baseTree);
                 Octokit.Commit newCommit = await Client.Git.Commit.Create(
                     owner,
                     repo,
@@ -654,18 +654,19 @@ namespace Microsoft.DotNet.DarcLib
         private async Task<TreeResponse> CreateGitHubTreeAsync(
             string owner,
             string repo,
-            string branch,
             IEnumerable<GitFile> filesToCommit,
             TreeResponse baseTree)
         {
+            string baseTreeSha = baseTree.Sha;
+
             IEnumerable<GitFile> filesToRemove = filesToCommit.Where(f => f.Operation == GitFileOperation.Delete);
 
             if (filesToRemove.Any())
             {
-                await RemoveFilesFromTree(owner, repo, branch, baseTree.Sha, filesToRemove);
+                baseTreeSha = await RemoveFilesFromTree(owner, repo, baseTree.Sha, filesToRemove);
             }
 
-            var newTree = new NewTree {BaseTree = baseTree.Sha };
+            var newTree = new NewTree {BaseTree = baseTreeSha };
 
             foreach (GitFile file in filesToCommit.Where(f => f.Operation == GitFileOperation.Add))
             {
@@ -745,45 +746,38 @@ namespace Microsoft.DotNet.DarcLib
             return treeItems;
         }
 
-        private async Task RemoveFilesFromTree(string owner, string repo, string branch, string treeSha, IEnumerable<GitFile> filesToRemove)
+        /// <summary>
+        /// This approach was based on https://github.com/octokit/octokit.net/issues/1610 which suggests
+        /// to clone the base tree without the files to be removed.
+        /// </summary>
+        /// <param name="owner">Repo owner.</param>
+        /// <param name="repo">The repo.</param>
+        /// <param name="baseTreeSha">The base tree sha.</param>
+        /// <param name="filesToRemove">The files to remove.</param>
+        /// <returns>The new tree sha.</returns>
+        private async Task<string> RemoveFilesFromTree(string owner, string repo, string baseTreeSha, IEnumerable<GitFile> filesToRemove)
         {
-            TreeResponse rootTree = await Client.Git.Tree.Get(owner, repo, treeSha);
-            TreeItem engTreeItem = rootTree.Tree.Where(t => t.Path == "eng").FirstOrDefault();
+            TreeResponse rootTree = await Client.Git.Tree.GetRecursive(owner, repo, baseTreeSha);
 
-            if (engTreeItem == null)
-            {
-                return;
-            }
-
-            TreeResponse engTree = await Client.Git.Tree.Get(owner, repo, engTreeItem.Sha);
-            TreeItem engCommonTreeItem = engTree.Tree.Where(t => t.Path == "common").FirstOrDefault();
-
-            if (engCommonTreeItem == null)
-            {
-                return;
-            }
-
-            TreeResponse engCommonTree = await Client.Git.Tree.Get(owner, repo, engCommonTreeItem.Sha);
             NewTree newTree = new NewTree();
-
-            foreach (TreeItem treeItem in engCommonTree.Tree)
-            {
-                if (!filesToRemove.Any(f => f.FilePath == $"eng/common/{treeItem.Path}"))
+            rootTree.Tree.Where(tree => tree.Type != TreeType.Tree)
+                .Select(tree => new NewTreeItem
                 {
-                    NewTreeItem newTreeItem = new NewTreeItem
+                    Path = tree.Path,
+                    Mode = tree.Mode,
+                    Type = tree.Type.Value,
+                    Sha = tree.Sha
+                }).ToList()
+                    .ForEach(tree =>
                     {
-                        Mode = treeItem.Mode,
-                        Path = treeItem.Path,
-                        Sha = treeItem.Sha,
-                        Type = treeItem.Type.Value
-                    };
-
-                    newTree.Tree.Add(newTreeItem);
-                }
-            }
+                        if (!filesToRemove.Any(f => f.FilePath == tree.Path))
+                        {
+                            newTree.Tree.Add(tree);
+                        }
+                    });
 
             TreeResponse createdTree = await Client.Git.Tree.Create(owner, repo, newTree);
-            await Client.Git.Reference.Update(owner, repo, $"heads/{branch}", new ReferenceUpdate(createdTree.Sha));
+            return createdTree.Sha;
         }
     }
 }
