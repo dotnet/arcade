47fc063a80d166acdc6bec40aa5a7ad8a797c469
Jon Fortescue <jofortes@microsoft.com>
2018-10-25 16:08:19 -0700

    Add Mission Control link to Azure DevOps logs

--- src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
+++ src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Microsoft.DotNet.Helix.Client;
+using Newtonsoft.Json;
 
 namespace Microsoft.DotNet.Helix.Sdk
 {
@@ -42,12 +44,39 @@ namespace Microsoft.DotNet.Helix.Sdk
                 var finishedCount = workItems.Count(wi => wi.State == "Finished");
                 if (waitingCount == 0 && runningCount == 0)
                 {
-                    Log.LogMessage($"Job {jobName} is completed with {finishedCount} finished work items.");
+                    Log.LogMessage($"Job {jobName} is completed with {finishedCount} finished work items. Results can be found at {GetMissionControlResultUri()}");
                     return;
                 }
 
                 Log.LogMessage($"Job {jobName} is not yet completed with Waiting: {waitingCount}, Running: {runningCount}, Finished: {finishedCount}");
                 await Task.Delay(10000);
+            }
+        }
+
+        private string GetMissionControlResultUri()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "AzureDevOps");
+                string githubJson = "";
+                try
+                {
+                    githubJson = client.DownloadString($"https://api.github.com/user?access_token={AccessToken}");
+                }
+                catch (WebException e)
+                {
+                    Log.LogMessage("Failed to connect to GitHub to retrieve username", e.StackTrace);
+                    return "Mission Control (generation of MC link failed)";
+                }
+                JsonTextReader jsonTextReader = new JsonTextReader(new StringReader(githubJson));
+                do
+                {
+                    jsonTextReader.Read();
+                } while (!(jsonTextReader.TokenType == JsonToken.PropertyName && (string)jsonTextReader.Value == "login"));
+                jsonTextReader.Read();
+                string userName = (string)jsonTextReader.Value;
+
+                return $"https://mc.dot.net/#/user/{userName}/builds";
             }
         }
     }
