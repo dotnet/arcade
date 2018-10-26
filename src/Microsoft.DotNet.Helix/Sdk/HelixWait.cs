c42ac22985986999c525bcdcf84b68aec184e269
Jon Fortescue <jofortes@microsoft.com>
2018-10-25 17:29:16 -0700

    PR feedback

--- src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
+++ src/Microsoft.DotNet.Helix/Sdk/HelixWait.cs
@@ -4,12 +4,14 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Microsoft.DotNet.Helix.Client;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.DotNet.Helix.Sdk
 {
@@ -26,6 +28,9 @@ namespace Microsoft.DotNet.Helix.Sdk
             // Wait 1 second to allow helix to register the job creation
             await Task.Delay(1000);
 
+            string mcUri = await GetMissionControlResultUri();
+            Log.LogMessage(MessageImportance.High, $"Results will be available from {mcUri}");
+
             List<string> jobNames = Jobs.Select(j => j.GetMetadata("Identity")).ToList();
 
             await Task.WhenAll(jobNames.Select(WaitForHelixJobAsync));
@@ -34,9 +39,7 @@ namespace Microsoft.DotNet.Helix.Sdk
         private async Task WaitForHelixJobAsync(string jobName)
         {
             await Task.Yield();
-            string mcUri = GetMissionControlResultUri()
             Log.LogMessage($"Waiting for completion of job {jobName}");
-            Log.LogMessage(MessageImportance.High, $"Results will be available from {mcUri}");
 
             while (true)
             {
@@ -46,37 +49,31 @@ namespace Microsoft.DotNet.Helix.Sdk
                 var finishedCount = workItems.Count(wi => wi.State == "Finished");
                 if (waitingCount == 0 && runningCount == 0)
                 {
-                    Log.LogMessage(MessageImportance.High, $"Job {jobName} is completed with {finishedCount} finished work items. Results can be found at {mcUri}");
+                    Log.LogMessage(MessageImportance.High, $"Job {jobName} is completed with {finishedCount} finished work items.");
                     return;
                 }
 
-                Log.LogMessage(MessageImportance.High, $"Job {jobName} is not yet completed with Waiting: {waitingCount}, Running: {runningCount}, Finished: {finishedCount}");
+                Log.LogMessage($"Job {jobName} is not yet completed with Waiting: {waitingCount}, Running: {runningCount}, Finished: {finishedCount}");
                 await Task.Delay(10000);
             }
         }
 
-        private string GetMissionControlResultUri()
+        private async Task<string> GetMissionControlResultUri()
         {
-            using (WebClient client = new WebClient())
+            using (HttpClient client = new HttpClient())
             {
-                client.Headers.Add("User-Agent", "AzureDevOps");
+                client.DefaultRequestHeaders.Add("User-Agent", "AzureDevOps");
                 string githubJson = "";
                 try
                 {
-                    githubJson = client.DownloadString($"https://api.github.com/user?access_token={AccessToken}");
+                    githubJson = await client.GetStringAsync($"https://api.github.com/user?access_token={AccessToken}");
                 }
-                catch (WebException e)
+                catch (HttpRequestException e)
                 {
                     Log.LogMessage(MessageImportance.High, "Failed to connect to GitHub to retrieve username", e.StackTrace);
                     return "Mission Control (generation of MC link failed)";
                 }
-                JsonTextReader jsonTextReader = new JsonTextReader(new StringReader(githubJson));
-                do
-                {
-                    jsonTextReader.Read();
-                } while (!(jsonTextReader.TokenType == JsonToken.PropertyName && (string)jsonTextReader.Value == "login"));
-                jsonTextReader.Read();
-                string userName = (string)jsonTextReader.Value;
+                string userName = JObject.Parse(githubJson)["login"].ToString();
 
                 return $"https://mc.dot.net/#/user/{userName}/builds";
             }
