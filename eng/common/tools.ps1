001f82ee9ac67657e08a0491ca113f00ddb2cfab
Mike McLaughlin <mikem@microsoft.com>
2018-11-08 11:33:33 -0800

    Fixed some minor problems for the diagnostic team's repos.
    Add optional architecture parameter to InstallDotNetSdk.

--- eng/common/tools.ps1
+++ eng/common/tools.ps1
@@ -161,9 +161,14 @@ function GetDotNetInstallScript([string] $dotnetRoot) {
   return $installScript
 }
 
-function InstallDotNetSdk([string] $dotnetRoot, [string] $version) {
+function InstallDotNetSdk([string] $dotnetRoot, [string] $version, [string] $architecture = "") {
   $installScript = GetDotNetInstallScript $dotnetRoot
-  & $installScript -Version $version -InstallDir $dotnetRoot
+  if ($architecture -eq "") {
+    & $installScript -Version $version -InstallDir $dotnetRoot
+  }
+  else {
+    & $installScript -Version $version -InstallDir $dotnetRoot -Architecture $architecture
+  }
   if ($lastExitCode -ne 0) {
     Write-Host "Failed to install dotnet cli (exit code '$lastExitCode')." -ForegroundColor Red
     ExitWithExitCode $lastExitCode
