ec05a0758b160026215ed352b07097130957bf72
Matt Mitchell <mmitche@microsoft.com>
2018-11-19 10:02:50 -0800

    respond to feedback

--- src/Maestro/Maestro.Web/RepositoryUrlAttribute.cs
+++ src/Maestro/Maestro.Web/RepositoryUrlAttribute.cs
@@ -15,18 +15,24 @@ namespace Maestro.Web
     /// <summary>
     ///     Validates a repository url. This is targeted at the target repository url of a subscription.
     ///     These generally come in two desired forms at the moment:
-    ///     - https://github.com/someorg/somerepo
-    ///     - https://dev.azure.com/someaccount/someproject/_git/somerepo
+    ///     <list type="bullet">
+    ///         <item>https://github.com/someorg/somerepo</item>
+    ///         <item>https://dev.azure.com/someaccount/someproject/_git/somerepo</item>
+    ///     </list>
     ///     While this is simple enough, there are other forms of 'dev.azure.com' repository URL that
     ///     also appear:
-    ///     - https://dnceng.visualstudio.com/someproject/_git/somerepo
-    ///     - https://dnceng@dev.azure.com/dnceng/someproject/_git/somerepo
+    ///     <list type="bullet">
+    ///         <item>https://dnceng.visualstudio.com/someproject/_git/somerepo</item>
+    ///         <item>https://dnceng@dev.azure.com/dnceng/someproject/_git/somerepo</item>
+    ///     </list>
     ///     If a user creates new subscriptions using a variety of these URLS the same target repo,
     ///     the repository level merge policies will diverge, and batched merges may not occur
     ///     as desired.
     ///     We could go two ways with this:
-    ///     - Canonicalize the input dev azure URL
-    ///     - Validate that the input URL is of a correct form.
+    ///     <list type="bullet">
+    ///         <item>Canonicalize the input dev azure URL</item>
+    ///         <item>Validate that the input URL is of a correct form.</item>
+    ///     </list>
     ///     Rather than try to guess exactly what the user meant, this code validates that all input URLS
     ///     are either of the first or second form listed initially.
     /// </summary>
@@ -35,7 +41,7 @@ namespace Maestro.Web
         /// <summary>
         ///     To add a new valid URL form, add an entry here
         /// </summary>
-        private readonly List<Regex> validUrlForms = new List<Regex>()
+        private static readonly List<Regex> _validUrlForms = new List<Regex>()
         {
             new Regex(@"^https://github\.com/[a-zA-Z0-9]+/[a-zA-Z0-9-]+$"),
             new Regex(@"^https://dev\.azure\.com/[a-zA-Z0-9]+/[a-zA-Z0-9-]+/_git/[a-zA-Z0-9-\.]+$")
@@ -44,11 +50,10 @@ namespace Maestro.Web
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             string targetUrl = (string)value;
-            if (!validUrlForms.Any(form => form.IsMatch(targetUrl)))
+            if (!_validUrlForms.Any(form => form.IsMatch(targetUrl)))
             {
-                string validForms = validUrlForms.Aggregate<Regex, string>("",
-                    (curr, form) => $"{curr}\n{form.ToString()}");
-                return new ValidationResult($"Target repository URL should be one of the following forms:{validForms}");
+                return new ValidationResult("Target repository URL should be one of the following forms: " +
+                    "https://github.com/:org/:repo or https://dev.azure.com/:account/:project/_git/:repo");
             }
             return ValidationResult.Success;
         }
