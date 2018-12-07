80d9a3b58c2fbe6b86c5c2c359f3741868d4d13f
juanam <juanam@microsoft.com>
2018-12-06 16:28:30 -0800

    File rename based on convention

--- eng/Validate-Sdk.cmd
+++ eng/Validate-Sdk.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0Validate-Sdk.ps1""" -restore -build -test -sign -pack -publish -ci %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0validate-sdk.ps1""" -restore -build -test -sign -pack -publish -ci %*"
 exit /b %ErrorLevel%
