08102ab4fad474a7d5a0807485800da12ff3656f
Matt Mitchell <mmitche@microsoft.com>
2018-12-20 09:56:48 -0800

    Allow for darc version to be specified by Microsoft.DotNet.Darc package Darc is moving to arcade services, and so will no longer be bound to the arcade sdk version. Preference using Microsoft.DotNet.Darc in global.json.

--- eng/common/darc-init.ps1
+++ eng/common/darc-init.ps1
@@ -12,11 +12,21 @@ function InstallDarcCli {
     Invoke-Expression "& `"$dotnet`" tool uninstall $darcCliPackageName -g"
   }
 
-  $toolsetVersion = $GlobalJson.'msbuild-sdks'.'Microsoft.DotNet.Arcade.Sdk'
+  # Preference the "Microsoft.DotNet.Darc" tool in global json.
+  # If it exists, use that as the version, otherwise use the arcade sdk version.
+  # TODO: This should eventually be replaced with a call to the build asset registry,
+  # when anonymous access is available for get-builds. Then, we should grab the latest
+  # build number from the prod channel, or the latest int build number if an additional
+  # parameter (e.g. -prerelease) is provided to the script.
+  if (Get-Member -InputObject $GlobalJson.tools -Name "Microsoft.DotNet.Darc") {
+    $darcVersion = $GlobalJson.'tools'.'Microsoft.DotNet.Darc'
+  } else {
+    $darcVersion = $GlobalJson.'msbuild-sdks'.'Microsoft.DotNet.Arcade.Sdk'
+  }
 
-  Write-Host "Installing Darc CLI version $toolsetVersion..."
+  Write-Host "Installing Darc CLI version $darcVersion..."
   Write-Host "You may need to restart your command window if this is the first dotnet tool you have installed."
-  Invoke-Expression "& `"$dotnet`" tool install $darcCliPackageName --version $toolsetVersion -v $verbosity -g"
+  Invoke-Expression "& `"$dotnet`" tool install $darcCliPackageName --version $darcVersion -v $verbosity -g"
 }
 
 InstallDarcCli
