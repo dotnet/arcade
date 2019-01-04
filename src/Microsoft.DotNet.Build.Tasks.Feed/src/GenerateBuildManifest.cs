d8b1e97f4e3abab2d2f5e9ead38a87d7a4d63ec7
Nate McMaster <nate.mcmaster@microsoft.com>
2019-01-04 10:41:24 -0800

    Add a task for generating the build manifest which doesn't require publishing credentials

---
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/GenerateBuildManifest.cs
@@ -0,0 +1,76 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using System;
+using MSBuild = Microsoft.Build.Utilities;
+
+namespace Microsoft.DotNet.Build.Tasks.Feed
+{
+    /// <summary>
+    /// This task generates an XML build manifest for a list of packages and files.
+    /// </summary>
+    public class GenerateBuildManifest : MSBuild.Task
+    {
+        /// <summary>
+        /// An list of files produced by the build.
+        /// <para>
+        /// Supported metadata values:
+        /// 
+        ///   * RelativeBlobPath = the expect path of a blob once it has been pushed to blob storage
+        ///   * ManifestArtifactData = an arbitrary key=value list of properties
+        /// </para>
+        /// </summary>
+        [Required]
+        public ITaskItem[] Artifacts { get; set; }
+
+        /// <summary>
+        /// The location where the build XML file will be generated
+        /// </summary>
+        [Required]
+        public string OutputPath { get; set; }
+
+        /// <summary>
+        /// The CI build ID.
+        /// </summary>
+        public string BuildId { get; set; } = "no build id provided";
+
+        /// <summary>
+        /// Named properties, and abitrary list of metadata about the current build.
+        /// </summary>
+        public string[] BuildData { get; set; }
+
+        /// <summary>
+        /// The URI of the source code repostory used to produce the artifacts.
+        /// </summary>
+        public string RepoUri { get; set; }
+
+        /// <summary>
+        /// The branch name of the source code used to produce the artifacts.
+        /// </summary>
+        public string RepoBranch { get; set; }
+
+        /// <summary>
+        /// The commit hash of the source code used to produce the artifacts.
+        /// </summary>
+        public string RepoCommit { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                var buildModel = BuildManifestUtil.CreateModelFromItems(Artifacts, BuildId, BuildData, RepoUri, RepoBranch, RepoCommit);
+
+                buildModel.WriteAsXml(OutputPath, Log);
+            }
+            catch (Exception e)
+            {
+                Log.LogErrorFromException(e, true);
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+
+    }
+}
---
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/common/MSBuildListSplitter.cs
@@ -0,0 +1,51 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.DotNet.Build.Tasks.Feed
+{
+    internal static class MSBuildListSplitter
+    {
+        public static IDictionary<string, string> GetNamedProperties(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            return GetNamedProperties(input.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public static IDictionary<string, string> GetNamedProperties(string[] input)
+        {
+            var values = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            if (input == null)
+            {
+                return values;
+            }
+
+            foreach (var item in input)
+            {
+                var splitIdx = item.IndexOf('=');
+                if (splitIdx < 0)
+                {
+                    continue;
+                }
+
+                var key = item.Substring(0, splitIdx).Trim();
+                if (string.IsNullOrEmpty(key))
+                {
+                    continue;
+                }
+
+                var value = item.Substring(splitIdx + 1);
+                values[key] = value;
+            }
+
+            return values;
+        }
+    }
+}
