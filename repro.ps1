995012a9e282f970e02c33f09fddecdc82bc68e1
Alex Perovich <alperovi@microsoft.com>
2019-01-09 17:24:05 -0800

    Add a retried test

--- repro.ps1
+++ repro.ps1
@@ -26,25 +26,78 @@ $runId = $run.id
 
 $body = @(
   @{
-    testCaseTitle="TestTest";
-    automatedTestName="TestAssembly.TestTest";
+    testCaseTitle="TestTest1";
+    automatedTestName="TestAssembly.TestTest1";
+    priority=1;
+    resultGroupType='rerun';
+    state="Completed";
+    outcome="Passed";
+    durationInMs=1250;
+    subResults=@(
+      @{
+        outcome="Failed";
+        errorMessage="An Error Occured";
+        stackTrace="This is a stack trace";
+        displayName="Attempt #1";
+        sequenceId=1;
+        durationInMs=1250;
+      },
+      @{
+        outcome="Passed";
+        displayName="Attempt #2";
+        sequenceId=2;
+        durationInMs=1250;
+      }
+    )
+  },
+  @{
+    testCaseTitle="TestTest2";
+    automatedTestName="TestAssembly.TestTest2";
+    priority=1;
+    resultGroupType='rerun';
+    state="Completed";
+    outcome="Failed";
+    errorMessage="An Error Occured";
+    stackTrace="This is a stack trace";
+    durationInMs=1250;
+    subResults=@(
+      @{
+        outcome="Failed";
+        errorMessage="An Error Occured";
+        stackTrace="This is a stack trace";
+        displayName="Attempt #1";
+        sequenceId=1;
+        durationInMs=1250;
+      },
+      @{
+        outcome="Failed";
+        errorMessage="An Error Occured";
+        stackTrace="This is a stack trace";
+        displayName="Attempt #2";
+        sequenceId=2;
+        durationInMs=1250;
+      }
+    )
+  },
+  @{
+    testCaseTitle="TestTestTest1";
+    automatedTestName="TestAssembly.TestTestTest1";
+    priority=1;
+    outcome="Passed";
+    state="Completed";
+  },
+  @{
+    testCaseTitle="TestTestTest2";
+    automatedTestName="TestAssembly.TestTestTest2";
     priority=1;
     outcome="Failed";
     state="Completed";
     errorMessage="An Error Occured";
     stackTrace="This is a stack trace";
-  },
-  @{
-    testCaseTitle="TestTestTest";
-    automatedTestName="TestAssembly.TestTestTest";
-    priority=1;
-    outcome="Passed";
-    state="Completed";
   }
 ) | ConvertTo-Json
 Write-Output $body
 Invoke-RestMethod "$collectionUri$teamProject/_apis/test/runs/$runId/results?api-version=5.0-preview.5" -Method Post -ContentType 'application/json' -Body $body -Verbose -Headers $headers
-
 
 
 
