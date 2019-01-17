e963a7db8ba6c41dc338032dba993b393490467b
Tom Meschter <tom.meschter@microsoft.com>
2019-01-17 13:52:59 -0800

    Add helper methods for "target" and "state"
    Per the XLIFF 1.2 spec, a `trans-unit` element must have a `source` element, but `target` is optional. Similarly, within a `target` element the `state` attribute is optional. As we manipulate .xlf documents we need to keep this in mind. This commit introduces some helper methods to makie it easier to get and set the value of `state` and `target`.
    
    - When getting the `target` value, if the `target` element is not present we instead return the `source` value.
    - When getting the `state` value, if the attribute is not present we default to returning "new".
    - When setting the `target` value, if the element is not already present we create it, and ensure that it comes immediately after the `source` element as required by the spec (or more precisely, it must come before the `note` element, if any).
    - When setting the `state` value, we create the both the `target` element (as above) and `state` attribute if necessary.

---
+++ src/XliffTasks.Tests/XElementExtensionsTests.cs
@@ -0,0 +1,164 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Xml.Linq;
+using XliffTasks.Model;
+using Xunit;
+
+namespace XliffTasks.Tests
+{
+    public class XElementExtensionsTests
+    {
+        private static XNamespace XliffNS = "urn:oasis:names:tc:xliff:document:1.2";
+        private static XNamespace XsiNS = "http://www.w3.org/2001/XMLSchema-instance";
+        private static XName Xliff = XliffNS + "xliff";
+        private static XName File = XliffNS + "file";
+        private static XName Body = XliffNS + "body";
+        private static XName Group = XliffNS + "group";
+        private static XName TransUnit = XliffNS + "trans-unit";
+        private static XName Source = XliffNS + "source";
+        private static XName Target = XliffNS + "target";
+        private static XName Note = XliffNS + "note";
+
+        [Fact]
+        public void GetTargetValueOrDefault_ReturnsTargetWhenTargetIsPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "source text"),
+                    new XElement(Target, "target text"));
+
+            string targetValue = transUnitElement.GetTargetValueOrDefault();
+
+            Assert.Equal(expected: "target text", actual: targetValue);
+        }
+
+        [Fact]
+        public void GetTargetValueOrDefault_ReturnsEmptyStringWhenTargetIsEmpty()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "source text"),
+                    new XElement(Target, string.Empty));
+
+            string targetValue = transUnitElement.GetTargetValueOrDefault();
+
+            Assert.Equal(expected: string.Empty, actual: targetValue);
+        }
+
+        [Fact]
+        public void GetTargetValueOrDefault_ReturnsSourceWhenTargetIsMissing()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "source text"));
+
+            string targetValue = transUnitElement.GetTargetValueOrDefault();
+
+            Assert.Equal(expected: "source text", actual: targetValue);
+        }
+
+        [Fact]
+        public void SetTargetValue_SetsValueIfTargetIsPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "source text"),
+                    new XElement(Target, "original target text"),
+                    new XElement(Note));
+
+            transUnitElement.SetTargetValue("new target text");
+
+            Assert.Equal(expected: "new target text", actual: transUnitElement.Element(Target).Value);
+        }
+
+        [Fact]
+        public void SetTargetValue_TargetIsCreatedIfNotPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "source text"),
+                    new XElement(Note));
+
+            transUnitElement.SetTargetValue("new target text");
+
+            Assert.Equal(expected: "new target text", actual: transUnitElement.Element(Target).Value);
+            Assert.Equal(expected: transUnitElement.Element(Source), actual: transUnitElement.Element(Target).PreviousNode);
+        }
+
+        [Fact]
+        public void GetTargetState_ReturnsStateWhenStateIsPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Target,
+                        new XAttribute("state", "original state value")));
+
+            string stateValue = transUnitElement.GetTargetStateOrDefault();
+
+            Assert.Equal(expected: "original state value", actual: stateValue);
+        }
+
+        [Fact]
+        public void GetTargetState_ReturnsDefaultWhenTargetIsPresentButStateIsNot()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Target));
+
+            string stateValue = transUnitElement.GetTargetStateOrDefault();
+
+            Assert.Equal(expected: "new", actual: stateValue);
+        }
+
+        [Fact]
+        public void GetTargetState_ReturnsDefaultWhenTargetIsNotPresent()
+        {
+            XElement transUnitElement = new XElement(TransUnit);
+
+            string stateValue = transUnitElement.GetTargetStateOrDefault();
+
+            Assert.Equal(expected: "new", actual: stateValue);
+        }
+
+        [Fact]
+        public void SetTargetState_SetsStateWhenAlreadyPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "soruce text"),
+                    new XElement(Target,
+                        new XAttribute("state", "new"),
+                        "target text"));
+
+            transUnitElement.SetTargetState("translated");
+
+            Assert.Equal(expected: "translated", actual: transUnitElement.Element(Target).Attribute("state").Value);
+        }
+
+        [Fact]
+        public void SetTargetState_AddsStateAttributeIfNotPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "soruce text"),
+                    new XElement(Target, "target text"));
+
+            transUnitElement.SetTargetState("translated");
+
+            Assert.Equal(expected: "translated", actual: transUnitElement.Element(Target).Attribute("state").Value);
+        }
+
+        [Fact]
+        public void SetTargetState_AddsTargetElementIfNotPresent()
+        {
+            XElement transUnitElement =
+                new XElement(TransUnit,
+                    new XElement(Source, "soruce text"));
+
+            transUnitElement.SetTargetState("translated");
+
+            Assert.Equal(expected: "translated", actual: transUnitElement.Element(Target).Attribute("state").Value);
+        }
+    }
+}
---
+++ src/XliffTasks/Model/XElementExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XliffTasks.Model
+{
+    internal static class XElementExtensions
+    {
+        private static XNamespace XliffNS = "urn:oasis:names:tc:xliff:document:1.2";
+        private static XNamespace XsiNS = "http://www.w3.org/2001/XMLSchema-instance";
+        private static XName Xliff = XliffNS + "xliff";
+        private static XName File = XliffNS + "file";
+        private static XName Body = XliffNS + "body";
+        private static XName Group = XliffNS + "group";
+        private static XName TransUnit = XliffNS + "trans-unit";
+        private static XName Source = XliffNS + "source";
+        private static XName Target = XliffNS + "target";
+        private static XName Note = XliffNS + "note";
+
+        public static string GetTargetValueOrDefault(this XElement transUnitElement) => 
+            transUnitElement.Element(Target)?.Value
+            ?? transUnitElement.Element(Source).Value;
+
+        public static void SetTargetValue(this XElement transUnitElement, string value)
+        {
+            XElement targetElement = transUnitElement.Element(Target);
+            if (targetElement == null)
+            {
+                XElement sourceElement = transUnitElement.Element(Source);
+                targetElement = new XElement(Target);
+                sourceElement.AddAfterSelf(targetElement);
+            }
+
+            targetElement.Value = value;
+        }
+
+        public static string GetTargetStateOrDefault(this XElement transUnitElement) =>
+            transUnitElement.Element(Target)?.Attribute("state")?.Value
+            ?? "new";
+
+        public static void SetTargetState(this XElement transUnitElement, string value)
+        {
+            XElement targetElement = transUnitElement.Element(Target);
+            if (targetElement == null)
+            {
+                XElement sourceElement = transUnitElement.Element(Source);
+                targetElement = new XElement(Target);
+                sourceElement.AddAfterSelf(targetElement);
+            }
+
+            XAttribute stateAttribute = targetElement.Attribute("state");
+            if (stateAttribute == null)
+            {
+                stateAttribute = new XAttribute("state", value);
+                targetElement.Add(stateAttribute);
+            }
+            else
+            {
+                stateAttribute.Value = value;
+            }
+        }
+    }
+}
