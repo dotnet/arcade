92981c21c139a68394eb94c877c6536c713fb46a
Tomas Matousek <tomat@microsoft.com>
2019-02-03 17:04:01 -0800

    Add Microsoft.DotNet.Build.Tasks.VisualStudio package.
    Implements builds tasks necessary for consumption of IBC data generated by VS IBC training process and for generating inputs to this process.

---
+++ src/Microsoft.DotNet.Build.Tasks.VisualStudio/OptProf/Json/OptProfTrainingConfiguration.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Microsoft.DotNet.Build.Tasks.VisualStudio
+{
+    internal sealed class OptProfTrainingConfiguration
+    {
+        [JsonProperty(PropertyName = "products")]
+        public ProductOptProfTraining[] Products { get; set; }
+
+        [JsonProperty(PropertyName = "assemblies")]
+        public AssemblyOptProfTraining[] Assemblies { get; set; }
+
+        public static OptProfTrainingConfiguration Deserialize(string json)
+            => JsonSerializer.CreateDefault().Deserialize<OptProfTrainingConfiguration>(new StringReader(json));
+    }
+}
