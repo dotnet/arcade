9bcbae6f6461fddf17fa33bb972613504f4fcd19
Jon Fortescue <jofortes@microsoft.com>
2019-04-18 17:00:02 -0700

    Fix init, add run

--- src/Microsoft.DotNet.SDLAutomator/init-sdl.ps1
+++ src/Microsoft.DotNet.SDLAutomator/init-sdl.ps1
@@ -4,7 +4,7 @@ Param(
   [string] $dncengPat,
 )
 
-$encodedPat = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(":$dncengPat"))
+$encodedPat = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$dncengPat"))
 $uri = "https://dev.azure.com/dnceng/internal/_apis/git/repositories/sdl-tool-cfg/Items?path=$([Uri]::EscapeDataString("/$repository/.gdn"))&versionDescriptor[versionOptions]=0&`$format=zip&api-version=5.0-preview.1"
 $zipFile = "$sourcesDirectory/gdn.zip"
 Invoke-WebRequest -Headers @{ "Accept"="application/zip"; "Authorization"="Basic $encodedPat" } -Uri $uri -OutFile $zipFile
---
+++ src/Microsoft.DotNet.SDLAutomator/run-sdl.ps1
@@ -0,0 +1,3 @@
+Param(
+  [string] $toolsList
+)
