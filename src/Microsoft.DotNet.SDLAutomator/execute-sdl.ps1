6cc8c3f3ad79d5f8f873772f380724bdbbef6c38
Jon Fortescue <jofortes@microsoft.com>
2019-04-23 15:10:10 -0700

    Add TSA onboarding

--- src/Microsoft.DotNet.SDLAutomator/execute-sdl.ps1
+++ src/Microsoft.DotNet.SDLAutomator/execute-sdl.ps1
@@ -7,6 +7,11 @@ Param(
   [string[]] $SourceToolsList, # tools which run on source code
   [string[]] $ArtifactToolsList, # tools which run on built artifacts
   [bool] $UpdateBaseline=$False,
+  [bool] $TsaOnboard=$False,
+  [string] $TsaCodebaseName,
+  [string] $TsaNotificationEmail,
+  [string] $TsaCodebaseAdmin,
+  [string] $TsaBugAreaPath,
   [string] $GdnLoggerLevel="Standard"
 )
 
@@ -14,6 +19,19 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
 $gdnFolder = Invoke-Expression "$(Join-Path $PSScriptRoot "init-sdl.ps1") -GuardianCliLocation $GuardianCliLocation -Repository $Repository -WorkingDirectory $ArtifactsDirectory -DncEngAccessToken $DncEngAccessToken -GdnLoggerLevel $GdnLoggerLevel"
+
+if ($TsaOnboard) {
+  if ($TsaCodebaseName -and $TsaNotificationEmail -and $TsaCodebaseAdmin -and $TsaBugAreaPath) {
+    Write-Host "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $WorkingDirectory -logger-level $GdnLoggerLevel"
+    Invoke-Expression "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $WorkingDirectory -logger-level $GdnLoggerLevel"
+    if ($LASTEXITCODE -ne 0) {
+      Write-Error "Guardian tsa-onboard failed with exit code $LASTEXITCODE."
+    }
+  } else {
+    Write-Error "Could not onboard to TSA -- not all required values ($$TsaCodebaseName, $$TsaNotificationEmail, $$TsaCodebaseAdmin, $$TsaBugAreaPath) were specified."
+  }
+}
+
 Invoke-Expression "$(Join-Path $PSScriptRoot "run-sdl.ps1") -GuardianCliLocation $GuardianCliLocation -Repository $Repository -WorkingDirectory $ArtifactsDirectory -GdnFolder $gdnFolder -ToolList $ArtifactsToolsList -DncEngAccessToken $DncEngAccessToken -UpdateBaseline $UpdateBaseline -GdnLoggerLevel $GdnLoggerLevel"
 Copy-Item -Recurse -Force $gdnFolder $SourceDirectory
 $gdnFolder = Join-Path $SourceDirectory ".gdn"
