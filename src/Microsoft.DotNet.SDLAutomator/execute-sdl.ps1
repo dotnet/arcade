28d1d8f39766682eccf3ecca62513ef0839b8712
Jon Fortescue <jofortes@microsoft.com>
2019-04-23 15:57:49 -0700

    Add TSA publishing

--- src/Microsoft.DotNet.SDLAutomator/execute-sdl.ps1
+++ src/Microsoft.DotNet.SDLAutomator/execute-sdl.ps1
@@ -1,18 +1,21 @@
 Param(
-  [string] $GuardianCliLocation,            # Required: the path to the guardian CLI executable
-  [string] $Repository,                     # Required: the name of the repository (e.g. dotnet/arcade)
-  [string] $SourceDirectory,                # Required: the directory where source files are located
-  [string] $ArtifactsDirectory,             # Required: the directory where build artifacts are located
-  [string] $DncEngAccessToken,              # Required: access token for dnceng; should be provided via KeyVault
-  [string[]] $SourceToolsList,              # Required: list of SDL tools to run on source code
-  [string[]] $ArtifactToolsList,            # Required: list of SDL tools to run on built artifacts
-  [bool] $UpdateBaseline=$False,            # Optional: if true, will update the baseline in the repository; should only be run after fixing any issues which need to be fixed
-  [bool] $TsaOnboard=$False,                # Optional: if true, will onboard the repository to TSA; should only be run once
-  [string] $TsaCodebaseName,                # Optional: only needed if TsaOnboard is true; the name of the codebase registered with TSA
-  [string] $TsaNotificationEmail,           # Optional: only needed if TsaOnboard is true; the email(s) which will receive notifications of TSA bug filings (e.g. alias@microsoft.com)
-  [string] $TsaCodebaseAdmin,               # Optional: only needed if TsaOnboard is true; the aliases which are admins of the TSA codebase (e.g. DOMAIN\alias)
-  [string] $TsaBugAreaPath,                 # Optional: only needed if TsaOnboard is true; the area path where TSA will file bugs in AzDO
-  [string] $GuardianLoggerLevel="Standard"  # Optional: the logger level for the Guardian CLI; options are Trace, Verbose, Standard, Warning, and Error
+  [string] $GuardianCliLocation,                    # Required: the path to the guardian CLI executable
+  [string] $Repository,                             # Required: the name of the repository (e.g. dotnet/arcade)
+  [string] $SourceDirectory,                        # Required: the directory where source files are located
+  [string] $ArtifactsDirectory,                     # Required: the directory where build artifacts are located
+  [string] $DncEngAccessToken,                      # Required: access token for dnceng; should be provided via KeyVault
+  [string[]] $SourceToolsList,                      # Required: list of SDL tools to run on source code
+  [string[]] $ArtifactToolsList,                    # Required: list of SDL tools to run on built artifacts
+  [bool] $TsaPublish=$False,                        # Required: true will publish results to TSA; only set to true after onboarding to TSA
+  [string] $BranchName=$env:BUILD_SOURCEBRANCHNAME, # Optional: required for TSA publish; defaults to $(Build.SourceBranchName)
+  [string] $BuildNumber=$env:BUILD_BUILDNUMBER,     # Optional: required for TSA publish; defaults to $(Build.BuildNumber)
+  [bool] $UpdateBaseline=$False,                    # Optional: if true, will update the baseline in the repository; should only be run after fixing any issues which need to be fixed
+  [bool] $TsaOnboard=$False,                        # Optional: if true, will onboard the repository to TSA; should only be run once
+  [string] $TsaCodebaseName,                        # Optional: only needed if TsaOnboard is true; the name of the codebase registered with TSA
+  [string] $TsaNotificationEmail,                   # Optional: only needed if TsaOnboard is true; the email(s) which will receive notifications of TSA bug filings (e.g. alias@microsoft.com)
+  [string] $TsaCodebaseAdmin,                       # Optional: only needed if TsaOnboard is true; the aliases which are admins of the TSA codebase (e.g. DOMAIN\alias)
+  [string] $TsaBugAreaPath,                         # Optional: only needed if TsaOnboard is true; the area path where TSA will file bugs in AzDO
+  [string] $GuardianLoggerLevel="Standard"          # Optional: the logger level for the Guardian CLI; options are Trace, Verbose, Standard, Warning, and Error
 )
 
 $ErrorActionPreference = "Stop"
@@ -22,8 +25,8 @@ $gdnFolder = Invoke-Expression "$(Join-Path $PSScriptRoot "init-sdl.ps1") -Guard
 
 if ($TsaOnboard) {
   if ($TsaCodebaseName -and $TsaNotificationEmail -and $TsaCodebaseAdmin -and $TsaBugAreaPath) {
-    Write-Host "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $WorkingDirectory -logger-level $GuardianLoggerLevel"
-    Invoke-Expression "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $WorkingDirectory -logger-level $GuardianLoggerLevel"
+    Write-Host "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $ArtifactsDirectory --logger-level $GuardianLoggerLevel"
+    Invoke-Expression "$GuardianCliLocation tsa-onboard --codebase-name `"$TsaCodebaseName`" --notification-alias `"$TsaNotificationEmail`" --codebase-admin `"$TsaCodebaseAdmin`" --instance-url `"https://dev.azure.com/dnceng/`" --project-name `"internal`" --area-path `"$TsaBugAreaPath`" --working-directory $ArtifactsDirectory --logger-level $GuardianLoggerLevel"
     if ($LASTEXITCODE -ne 0) {
       Write-Error "Guardian tsa-onboard failed with exit code $LASTEXITCODE."
     }
@@ -36,3 +39,12 @@ Invoke-Expression "$(Join-Path $PSScriptRoot "run-sdl.ps1") -GuardianCliLocation
 Copy-Item -Recurse -Force $gdnFolder $SourceDirectory
 $gdnFolder = Join-Path $SourceDirectory ".gdn"
 Invoke-Expression "$(Join-Path $PSScriptRoot "run-sdl.ps1") -GuardianCliLocation $GuardianCliLocation -Repository $Repository -WorkingDirectory $SourceDirectory -GdnFolder $gdnFolder -ToolList $SourceToolsList -DncEngAccessToken $DncEngAccessToken -UpdateBaseline $UpdateBaseline -GuardianLoggerLevel $GuardianLoggerLevel"
+
+if ($TsaPublish) {
+  $TsaRepositoryName = $Repository.Replace("/", "-")
+  Write-Host "$GuardianCliLocation tsa-publish --all-tools --repository-name $TsaRepositoryName --branch-name $BranchName --build-number $BuildNumber --working-directory $SourceDirectory --logger-level $GuardianLoggerLevel"
+  Invoke-Expression "$GuardianCliLocation tsa-publish --all-tools --repository-name $TsaRepositoryName --branch-name $BranchName --build-number $BuildNumber --working-directory $SourceDirectory --logger-level $GuardianLoggerLevel"
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Guardian tsa-publish failed with exit code $LASTEXITCODE."
+  }
+}
