2120ef13d787cf43cec69ca063289539ccc60f95
Tomas Matousek <tomat@microsoft.com>
2019-04-24 17:34:09 -0700

    Generate props file with IBC input file URL

---
+++ src/Microsoft.DotNet.Build.Tasks.VisualStudio/OptProf/GenerateTrainingPropsFile.cs
@@ -0,0 +1,64 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.DotNet.Build.Tasks.VisualStudio
+{
+    /// <summary>
+    /// Generates a .props file pointing to a drops URL where IBC optimization inputs will be uploaded.
+    /// </summary>
+    public sealed class GenerateTrainingPropsFile : Task
+    {
+        private const string ProductDropNamePrefix = "Products/";
+
+        /// <summary>
+        /// GitHub repository name (e.g. 'dotnet/roslyn').
+        /// </summary>
+        [Required]
+        public string RepositoryName { get; set; }
+
+        /// <summary>
+        /// Product drop name, e.g. 'Products/$(System.TeamProject)/$(Build.Repository.Name)/$(Build.SourceBranchName)/$(Build.BuildNumber)'
+        /// </summary>
+        public string ProductDropName { get; set; }
+
+        /// <summary>
+        /// Directory to output the props file to.
+        /// </summary>
+        [Required]
+        public string OutputDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            ExecuteImpl();
+            return !Log.HasLoggedErrors;
+        }
+
+        private void ExecuteImpl()
+        {
+            if (ProductDropName != "" && !ProductDropName.StartsWith(ProductDropNamePrefix, StringComparison.Ordinal))
+            {
+                Log.LogError($"Invalid value of vsDropName argument: must start with '{ProductDropNamePrefix}'.");
+                return;
+            }
+
+            var dropName = ProductDropName?.Substring(ProductDropNamePrefix.Length) ?? "dummy";
+            var outputFilePath = Path.Combine(OutputDirectory, RepositoryName.Replace('/', '.') + ".props");
+
+            Directory.CreateDirectory(OutputDirectory);
+            File.WriteAllText(outputFilePath,
+$@"<?xml version=""1.0""?>
+<Project>
+  <ItemGroup>
+    <TestStore Include=""vstsdrop:ProfilingInputs/{dropName}"" />
+  </ItemGroup>
+</Project>", Encoding.UTF8);
+        }
+    }
+}
