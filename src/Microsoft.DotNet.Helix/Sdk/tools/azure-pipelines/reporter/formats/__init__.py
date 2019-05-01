3700e8d5b03e2f647b325e524fb2ad958f6014ae
Alex Perovich <alperovi@microsoft.com>
2019-05-01 16:17:42 -0700

    use relative imports

--- src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/formats/__init__.py
+++ src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/formats/__init__.py
@@ -1,8 +1,8 @@
 from typing import List
-from result_format import ResultFormat
-from xunit import XUnitFormat
-from junit import JUnitFormat
-from trx import TRXFormat
+from .result_format import ResultFormat
+from .xunit import XUnitFormat
+from .junit import JUnitFormat
+from .trx import TRXFormat
 
 
 all_formats = [
