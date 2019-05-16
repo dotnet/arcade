503e8c672ffeac380cd13b84ceefc8daff5d0879
Jon Fortescue <jofortes@microsoft.com>
2019-05-16 15:55:59 -0700

    Fix up TSA publish

--- eng/common/sdl/execute-all-sdl-tools.ps1
+++ eng/common/sdl/execute-all-sdl-tools.ps1
@@ -1,22 +1,24 @@
 Param(
-  [string] $GuardianCliLocation,                    # Required: the path to the guardian CLI executable
-  [string] $Repository,                             # Required: the name of the repository (e.g. dotnet/arcade)
-  [string] $BranchName="master",                    # Optional: name of branch or version of gdn settings; defaults to master
-  [string] $SourceDirectory,                        # Required: the directory where source files are located
-  [string] $ArtifactsDirectory,                     # Required: the directory where build artifacts are located
-  [string] $DncEngAccessToken,                      # Required: access token for dnceng; should be provided via KeyVault
-  [string[]] $SourceToolsList,                      # Optional: list of SDL tools to run on source code
-  [string[]] $ArtifactToolsList,                    # Optional: list of SDL tools to run on built artifacts
-  [bool] $TsaPublish=$False,                        # Optional: true will publish results to TSA; only set to true after onboarding to TSA
-  [string] $TsaBranchName=$env:BUILD_SOURCEBRANCHNAME, # Optional: required for TSA publish; defaults to $(Build.SourceBranchName)
-  [string] $BuildNumber=$env:BUILD_BUILDNUMBER,     # Optional: required for TSA publish; defaults to $(Build.BuildNumber)
-  [bool] $UpdateBaseline=$False,                    # Optional: if true, will update the baseline in the repository; should only be run after fixing any issues which need to be fixed
-  [bool] $TsaOnboard=$False,                        # Optional: if true, will onboard the repository to TSA; should only be run once
-  [string] $TsaCodebaseName,                        # Optional: only needed if TsaOnboard is true; the name of the codebase registered with TSA
-  [string] $TsaNotificationEmail,                   # Optional: only needed if TsaOnboard is true; the email(s) which will receive notifications of TSA bug filings (e.g. alias@microsoft.com)
-  [string] $TsaCodebaseAdmin,                       # Optional: only needed if TsaOnboard is true; the aliases which are admins of the TSA codebase (e.g. DOMAIN\alias)
-  [string] $TsaBugAreaPath,                         # Optional: only needed if TsaOnboard is true; the area path where TSA will file bugs in AzDO
-  [string] $GuardianLoggerLevel="Standard"          # Optional: the logger level for the Guardian CLI; options are Trace, Verbose, Standard, Warning, and Error
+  [string] $GuardianCliLocation,                        # Required: the path to the guardian CLI executable
+  [string] $Repository,                                 # Required: the name of the repository (e.g. dotnet/arcade)
+  [string] $BranchName="master",                        # Optional: name of branch or version of gdn settings; defaults to master
+  [string] $SourceDirectory,                            # Required: the directory where source files are located
+  [string] $ArtifactsDirectory,                         # Required: the directory where build artifacts are located
+  [string] $DncEngAccessToken,                          # Required: access token for dnceng; should be provided via KeyVault
+  [string[]] $SourceToolsList,                          # Optional: list of SDL tools to run on source code
+  [string[]] $ArtifactToolsList,                        # Optional: list of SDL tools to run on built artifacts
+  [bool] $TsaPublish=$False,                            # Optional: true will publish results to TSA; only set to true after onboarding to TSA
+  [string] $TsaBranchName=$env:BUILD_SOURCEBRANCHNAME,  # Optional: required for TSA publish; defaults to $(Build.SourceBranchName)
+  [string] $TsaRepositoryName,                          # Optional: TSA repository name; will be generated automatically if not submitted
+  [string] $BuildNumber=$env:BUILD_BUILDNUMBER,         # Optional: required for TSA publish; defaults to $(Build.BuildNumber)
+  [bool] $UpdateBaseline=$False,                        # Optional: if true, will update the baseline in the repository; should only be run after fixing any issues which need to be fixed
+  [bool] $TsaOnboard=$False,                            # Optional: if true, will onboard the repository to TSA; should only be run once
+  [string] $TsaCodebaseName,                            # Optional: only needed if TsaOnboard or TsaPublish is true; the name of the codebase registered with TSA
+  [string] $TsaProjectName,                             # Optional: only needed if TsaOnboard or TsaPublish is true; the name of the project registered with TSA
+  [string] $TsaNotificationEmail,                       # Optional: only needed if TsaOnboard is true; the email(s) which will receive notifications of TSA bug filings (e.g. alias@microsoft.com)
+  [string] $TsaCodebaseAdmin,                           # Optional: only needed if TsaOnboard is true; the aliases which are admins of the TSA codebase (e.g. DOMAIN\alias)
+  [string] $TsaBugAreaPath,                             # Optional: only needed if TsaOnboard is true; the area path where TSA will file bugs in AzDO
+  [string] $GuardianLoggerLevel="Standard"              # Optional: the logger level for the Guardian CLI; options are Trace, Verbose, Standard, Warning, and Error
 )
 
 $ErrorActionPreference = "Stop"
@@ -50,9 +52,11 @@ if ($UpdateBaseline) {
 
 if ($TsaPublish) {
   if ($TsaBranchName -and $BuildNumber) {
-    $TsaRepositoryName = "$($Repository.Replace("/", "-")-$BranchName)"
-    Write-Host "$GuardianCliLocation tsa-publish --all-tools --repository-name $TsaRepositoryName --branch-name $TsaBranchName --build-number $BuildNumber --working-directory $SourceDirectory --logger-level $GuardianLoggerLevel"
-    &$GuardianCliLocation tsa-publish --all-tools --repository-name $TsaRepositoryName --branch-name $TsaBranchName --build-number $BuildNumber --working-directory $SourceDirectory --logger-level $GuardianLoggerLevel
+    if (-not $TsaRepositoryName) {
+      $TsaRepositoryName = "$($Repository.Replace("/", "-")-$BranchName)"
+    }
+    Write-Host "$GuardianCliLocation tsa-publish --all-tools --repository-name `"$TsaRepositoryName`" --branch-name `"$TsaBranchName`" --build-number `"$BuildNumber`" --working-directory $SourceDirectory --logger-level $GuardianLoggerLevel"
+    &$GuardianCliLocation tsa-publish --all-tools --repository-name "$TsaRepositoryName" --branch-name "$TsaBranchName" --build-number "$BuildNumber" --codebase-name "$TsaCodebaseName" --project-name "$TsaProjectName" --working-directory $SourceDirectory  --logger-level $GuardianLoggerLevel
     if ($LASTEXITCODE -ne 0) {
       Write-Error "Guardian tsa-publish failed with exit code $LASTEXITCODE."
     }
