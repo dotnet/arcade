f432875fc113cb68d00f1a472265404fb41da6ce
Alex Perovich <alperovi@microsoft.com>
2019-06-07 15:08:40 -0700

    Put Flaky test support behind a flag

--- src/Microsoft.DotNet.Helix/Sdk/CheckAzurePipelinesTestResults.cs
+++ src/Microsoft.DotNet.Helix/Sdk/CheckAzurePipelinesTestResults.cs
@@ -12,23 +12,70 @@ namespace Microsoft.DotNet.Helix.AzureDevOps
     {
         public int[] TestRunIds { get; set; }
 
-        public string DisableFlakyTestSupport { get; set; }
-
         public ITaskItem[] ExpectedTestFailures { get; set; }
+
+        public string EnableFlakyTestSupport { get; set; }
 
         protected override async Task ExecuteCoreAsync(HttpClient client)
         {
-            if (ExpectedTestFailures?.Length > 0 || !string.IsNullOrEmpty(DisableFlakyTestSupport))
+            if (ExpectedTestFailures?.Length > 0)
             {
                 await ValidateExpectedTestFailuresAsync(client);
                 return;
             }
+
+            if (!string.IsNullOrEmpty(EnableFlakyTestSupport))
+            {
+                await CheckTestResultsWithFlakySupport(client);
+                return;
+            }
+
+
+            await CheckTestResultsAsync(client);
+        }
+
+        private async Task CheckTestResultsAsync(HttpClient client)
+        {
+            foreach (var testRunId in TestRunIds)
+            {
+                var data = await RetryAsync(
+                    async () =>
+                    {
+                        using (var req = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"{CollectionUri}{TeamProject}/_apis/test/runs/{testRunId}?api-version=5.0"))
+                        {
+                            using (var res = await client.SendAsync(req))
+                            {
+                                return await ParseResponseAsync(req, res);
+                            }
+                        }
+                    });
+                if (data != null && data["runStatistics"] is JArray runStatistics)
+                {
+                    var failed = runStatistics.Children()
+                        .FirstOrDefault(stat => stat["outcome"]?.ToString() == "Failed");
+                    if (failed != null)
+                    {
+                        Log.LogError($"Test run {testRunId} has one or more failing tests.");
+                    }
+                    else
+                    {
+                        Log.LogMessage(MessageImportance.Low, $"Test run {testRunId} has not failed.");
+                    }
+                }
+            }
+        }
+
+        private async Task CheckTestResultsWithFlakySupport(HttpClient client)
+        {
             JObject data = await RetryAsync(
                 async () =>
                 {
                     using (var req = new HttpRequestMessage(
                         HttpMethod.Get,
-                        $"{CollectionUri}{TeamProject}/_apis/test/resultsummarybybuild?buildId={BuildId}&api-version=5.1-preview.2"))
+                        $"{CollectionUri}{TeamProject}/_apis/test/resultsummarybybuild?buildId={BuildId}&api-version=5.1-preview.2")
+                    )
                     {
                         using (HttpResponseMessage res = await client.SendAsync(req))
                         {
@@ -42,7 +89,7 @@ namespace Microsoft.DotNet.Helix.AzureDevOps
                 foreach (JProperty property in resultsByOutcome.Properties())
                 {
                     string outcome = property.Name.ToLowerInvariant();
-                    var outcomeResults = (JObject)property.Value;
+                    var outcomeResults = (JObject) property.Value;
                     int count = outcomeResults["count"].ToObject<int>();
                     var message = $"Build has {count} {outcome} tests.";
                     if (outcome == "failed")
