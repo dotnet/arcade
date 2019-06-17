816da75bcda5d47814ad5af682be10763e3735c0
juanam <juanam@microsoft.com>
2019-06-17 16:44:00 -0700

    Adding comments

--- eng/common/internal-feed-operations.ps1
+++ eng/common/internal-feed-operations.ps1
@@ -11,6 +11,10 @@ Set-StrictMode -Version 2.0
 
 . $PSScriptRoot\tools.ps1
 
+# Sets VSS_NUGET_EXTERNAL_FEED_ENDPOINTS based on the "darc-int-*" feeds defined in NuGet.config. This is needed by
+# in build agents by CredProvider to authenticate the restore requests to internal feeds as specified in
+# https://github.com/microsoft/artifacts-credprovider#environment-variables. This should ONLY be called from identified
+# internal builds
 function SetupCredProvider{
   param(
     [string] $AuthToken
