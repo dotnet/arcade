4d8ba0aff783529798078c7430a8d4ef882b164e
Alex Perovich <alperovi@microsoft.com>
2019-07-01 10:33:37 -0700

    Ensure vsts is installed even if the env was already there

--- src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.bat
+++ src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.bat
@@ -4,8 +4,9 @@ echo  %date%-%time%
 
 if NOT EXIST %ENV_PATH%\Scripts\python.exe (
   %HELIX_PYTHONPATH% -m virtualenv --no-site-packages %ENV_PATH%
-  %ENV_PATH%\Scripts\python.exe -m pip install vsts==0.1.20
 )
+
+%ENV_PATH%\Scripts\python.exe -c "import vsts" || %ENV_PATH%\Scripts\python.exe -m pip install vsts==0.1.20
 
 %ENV_PATH%\Scripts\python.exe -c "import future" || %ENV_PATH%\Scripts\python.exe -m pip install future==0.17.1
 
