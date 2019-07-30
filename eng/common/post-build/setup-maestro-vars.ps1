d3a43a80eef85d216d5a8c879c537774c0b060c3
Cesar Soares Lucas <disoares@microsoft.com>
2019-07-29 17:31:38 -0700

    Refactor logic around IsStableBuild variable

--- eng/common/post-build/setup-maestro-vars.ps1
+++ eng/common/post-build/setup-maestro-vars.ps1
@@ -11,12 +11,9 @@ try {
   
   $Channels = ""            
   $Content | Select -Index 1 | ForEach-Object { $Channels += "$_ ," }
-  
-  $IsStableBuild = $Content | Select -Index 2
 
   Write-PipelineSetVariable -Name 'BARBuildId' -Value $BarId
   Write-PipelineSetVariable -Name 'InitialChannels' -Value "$Channels"
-  Write-PipelineSetVariable -Name 'IsStableBuild' -Value $IsStableBuild
 }
 catch {
   Write-Host $_
