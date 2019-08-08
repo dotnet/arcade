ede68581b29177fa3d24ec052b0d05cc9216431f
Jacques Eloff <joeloff@microsoft.com>
2019-08-07 20:08:59 -0700

    SignCheck: Ensure container contents is checked when the container is excluded

--- src/SignCheck/Microsoft.SignCheck/Verification/SignatureVerificationManager.cs
+++ src/SignCheck/Microsoft.SignCheck/Verification/SignatureVerificationManager.cs
@@ -115,25 +115,18 @@ namespace Microsoft.SignCheck.Verification
         {
             foreach (string file in files)
             {
-                // If the file is excluded add a default result
-                if (Exclusions.IsExcluded(file, parent: null, containerPath: null))
-                {
-                    var result = SignatureVerificationResult.ExcludedFileResult(file, parent: null);
-                    Results.Add(result);
-                }
-                else
-                {
-                    FileVerifier fileVerifier = GetFileVerifier(file);
-                    SignatureVerificationResult result = fileVerifier.VerifySignature(file, parent: null);
+                FileVerifier fileVerifier = GetFileVerifier(file);
+                SignatureVerificationResult result;
+                result = fileVerifier.VerifySignature(file, parent: null);
 
-                    if ((Options & SignatureVerificationOptions.GenerateExclusion) == SignatureVerificationOptions.GenerateExclusion)
-                    {
-                        result.ExclusionEntry = String.Join(";", String.Join("|", file, String.Empty), String.Empty, String.Empty);
-                        Log.WriteMessage(LogVerbosity.Diagnostic, SignCheckResources.DiagGenerateExclusion, result.Filename, result.ExclusionEntry);
-                    }
-
-                    Results.Add(result);
+                if ((Options & SignatureVerificationOptions.GenerateExclusion) == SignatureVerificationOptions.GenerateExclusion)
+                {
+                    result.ExclusionEntry = String.Join(";", String.Join("|", file, String.Empty), String.Empty, String.Empty);
+                    Log.WriteMessage(LogVerbosity.Diagnostic, SignCheckResources.DiagGenerateExclusion, result.Filename, result.ExclusionEntry);
                 }
+
+                result.IsExcluded = Exclusions.IsExcluded(file, parent: null, containerPath: null);
+                Results.Add(result);
             }
 
             return Results;
