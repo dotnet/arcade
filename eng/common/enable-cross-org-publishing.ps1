f5e600497536cfcee7db2a315cc446e9031394fd
juanam <jcagme@gmail.com>
2019-09-17 16:10:08 -0700

    Go back to PS1 and remove issecret

--- eng/common/enable-cross-org-publishing.ps1
+++ eng/common/enable-cross-org-publishing.ps1
@@ -2,5 +2,5 @@ param(
   [string] $token
 )
 
-Write-Host "##vso[task.setvariable variable=VSS_NUGET_ACCESSTOKEN;issecret=true;]$token"
+Write-Host "##vso[task.setvariable variable=VSS_NUGET_ACCESSTOKEN]$token"
 Write-Host "##vso[task.setvariable variable=VSS_NUGET_URI_PREFIXES]https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/"
