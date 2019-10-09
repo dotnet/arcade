f9aa63e0dafc2760afe5ecbaf631e52db8c93f13
Jacques Eloff <joeloff@users.noreply.github.com>
2019-10-09 15:33:05 -0700

    SignCheck fixes (#4058) (#4083)
    * Add support for files that should never be signed
    * nupkg repository signature checks
    * Fix handling external CAB files in containers to ensure both the CAB + MSI are extracted before verifying the MSI

--- src/SignCheck/Microsoft.SignCheck/Verification/NupkgVerifier.cs
+++ src/SignCheck/Microsoft.SignCheck/Verification/NupkgVerifier.cs
@@ -2,22 +2,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SignCheck.Logging;
 using NuGet.Common;
 using NuGet.Packaging;
 using NuGet.Packaging.Signing;
-using Cryptography = System.Security.Cryptography;
 
 namespace Microsoft.SignCheck.Verification
 {
-    public class NupkgVerifier : FileVerifier
+    public class NupkgVerifier : ArchiveVerifier
     {
-        private static readonly string _fingerPrint = "3F9001EA83C560D712C24CF213C3D312CB3BFF51EE89435D3430BD06B5D0EECE";
+        private static List<CertificateHashAllowListEntry> AllowListEntries = new List<CertificateHashAllowListEntry>()
+        {
+            new CertificateHashAllowListEntry(VerificationTarget.Author | VerificationTarget.Repository, SignaturePlacement.PrimarySignature, "3F9001EA83C560D712C24CF213C3D312CB3BFF51EE89435D3430BD06B5D0EECE", HashAlgorithmName.SHA256),
+            new CertificateHashAllowListEntry(VerificationTarget.Author | VerificationTarget.Repository, SignaturePlacement.PrimarySignature, "0E5F38F57DC1BCC806D8494F4F90FBCEDD988B46760709CBEEC6F4219AA6157D", HashAlgorithmName.SHA256)
+        };
 
         public NupkgVerifier(Log log, Exclusions exclusions, SignatureVerificationOptions options) : base(log, exclusions, options, fileExtension: ".nupkg")
         {
@@ -31,34 +33,7 @@ namespace Microsoft.SignCheck.Verification
 
             svr.IsSigned = IsSigned(fullPath);
             svr.AddDetail(DetailKeys.File, SignCheckResources.DetailSigned, svr.IsSigned);
-
-            if (VerifyRecursive)
-            {
-                using (ZipArchive zipArchive = ZipFile.OpenRead(fullPath))
-                {
-                    string tempPath = svr.TempPath;
-                    CreateDirectory(tempPath);
-                    Log.WriteMessage(LogVerbosity.Diagnostic, SignCheckResources.DiagExtractingFileContents, tempPath);
-
-                    foreach (ZipArchiveEntry archiveEntry in zipArchive.Entries)
-                    {
-                        // Generate an alias for the actual file that has the same extension. We do this to avoid path too long errors so that
-                        // containers can be flattened
-                        string aliasFileName = Utils.GetHash(archiveEntry.FullName, Cryptography.HashAlgorithmName.MD5.Name) + Path.GetExtension(archiveEntry.FullName);
-                        string aliasFullName = Path.Combine(tempPath, aliasFileName);
-
-                        archiveEntry.ExtractToFile(aliasFullName);
-                        SignatureVerificationResult archiveEntryResult = VerifyFile(aliasFullName, svr.Filename, archiveEntry.FullName);
-
-                        // Tag the full path into the result detail
-                        archiveEntryResult.AddDetail(DetailKeys.File, SignCheckResources.DetailFullName, archiveEntry.FullName);
-
-                        svr.NestedResults.Add(archiveEntryResult);
-                    }
-
-                    DeleteDirectory(tempPath);
-                }
-            }
+            VerifyContent(svr);
 
             return svr;
         }
@@ -68,11 +43,7 @@ namespace Microsoft.SignCheck.Verification
             IEnumerable<ISignatureVerificationProvider> providers = SignatureVerificationProviderFactory.GetSignatureVerificationProviders();
             var packageSignatureVerifier = new PackageSignatureVerifier(providers);
 
-            var allowListEntries = new List<CertificateHashAllowListEntry>();
-            allowListEntries.Add(new CertificateHashAllowListEntry(VerificationTarget.Author | VerificationTarget.Repository,
-                SignaturePlacement.PrimarySignature, _fingerPrint, HashAlgorithmName.SHA256));
-
-            var verifierSettings = SignedPackageVerifierSettings.GetVerifyCommandDefaultPolicy(clientAllowListEntries: allowListEntries);
+            var verifierSettings = SignedPackageVerifierSettings.GetVerifyCommandDefaultPolicy(clientAllowListEntries: AllowListEntries);
             IEnumerable<ISignatureVerificationProvider> verificationProviders = SignatureVerificationProviderFactory.GetSignatureVerificationProviders();
             var verifier = new PackageSignatureVerifier(verificationProviders);
 
