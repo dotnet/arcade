c09f2607924a9e9396787d5879370400f5fc180a
mmitche <mmitche@microsoft.com>
2019-11-05 13:38:24 -0800

    Make restarting the script more efficient

--- scripts/duplicate-feed.ps1
+++ scripts/duplicate-feed.ps1
@@ -30,6 +30,41 @@ param (
     [switch]$UseKeyVault
 )
 
+function Parse-Feed-Uri($inputFeedUri) {
+    if (-not $($inputFeedUri -match "https://pkgs.dev.azure.com/(?<account>[a-zA-Z0-9]+)/(?<visibility>[a-zA-Z0-9-]+/)?_packaging/(?<feed>.+)/nuget/v3/index.json")) {
+        Write-Error "$inputFeedUri is not in the expected format."
+        exit
+    }
+    return @{
+        account = $Matches["account"];
+        visibility = $Matches["visibility"];
+        feed = $Matches["feed"];
+    }
+}
+
+function Get-Package-List($vstsAuthHeader, $account, $visibility, $feed) {
+    # Get the list of packages from the source and target
+    $listOfPackages = @()
+    try {
+        $packageListUri = "https://feeds.dev.azure.com/$account/${visibility}_apis/packaging/Feeds/$feed/packages?api-version=5.1-preview.1"
+        Write-Host "Looking up packages on feed at: $packageListUri"
+        $result = Invoke-WebRequest -Headers $vstsAuthHeader $packageListUri
+        $resultJson = $result | ConvertFrom-Json
+        Write-Host "Feed $SourceFeedUri has $($resultJson.count) packages"
+        foreach ($package in $resultJson.value) {
+            $versionsResult = Invoke-WebRequest -Headers $vstsAuthHeader $package._links.versions.href
+            $versionsResultJson = $versionsResult | ConvertFrom-Json
+            foreach ($version in $versionsResultJson.value) {
+                $listOfPackages += @{ name = $package.name; version = $version.version }
+            }
+        }
+    } catch {
+        Write-Error $_
+        exit
+    }
+    $listOfPackages
+}
+
 if ($UseKeyVault) {
     try {
         Write-Output "Obtaining required secrets from keyvault"
@@ -53,37 +88,15 @@ if ($UseKeyVault) {
 $base64authinfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$AzDOPat"))
 $vstsAuthHeader = @{"Authorization"="Basic $base64authinfo"}
 
-if (-not $($SourceFeedUri -match "https://pkgs.dev.azure.com/(?<account>[a-zA-Z0-9]+)/(?<visibility>[a-zA-Z0-9-]+/)?_packaging/(?<feed>.+)/nuget/v3/index.json")) {
-    Write-Error "$SourceFeedUri is not in the expected format."
-    exit
-}
-$sourceFeedAccount = $Matches["account"];
-$sourceFeedVisibility = $Matches["visibility"];
-$sourceFeedName = $Matches["feed"];
+$sourceFeedInfo = Parse-Feed-Uri $SourceFeedUri
+$targetFeedInfo = Parse-Feed-Uri $TargetFeedUri
 
-# Get the list of packages from the source
-$listOfPackages = @()
-try {
-    $packageListUri = "https://feeds.dev.azure.com/$sourceFeedAccount/${sourceFeedVisibility}_apis/packaging/Feeds/$sourceFeedName/packages?api-version=5.1-preview.1"
-    Write-Host "Looking up packages on feed at: $packageListUri"
-    $result = Invoke-WebRequest -Headers $vstsAuthHeader $packageListUri
-    $resultJson = $result | ConvertFrom-Json
-    Write-Host "Feed $SourceFeedUri has $($resultJson.count) packages"
-    foreach ($package in $resultJson.value) {
-        $versionsResult = Invoke-WebRequest -Headers $vstsAuthHeader $package._links.versions.href
-        $versionsResultJson = $versionsResult | ConvertFrom-Json
-        foreach ($version in $versionsResultJson.value) {
-            $listOfPackages += @{ name = $package.name; version = $version.version }
-        }
-    }
-} catch {
-    Write-Error $_
-    exit
-}
+$listOfSourcePackages = Get-Package-List $vstsAuthHeader $sourceFeedInfo.account $sourceFeedInfo.visibility $sourceFeedInfo.feed
+$listOfTargetPackages = Get-Package-List $vstsAuthHeader $targetFeedInfo.account $targetFeedInfo.visibility $targetFeedInfo.feed
 
-Write-Host "Copying:"
-foreach ($packageToCopy in $listOfPackages) {
-    Write-Host "  $($packageToCopy.name) @ $($packageToCopy.version)"
+$targetPackageSet = @{}
+foreach ($package in $listOfTargetPackages) {
+    $targetPackageSet.Add("$($package.name)@$($package.version)", $true)
 }
 
 # Download all packages
@@ -91,9 +104,16 @@ $downloadRootBase = $([System.IO.Path]::GetTempPath())
 $downloadRoot = Join-Path -Path $downloadRootBase -ChildPath $([System.IO.Path]::GetRandomFileName())
 New-Item -Path $downloadRoot -ItemType Directory | Out-Null
 
-foreach ($packageToCopy in $listOfPackages) {
+foreach ($packageToCopy in $listOfSourcePackages) {
+    if ($targetPackageSet.Contains("$($packageToCopy.name)@$($packageToCopy.version)")) {
+        Write-Host "Skipping (already exists): $($packageToCopy.name) @ $($packageToCopy.version)"
+        continue
+    } else {
+        Write-Host "Copying: $($packageToCopy.name) @ $($packageToCopy.version)"
+    }
+    
     try {
-        $packageContentUrl = "https://pkgs.dev.azure.com/$sourceFeedAccount/${sourceFeedVisibility}_apis/packaging/feeds/$sourceFeedName/nuget/packages/$($packageToCopy.name)/versions/$($packageToCopy.version)/content";
+        $packageContentUrl = "https://pkgs.dev.azure.com/$($sourceFeedInfo.account)/$($sourceFeedInfo.visibility)_apis/packaging/feeds/$($sourceFeedInfo.feed)/nuget/packages/$($packageToCopy.name)/versions/$($packageToCopy.version)/content";
         Write-Host "Downloading package $($packageToCopy.name) @ $($packageToCopy.version) from $packageContentUrl"
         $localPackagePath = Join-Path -Path $downloadRoot -ChildPath "$($packageToCopy.name).$($packageToCopy.version).nupkg"
         Invoke-WebRequest -Headers $vstsAuthHeader $packageContentUrl -OutFile $localPackagePath
