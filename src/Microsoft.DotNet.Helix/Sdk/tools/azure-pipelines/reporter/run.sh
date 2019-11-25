30e945d1fd70924da2eee3ad3dd6b23a72893d12
Ulises Hernandez <ulisesh@microsoft.com>
2019-11-25 09:54:18 -0800

    Fix AzDo results reporter permissions issue (#4403)
    * Use sudo in all python calls
    
    * Use sudo -E

--- src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.sh
+++ src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.sh
@@ -14,18 +14,18 @@ if [ ! -f $ENV_PATH/bin/python ]; then
   mv -T $TMP_ENV_PATH $ENV_PATH
 fi
 
-if $ENV_PATH/bin/python -c "import azure.devops"; then
+if sudo $ENV_PATH/bin/python -c "import azure.devops"; then
   echo "azure-devops module already available"
 else
   sudo $ENV_PATH/bin/python -m pip install azure-devops==5.0.0b9
 fi
 
-if $ENV_PATH/bin/python -c "import future"; then
+if sudo $ENV_PATH/bin/python -c "import future"; then
   echo "future module already available"
 else
   sudo $ENV_PATH/bin/python -m pip install future==0.17.1
 fi
 
 date -u +"%FT%TZ"
-$ENV_PATH/bin/python $script_path/run.py "$@"
+sudo -E $ENV_PATH/bin/python $script_path/run.py "$@"
 date -u +"%FT%TZ"
