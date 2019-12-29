a08cec99e19b349b19efb3c3225792a806267a74
Tomas Matousek <tomat@microsoft.com>
2019-12-29 13:22:35 +0100

    Fix

--- src/Microsoft.DotNet.Arcade.Sdk.Tests/GenerateResxSourceTests.cs
+++ src/Microsoft.DotNet.Arcade.Sdk.Tests/GenerateResxSourceTests.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-<<<<<<< HEAD
 using System;
 using System.IO;
 using Microsoft.DotNet.Arcade.Sdk.Tests.Utilities;
