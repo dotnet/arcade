11e7e8fd6582dd3199c2fe57fbe0aa824acbb413
Anipik <anirudhagnihotry098@gmail.com>
2020-01-02 15:30:09 -0800

    working model

--- src/Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk/src/ChooseBestP2PTargetFrameworkTask.cs
+++ src/Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk/src/ChooseBestP2PTargetFrameworkTask.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Build.Framework;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk
 {
@@ -21,6 +23,9 @@ namespace Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk
         [Output]
         public ITaskItem[] AnnotatedProjectReferencesWithSetTargetFramework { get; set; }
 
+        [Required]
+        public bool BuildAllConfigurations { get; set; }
+
         public override bool Execute()
         {
             AnnotatedProjectReferencesWithSetTargetFramework = new ITaskItem[ProjectReferencesWithTargetFrameworks.Length];
@@ -29,17 +34,39 @@ namespace Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk
             for (int i = 0; i < ProjectReferencesWithTargetFrameworks.Length; i++)
             {
                 ITaskItem projectReference = ProjectReferencesWithTargetFrameworks[i];
-                string targetFrameworks = projectReference.GetMetadata("TargetFrameworks");
-                string bestTargetFramework = targetFrameworkResolver.GetBestSupportedTargetFramework(targetFrameworks.Split(';'), TargetFrameworkOSGroup);
+                string[] targetFrameworks = projectReference.GetMetadata("TargetFrameworks").Split(';');
+                bool refProject = projectReference.ItemSpec.Contains(@"\ref\");
+
+                string bestTargetFramework = targetFrameworkResolver.GetBestSupportedTargetFramework(targetFrameworks, TargetFrameworkOSGroup);
                 if (bestTargetFramework == null)
                 {
-                    Log.LogError($"Not able to find a compatible supported target framework for {TargetFrameworkOSGroup} in Project {Path.GetFileName(projectReference.ItemSpec)}. The Supported Configurations are {targetFrameworks}");
+                    if (refProject)
+                    {                        
+                        bestTargetFramework = targetFrameworkResolver.GetBestSupportedTargetFramework(targetFrameworks, TargetFrameworkOSGroup.Split('-')[0]);                    
+                    }
+                    else if (TargetFrameworkOSGroup.Contains("OSX") || TargetFrameworkOSGroup.Contains("Linux") || TargetFrameworkOSGroup.Contains("FreeBSD"))
+                    {
+                        // This is a temporary fix and this needs to be resolved.
+                        bestTargetFramework = targetFrameworkResolver.GetBestSupportedTargetFramework(targetFrameworks, TargetFrameworkOSGroup.Split('-')[0] + "-Unix");
+                    }
                 }
-                projectReference.SetMetadata("SetTargetFramework", "TargetFramework=" + bestTargetFramework.Split('-')[0]);
+                if (bestTargetFramework == null)
+                {
+                    Log.LogError($"Not able to find a compatible supported target framework for {TargetFrameworkOSGroup} in Project {Path.GetFileName(projectReference.ItemSpec)}. The Supported Configurations are {string.Join(", ", targetFrameworks)}");
+                }
+
+                if (!BuildAllConfigurations || refProject)
+                {
+                    projectReference.SetMetadata("SetTargetFramework", "TargetFramework=" + bestTargetFramework.Split('-')[0]);
+                }
+                else
+                {
+                    projectReference.SetMetadata("SetTargetFramework", "TargetFramework=" + bestTargetFramework);
+                }
                 AnnotatedProjectReferencesWithSetTargetFramework[i] = projectReference;
             }
 
             return !Log.HasLoggedErrors;
-        }
+        }        
     }
 }
