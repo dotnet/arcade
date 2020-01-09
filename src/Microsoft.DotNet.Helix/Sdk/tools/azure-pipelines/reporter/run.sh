3233b1809f3ba7c5802eac4ff14a5b573fcbfb11
Alex Perovich <alperovi@microsoft.com>
2020-01-09 13:32:03 -0800

    `sudo -E` the whole reporter script

--- src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.sh
+++ src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sudo -E /bin/sh
 set -x
 
 script_path=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
@@ -14,18 +14,18 @@ if [ ! -f $ENV_PATH/bin/python ]; then
   mv -T $TMP_ENV_PATH $ENV_PATH
 fi
 
-if sudo $ENV_PATH/bin/python -c "import azure.devops"; then
+if $ENV_PATH/bin/python -c "import azure.devops"; then
   echo "azure-devops module already available"
 else
-  sudo $ENV_PATH/bin/python -m pip install azure-devops==5.0.0b9
+  $ENV_PATH/bin/python -m pip install azure-devops==5.0.0b9
 fi
 
-if sudo $ENV_PATH/bin/python -c "import future"; then
+if $ENV_PATH/bin/python -c "import future"; then
   echo "future module already available"
 else
-  sudo $ENV_PATH/bin/python -m pip install future==0.17.1
+  $ENV_PATH/bin/python -m pip install future==0.17.1
 fi
 
 date -u +"%FT%TZ"
-sudo -E $ENV_PATH/bin/python $script_path/run.py "$@"
+$ENV_PATH/bin/python $script_path/run.py "$@"
 date -u +"%FT%TZ"
