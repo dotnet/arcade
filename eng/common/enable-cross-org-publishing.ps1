a7bd41dc9a7352fb380f78d551e8b74901012ad4
Matt Galbraith <MattGal@users.noreply.github.com>
2020-01-21 15:45:41 -0800

    Set $ci=$true for enable-cross-org-publishing
    Address https://github.com/dotnet/arcade/issues/4660 ; the variables that work with logging commands now require $ci to be set to do stuff.

--- eng/common/enable-cross-org-publishing.ps1
+++ eng/common/enable-cross-org-publishing.ps1
@@ -2,6 +2,10 @@ param(
   [string] $token
 )
 
+# Write-PipelineSetVariable will no-op if a variable named $ci is not defined
+# Since this script is only ever called in AzDO builds, just universally set it
+$ci = $true
+
 . $PSScriptRoot\pipeline-logging-functions.ps1
 
 Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN' -Value $token
