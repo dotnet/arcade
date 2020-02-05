45d813428236a98f1e8fb640c53351b95897acec
Santiago Fernandez Madero <safern@microsoft.com>
2020-02-05 00:21:32 -0800

    Add RuntimeConfiguration flags to have more flexibility on SkipOnCoreClrAttribute

--- src/Microsoft.DotNet.XUnitExtensions/src/Attributes/SkipOnCoreClrAttribute.cs
+++ src/Microsoft.DotNet.XUnitExtensions/src/Attributes/SkipOnCoreClrAttribute.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using Xunit.Sdk;
 
@@ -11,7 +15,11 @@ namespace Xunit
 
         public SkipOnCoreClrAttribute(string reason, TestPlatforms testPlatforms) { }
         public SkipOnCoreClrAttribute(string reason, RuntimeTestModes testMode) { }
+        public SkipOnCoreClrAttribute(string reason, RuntimeConfiguration runtimeConfigurations) { }
+        public SkipOnCoreClrAttribute(string reason, RuntimeConfiguration runtimeConfigurations, RuntimeTestModes testModes) { }
+        public SkipOnCoreClrAttribute(string reason, TestPlatforms testPlatforms, RuntimeConfiguration runtimeConfigurations) { }
         public SkipOnCoreClrAttribute(string reason, TestPlatforms testPlatforms, RuntimeTestModes testMode) { }
+        public SkipOnCoreClrAttribute(string reason, TestPlatforms testPlatforms, RuntimeConfiguration runtimeConfigurations, RuntimeTestModes testModes) { }
         public SkipOnCoreClrAttribute(string reason) { }
     }
 }
