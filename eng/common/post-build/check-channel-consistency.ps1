aa0cd54490f72a762d6d01d7728e0b889b6e9b21
Cesar Soares Lucas <disoares@microsoft.com>
2020-02-06 12:38:11 -0800

    PR feedback. Move list of channels IDs out of PS file. Merge parameters of PS file.

--- eng/common/post-build/check-channel-consistency.ps1
+++ eng/common/post-build/check-channel-consistency.ps1
@@ -1,31 +1,18 @@
 param(
-  [Parameter(Mandatory=$true)][string] $InitialChannels,            # List of channels that the build should be promoted to
-  [Parameter(Mandatory=$true)][int] $PromoteToMaestroChannelId      # If the build should be promoted to a channel this will have the Channel ID
+  [Parameter(Mandatory=$true)][string] $PromoteToChannels,            # List of channels that the build should be promoted to
+  [Parameter(Mandatory=$true)][array] $AvailableChannelIds            # List of channel IDs available in the YAML implementation
 )
 
 try {
   . $PSScriptRoot\post-build-utils.ps1
 
-  $AvailableChannelIds = 2, 9, 131, 529, 548, 549, 551, 562, 678, 679
-
-  if ($InitialChannels -eq "[] ,") {
-    Write-PipelineTelemetryError -Category 'CheckChannelConsistency' -Message "This build won't publish to any channel. Maestro didn't return any default channel."
-  }
-
   # Check that every channel that Maestro told to promote the build to 
   # is available in YAML
-  $InitialChannelsIds = $InitialChannels -split "\D" | Where-Object { $_ }
+  $PromoteToChannelsIds = $PromoteToChannels -split "\D" | Where-Object { $_ }
 
-  foreach ($id in $InitialChannelsIds) {
+  foreach ($id in $PromoteToChannelsIds) {
     if ($id -notin $AvailableChannelIds) {
       Write-PipelineTelemetryError -Category 'CheckChannelConsistency' -Message "Channel $id is not present in the post-build YAML configuration!"
-      ExitWithExitCode 1
-    }
-  }
-
-  if ($PromoteToMaestroChannelId -ne 0) {
-    if ($PromoteToMaestroChannelId -notin $AvailableChannelIds) {
-      Write-PipelineTelemetryError -Category 'CheckChannelConsistency' -Message "Channel $PromoteToMaestroChannelId is not present in the post-build YAML configuration!"
       ExitWithExitCode 1
     }
   }
