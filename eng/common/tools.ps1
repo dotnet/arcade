930feb9784bc1450ca4d23c9f0c3c776f35de113
Vlad Zarytovskii <vlza@microsoft.com>
2020-02-06 10:56:24 +0100

    Fixed powershell script (retry one less time + aactually delay), added universal retries function in bash script to handle cases when curl/wget cannot retry

--- eng/common/tools.ps1
+++ eng/common/tools.ps1
@@ -189,7 +189,7 @@ function GetDotNetInstallScript([string] $dotnetRoot) {
     Create-Directory $dotnetRoot
     $ProgressPreference = 'SilentlyContinue' # Don't display the console progress UI - it's a huge perf hit
 
-    $maxRetries = 6
+    $maxRetries = 5
     $retries = 1
 
     $uri = "https://dot.net/$dotnetInstallScriptVersion/dotnet-install.ps1"
@@ -208,6 +208,7 @@ function GetDotNetInstallScript([string] $dotnetRoot) {
       if (++$retries -le $maxRetries) {
         $delayInSeconds = [math]::Pow(2, $retries) - 1 # Exponential backoff
         Write-Host "Retrying. Waiting for $delayInSeconds seconds before next attempt ($retries of $maxRetries)."
+        Start-Sleep -Seconds $delayInSeconds
       }
       else {
         throw "Unable to download file in $maxRetries attempts."
