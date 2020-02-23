3696b2597398d9dce819be3d121b523fa1cc23ad
Jan Jahoda <jajahoda@microsoft.com>
2020-02-23 04:57:17 +0100

    Fix swallow exception when async function is executed

---
+++ src/Microsoft.DotNet.RemoteExecutor/tests/RemoteExecutorTests.cs
@@ -0,0 +1,72 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.RemoteExecutor;
+using System.Threading.Tasks;
+
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Microsoft.DotNet.RemoteExecutor.Tests
+{
+    public class RemoteExecutorTests
+    {
+        [Fact]
+        public void AsyncAction_ThrowException()
+        {
+            Assert.Throws<RemoteExecutionException>(() =>
+                RemoteExecutor.Invoke(async () =>
+                {
+                    Assert.True(false);
+                    await Task.Delay(1);
+                }).Dispose()
+            );
+        }
+
+        [Fact]
+        public void AsyncAction()
+        {
+            RemoteExecutor.Invoke(async () =>
+            {
+                await Task.Delay(1);
+            }).Dispose();
+        }
+
+        [Fact]
+        public void AsyncFunc_ThrowException()
+        {
+            Assert.Throws<RemoteExecutionException>(() =>
+                RemoteExecutor.Invoke(async () =>
+                {
+                    Assert.True(false);
+                    await Task.Delay(1);
+                    return 1;
+                }).Dispose()
+            );
+        }
+
+        [Fact]
+        public void AsyncFunc_InvalidReturnCode()
+        {
+            Assert.Throws<TrueException>(() =>
+                RemoteExecutor.Invoke(async () =>
+                {
+                    await Task.Delay(1);
+                    return 1;
+                }).Dispose()
+            );
+        }
+
+        [Fact]
+        public void AsyncFunc_NoThrow_ValidReturnCode()
+        {
+            RemoteExecutor.Invoke(async () =>
+            {
+                await Task.Delay(1);
+                return RemoteExecutor.SuccessExitCode;
+            }).Dispose();
+        }
+    }
+}
