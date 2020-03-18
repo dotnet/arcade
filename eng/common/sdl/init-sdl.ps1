21f29cfa82ab1e5c22b004978cf969044f41d171
Cesar Soares Lucas <disoares@microsoft.com>
2020-03-18 13:43:15 -0700

    Set $ci=$true all around

--- eng/common/sdl/init-sdl.ps1
+++ eng/common/sdl/init-sdl.ps1
@@ -12,6 +12,10 @@ Set-StrictMode -Version 2.0
 $disableConfigureToolsetImport = $true
 $LASTEXITCODE = 0
 
+# `tools.ps1` checks $ci to perform some actions. Since the SDL
+# scripts don't necessarily execute in the same agent that run the
+# build.ps1/sh script this variable isn't automatically set.
+$ci = $true
 . $PSScriptRoot\..\tools.ps1
 
 # Don't display the console progress UI - it's a huge perf hit
