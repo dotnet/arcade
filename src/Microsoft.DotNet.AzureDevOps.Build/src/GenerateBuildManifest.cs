ff0997ac6d39a286cebcc1e28d787413dcc3d587
Christopher Costa <chcosta@microsoft.com>
2020-04-22 10:48:50 -0700

    Add null checks around Property iterators (#5327)

--- src/Microsoft.DotNet.AzureDevOps.Build/src/GenerateBuildManifest.cs
+++ src/Microsoft.DotNet.AzureDevOps.Build/src/GenerateBuildManifest.cs
@@ -40,17 +40,26 @@ namespace Microsoft.DotNet.AzureDevOps.Build
                     }
                 }
             }
-            foreach (var signInfo in StrongNameSignInfo)
+            if (StrongNameSignInfo != null)
             {
-                itemGroup.AddItem("StrongNameSignInfo", Path.GetFileName(signInfo.ItemSpec), signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                foreach (var signInfo in StrongNameSignInfo)
+                {
+                    itemGroup.AddItem("StrongNameSignInfo", Path.GetFileName(signInfo.ItemSpec), signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                }
             }
-            foreach (var signInfo in FileSignInfo)
+            if (FileSignInfo != null)
             {
-                itemGroup.AddItem("FileSignInfo", signInfo.ItemSpec, signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                foreach (var signInfo in FileSignInfo)
+                {
+                    itemGroup.AddItem("FileSignInfo", signInfo.ItemSpec, signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                }
             }
-            foreach (var signInfo in FileExtensionSignInfo)
+            if (FileExtensionSignInfo != null)
             {
-                itemGroup.AddItem("FileExtensionSignInfo", signInfo.ItemSpec, signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                foreach (var signInfo in FileExtensionSignInfo)
+                {
+                    itemGroup.AddItem("FileExtensionSignInfo", signInfo.ItemSpec, signInfo.CloneCustomMetadata() as Dictionary<string, string>);
+                }
             }
             project.Save(ManifestPath);
             return true;
