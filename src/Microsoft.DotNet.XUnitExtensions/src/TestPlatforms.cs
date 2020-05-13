6a19a8afa92fc586ff9f772b78a6c3a2e2067caa
Adeel Mujahid <adeelbm@outlook.com>
2020-05-14 00:37:18 +0300

    Add SunOS to test discovery (#5456)

--- src/Microsoft.DotNet.XUnitExtensions/src/TestPlatforms.cs
+++ src/Microsoft.DotNet.XUnitExtensions/src/TestPlatforms.cs
@@ -14,7 +14,8 @@ namespace Xunit
         OSX = 4,
         FreeBSD = 8,
         NetBSD = 16,
-        AnyUnix = FreeBSD | Linux | NetBSD | OSX,
+        SunOS = 32,
+        AnyUnix = FreeBSD | Linux | NetBSD | OSX | SunOS,
         Any = ~0
     }
 }
