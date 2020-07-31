539fa4c7e084787f369fc024b0dd42ff33f166a2
Bill Wert <billwert@microsoft.com>
2020-07-31 11:17:44 -0700

    fix typo

--- eng/common/performance/performance-setup.sh
+++ eng/common/performance/performance-setup.sh
@@ -136,7 +136,7 @@ while (($# > 0)); do
       echo "  --runcategories <value>        Related to csproj. Categories of benchmarks to run. Defaults to \"coreclr corefx\""
       echo "  --internal                     If the benchmarks are running as an official job."
       echo "  --monodotnet                   Pass the path to the mono dotnet for mono performance testing."
-      echo "  --wasm                         Path to the unpacled wasm runtime pack."
+      echo "  --wasm                         Path to the unpacked wasm runtime pack."
       echo ""
       exit 0
       ;;
