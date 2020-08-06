8788bf394393c3d7a16e22659120fd473b2a8490
Anipik <anirudhagnihotry098@gmail.com>
2020-08-06 15:30:57 -0700

    add not supported parsing

---
+++ src/Microsoft.DotNet.GenFacades/NotSupportedAssemblyGenerator.cs
@@ -0,0 +1,160 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.DotNet.Build.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.DotNet.GenFacades
+{
+    public class NotSupportedAssemblyGenerator : BuildTask
+    {
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+
+        [Required]
+        public string IntermediateOutputPath { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public string ApiExclusionListPath { get; set; }
+
+        public override bool Execute()
+        {
+           GenerateNotSupportedAssemblyFiles(SourceFiles?.Select(item => item.ItemSpec).ToList(), IntermediateOutputPath, Message, ApiExclusionListPath);
+           return !Log.HasLoggedErrors;
+        }
+
+        public static void GenerateNotSupportedAssemblyFiles(IEnumerable<string> sourceFiles, string intermediatePath, string message, string apiExclusionListPath)
+        {
+            foreach (string sourceFile in sourceFiles)
+            {
+                if (string.IsNullOrEmpty(sourceFile))
+                {
+                    continue;
+                }
+                if (!File.Exists(sourceFile))
+                {
+                    throw new FileNotFoundException($"File {sourceFile} was not found.");
+                }
+                string text = GenerateNotSupportedAssemblyForSourceFile(sourceFile, message, apiExclusionListPath);
+                string path = intermediatePath + Path.GetFileNameWithoutExtension(sourceFile) + ".notSupported.cs";
+                File.WriteAllText(path, text);
+            }
+        }
+
+        public static string GenerateNotSupportedAssemblyForSourceFile(string sourceFile, string Message, string apiExclusionListPath)
+        {
+            string[] temp;
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(sourceFile));
+            if (apiExclusionListPath == null)
+            {
+                temp = null;
+            }
+            else
+            {
+                temp = File.ReadAllLines(Path.GetFullPath(apiExclusionListPath));
+            }
+            var rewriter = new NotSupportedAssmblyRewriter(Message, temp);
+            SyntaxNode root = rewriter.Visit(syntaxTree.GetRoot());
+            return root.GetText().ToString();
+        }
+    }
+
+    internal class NotSupportedAssmblyRewriter : CSharpSyntaxRewriter
+    {
+        private string _message;
+        private List<string> _exclusionApis;
+
+        public NotSupportedAssmblyRewriter(string Message, string[] ExclusionApis)
+        {
+            _message = Message;
+            _exclusionApis = ExclusionApis?.ToList();
+        }
+
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            if (node.Body == null)
+                return node;
+
+            if (_exclusionApis != null && _exclusionApis.Contains(GetMethodDefination(node)))
+                return null;
+
+            string message = "{ throw new System.PlatformNotSupportedException(" + $"{ _message }); "+ " }\n";  
+            var block = (BlockSyntax)SyntaxFactory.ParseStatement(message);
+            var newNode = node.Update(node.AttributeLists,
+                node.Modifiers,
+                node.ReturnType,
+                node.ExplicitInterfaceSpecifier,
+                node.Identifier,
+                node.TypeParameterList,
+                node.ParameterList,
+                node.ConstraintClauses,
+                block,
+                node.SemicolonToken);
+            return newNode;
+        }
+
+        public override SyntaxNode VisitConstructorDeclaration(ConstructorDeclarationSyntax node)
+        {
+            string message = "{ throw new System.PlatformNotSupportedException(" + $"{ _message }); "+ " }\n";        
+            var block = (BlockSyntax)SyntaxFactory.ParseStatement(message);
+            var newNode = node.Update(node.AttributeLists,
+                node.Modifiers,
+                node.Identifier,
+                node.ParameterList,
+                node.Initializer,
+                block,
+                node.SemicolonToken);
+
+            return newNode;
+        }
+
+        public override SyntaxNode VisitAccessorDeclaration(AccessorDeclarationSyntax node)
+        {
+            if (node.Keyword.Text == "set" || node.Body == null)
+                return node;
+            string message = "{ throw new System.PlatformNotSupportedException(" + $"{ _message }); "+ " } ";       
+            var block = (BlockSyntax)SyntaxFactory.ParseStatement(message);
+            var newNode = node.Update(node.AttributeLists,
+                node.Modifiers,
+                node.Keyword,
+                block,
+                node.SemicolonToken);
+
+            return newNode;
+        }
+
+        private string GetFullyQualifiedName(TypeDeclarationSyntax node)
+        {
+            string parent;
+            if (node.Parent is NamespaceDeclarationSyntax)
+            {
+                parent = GetFullyQualifiedName((NamespaceDeclarationSyntax)node.Parent);
+            }
+            else
+            {
+                parent = GetFullyQualifiedName((TypeDeclarationSyntax)node.Parent);
+            }
+            return parent + "." + node.Identifier.ValueText.Trim();
+        }
+
+        private string GetFullyQualifiedName(NamespaceDeclarationSyntax node)
+        {
+            return node.Name.ToFullString().Trim();        
+        }
+
+        private string GetMethodDefination(MethodDeclarationSyntax node)
+        {
+            string methodName = GetFullyQualifiedName((TypeDeclarationSyntax)node.Parent) + "." + node.Identifier.ValueText;
+            return methodName;
+        }
+    }
+}
