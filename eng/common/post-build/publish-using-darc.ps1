3da53ec0041873dfa1ee908f0934df4e545c118f
Epsitha Ananth <epananth@microsoft.com>
2020-08-19 14:05:37 -0700

    reverting changes

--- eng/common/post-build/publish-using-darc.ps1
+++ eng/common/post-build/publish-using-darc.ps1
@@ -54,7 +54,7 @@ try {
   --id $buildId `
   --publishing-infra-version $PublishingInfraVersion `
   --default-channels `
-  --source-branch publish-v3 `
+  --source-branch master `
   --azdev-pat $AzdoToken `
   --bar-uri $MaestroApiEndPoint `
   --password $MaestroToken `
