b11458f68a9c820162a06931b5fda6e790df2fad
Matt Galbraith <mattgal@microsoft.com>
2020-08-26 13:53:59 -0700

    Add special versions of dotnet-install.* that understand the existence of 'productVersion.txt' These become the default, but can be turned off via providing '-useDefaultDotnetInstall true' args
    Add Pipeline telemetry error logging exclusions for dotnet-install.* (can't tell where these will run from reliably and the things which invoke them do use the infra)

--- eng/configure-toolset.sh
+++ eng/configure-toolset.sh
@@ -1,7 +1,7 @@
 function Test-FilesUseTelemetryOutput {
     _files_use_telemetry_result=0
-    declare -a require_telmetry_exclude_files
-    require_telmetry_exclude_files=(
+    declare -a require_telemetry_exclude_files
+    require_telemetry_exclude_files=(
         'eng/common/build.sh'
         'eng/common/cibuild.sh'
         'eng/common/cross/armel/tizen-build-rootfs.sh'
@@ -12,12 +12,13 @@ function Test-FilesUseTelemetryOutput {
         'eng/common/cross/build-rootfs.sh'
         'eng/common/darc-init.sh'
         'eng/common/msbuild.sh'
-        'eng/common/performance/performance-setup.sh'
+        'eng/common/performance/performance-setup.sh',
+        'eng/common/dotnet-install-scripts/dotnet-install.sh'
     )
 
     local file_list=`grep --files-without-match --recursive --include=*.sh "Write-PipelineTelemetryError" $scriptroot`
     for file in $file_list; do
-        for remove_file in ${require_telmetry_exclude_files[@]}; do
+        for remove_file in ${require_telemetry_exclude_files[@]}; do
             if [[ $file =~ .*"$remove_file" ]]; then
             file_list=( "${file_list[@]/$file}" )
             fi
