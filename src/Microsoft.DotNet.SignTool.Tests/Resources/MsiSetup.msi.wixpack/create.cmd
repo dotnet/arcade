4c92f88e18d43f5cc6106d11c20c1775837e2f60
Christopher Costa <chcosta@microsoft.com>
2020-09-18 17:57:54 -0700

    remove unneeded folder

--- src/Microsoft.DotNet.SignTool.Tests/Resources/MsiSetup.msi.wixpack/create.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set outputfolder=%1
-if "%outputfolder%" NEQ "" (
-  if "%outputfolder:~-1%" NEQ "\" ( 
-    set outputfolder=%outputfolder%\
-  )
-)
-light.exe -out %outputfolder%MsiSetup.msi -pdbout MsiSetup.wixpdb -cultures:null Product.wixobj
