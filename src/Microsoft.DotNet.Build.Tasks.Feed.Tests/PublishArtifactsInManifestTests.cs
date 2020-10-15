bbc746f522d3b46441133d83b4b0d7e8b00c1ecd
Epsitha Ananth <epananth@microsoft.com>
2020-10-15 14:27:40 -0700

    review comments

--- src/Microsoft.DotNet.Build.Tasks.Feed.Tests/PublishArtifactsInManifestTests.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed.Tests/PublishArtifactsInManifestTests.cs
@@ -261,18 +261,17 @@ namespace Microsoft.DotNet.Build.Tasks.Feed.Tests
         [InlineData("3.0.0-preview1-12345", true, false)]
         [InlineData("5.3.0-rtm.6198", true, false)]
         [InlineData("3.3.1-beta3-19430-03", true, false)]
-        [InlineData("3.0.0", false, false, "false",true)]
-        [InlineData("5.0.0", true, false, "true")]
-        [InlineData("5.0.1-preview-123", true, false, "false", true)]
-        [InlineData("5.0.0", false, true, "false")]
-        [InlineData("5.0.1-preview-123", false, false, "true")]
-        public async Task StableAssetCheckV2Async(string assetVersion, bool isIsolatedFeed, bool shouldError, string isReleaseOnlyPackageVersion = "false" ,bool skipChecks = false )
+        [InlineData("3.0.0", false, false, false, true)]
+        [InlineData("5.0.0", true, false, true)]
+        [InlineData("5.0.1-preview-123", true, false, false, true)]
+        [InlineData("5.0.1-preview-123", false, false, true)]
+        public async Task StableAssetCheckV2Async(string assetVersion, bool isIsolatedFeed, bool shouldError, bool isReleaseOnlyPackageVersion = false ,bool skipChecks = false )
         {
             const string packageId = "Foo.Package";
 
             BuildIdentity buildIdentity = new BuildIdentity
             {
-                IsReleaseOnlyPackageVersion = isReleaseOnlyPackageVersion
+                IsReleaseOnlyPackageVersion = isReleaseOnlyPackageVersion.ToString()
             };
             BuildModel buildModel = new BuildModel(buildIdentity)
             {
