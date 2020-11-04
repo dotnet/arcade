50df0f8e123f24d4b26ddcad56f4b49606132c7e
Michelle McDaniel <michelm@microsoft.com>
2020-11-03 16:48:53 -0800

    Fix MSBuild escaping issues
    MSBuild does some weird escaping of strings in metadata when we use the basic constructor (ie, it removes any escaping behavior/assumes % as an escape character and compresses \\). We do not want that behavior. We want all characters to be treated as the literal character. This change modifies how we save the metadata by using ITaskItem2 and SetMetadataValueLiteral.
    
    Fixes #6163 and #6490.

--- src/Microsoft.DotNet.Helix/Sdk/GetHelixWorkItems.cs
+++ src/Microsoft.DotNet.Helix/Sdk/GetHelixWorkItems.cs
@@ -49,15 +49,27 @@ namespace Microsoft.DotNet.Helix.Sdk
 
             var workItems = new List<ITaskItem>();
 
-            IDictionary CreateWorkItemMetadata(string name)
+            Dictionary<string, string> CreateWorkItemMetadata(string name)
             {
-                var metadata = job.CloneCustomMetadata();
+                Dictionary<string, string> metadata = job.CloneCustomMetadata() as Dictionary<string, string>;
                 metadata["JobName"] = jobName;
                 metadata["WorkItemName"] = name;
                 var consoleUri = HelixApi.Options.BaseUri.AbsoluteUri.TrimEnd('/') + $"/api/2019-06-17/jobs/{jobName}/workitems/{Uri.EscapeDataString(name)}/console";
                 metadata["ConsoleOutputUri"] = consoleUri;
 
                 return metadata;
+            }
+
+            ITaskItem2 CreateTaskItem(string workItemName, Dictionary<string, string> metadata)
+            {
+                var workItem = (ITaskItem2) new TaskItem(workItemName);
+
+                foreach(KeyValuePair<string, string> entry in metadata)
+                {
+                    workItem.SetMetadataValueLiteral(entry.Key, entry.Value);
+                }
+
+                return workItem;
             }
 
             foreach (string name in status.Passed)
@@ -67,7 +79,7 @@ namespace Microsoft.DotNet.Helix.Sdk
                 var metadata = CreateWorkItemMetadata(wi);
                 metadata["Failed"] = "false";
 
-                workItems.Add(new TaskItem($"{jobName}/{wi}", metadata));
+                workItems.Add(CreateTaskItem($"{jobName}/{wi}", metadata));
             }
 
             foreach (string name in status.Failed)
@@ -90,14 +102,14 @@ namespace Microsoft.DotNet.Helix.Sdk
                                 .ToImmutableList();
                     }
 
-                    metadata["UploadedFiles"] = JsonConvert.SerializeObject(files).Replace("%", "%25");
+                    metadata["UploadedFiles"] = JsonConvert.SerializeObject(files);
                 }
                 catch (Exception ex)
                 {
                     Log.LogWarningFromException(ex);
                 }
 
-                workItems.Add(new TaskItem($"{jobName}/{wi}", metadata));
+                workItems.Add(CreateTaskItem($"{jobName}/{wi}", metadata));
                 await Task.Delay(DelayBetweenHelixApiCallsInMs, cancellationToken);
             }
 
