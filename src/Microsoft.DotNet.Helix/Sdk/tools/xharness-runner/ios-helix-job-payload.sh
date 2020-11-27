17e1b954d51a75cc98ae1d89914a0d1e9983434b
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-11-27 10:19:42 +0100

    Support the `xharness ios run` command in Helix SDK (#6621)
    Adds a new MSBuild property `IncludesTestRunner` that enables switching to the `ios run` command which only runs an iOS app bundle and detects its exit code without running any unit tests.
    
    Adds integration test that download a test app that returns the 200 exit code and verifies it using the `ExpectedExitCode` property.
    
    Requires XHarness **1.0.0-prerelease.20576.2** and up
    
    Resolves https://github.com/dotnet/core-eng/issues/11502
    Resolves https://github.com/dotnet/xharness/issues/365

--- src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/ios-helix-job-payload.sh
+++ src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/ios-helix-job-payload.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###
+### This script is used as a payload of Helix jobs that execute iOS/tvOS workloads through XHarness.
+###
+
 set -ex
 
 app=''
@@ -74,10 +78,6 @@ if [ -z "$timeout" ]; then
     die "Test timeout wasn't provided";
 fi
 
-if [ -z "$launch_timeout" ]; then
-    die "Launch timeout wasn't provided";
-fi
-
 if [ -z "$xharness_cli_path" ]; then
     die "XHarness path wasn't provided";
 fi
@@ -88,6 +88,9 @@ fi
 
 if [ "$command" == "run" ]; then
     app_arguments="--expected-exit-code=$expected_exit_code $app_arguments"
+elif [ -n "$launch_timeout" ]; then
+    # shellcheck disable=SC2089
+    app_arguments="--launch-timeout=$launch_timeout $app_arguments"
 fi
 
 set +e
@@ -105,13 +108,14 @@ open -a "$simulator_app"
 export XHARNESS_DISABLE_COLORED_OUTPUT=true
 export XHARNESS_LOG_WITH_TIMESTAMPS=true
 
-# shellcheck disable=SC2086
+# We include $app_arguments non-escaped and not arrayed because it might contain several extra arguments
+# which come from outside and are appeneded behind "--" and forwarded to the iOS application from XHarness.
+# shellcheck disable=SC2086,SC2090
 dotnet exec "$xharness_cli_path" ios $command \
     --app="$app"                              \
     --output-directory="$output_directory"    \
     --targets="$targets"                      \
     --timeout="$timeout"                      \
-    --launch-timeout="$launch_timeout"        \
     --xcode="$xcode_path"                     \
     -v                                        \
     $app_arguments
@@ -128,22 +132,24 @@ fi
 # The simulator logs comming from the sudo-spawned Simulator.app are not readable by the helix uploader
 chmod 0644 "$output_directory"/*.log
 
-test_results=$(ls "$output_directory"/xunit-*.xml)
+if [ "$command" == 'test' ]; then
+  test_results=$(ls "$output_directory"/xunit-*.xml)
 
-if [ ! -f "$test_results" ]; then
-    echo "Failed to find xUnit tests results in the output directory. Existing files:"
-    ls -la "$output_directory"
+  if [ ! -f "$test_results" ]; then
+      echo "Failed to find xUnit tests results in the output directory. Existing files:"
+      ls -la "$output_directory"
 
-    if [ $exit_code -eq 0 ]; then
-        exit_code=5
-    fi
+      if [ $exit_code -eq 0 ]; then
+          exit_code=5
+      fi
 
-    exit $exit_code
+      exit $exit_code
+  fi
+
+  echo "Found test results in $output_directory/$test_results. Renaming to testResults.xml to prepare for Helix upload"
+
+  # Prepare test results for Helix to pick up
+  mv "$test_results" "$output_directory/testResults.xml"
 fi
-
-echo "Found test results in $output_directory/$test_results. Renaming to testResults.xml to prepare for Helix upload"
-
-# Prepare test results for Helix to pick up
-mv "$test_results" "$output_directory/testResults.xml"
 
 exit $exit_code
