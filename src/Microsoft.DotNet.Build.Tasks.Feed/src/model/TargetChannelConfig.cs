e3d4a159d6c1eeab33f2827dde6c24c8fa8ad738
Epsitha Ananth <epananth@microsoft.com>
2020-12-18 11:21:20 -0800

    remove import

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/model/TargetChannelConfig.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/model/TargetChannelConfig.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.DotNet.VersionTools.BuildManifest.Model;
-using NuGet.ContentModel;
 using System;
 
 namespace Microsoft.DotNet.Build.Tasks.Feed.Model
