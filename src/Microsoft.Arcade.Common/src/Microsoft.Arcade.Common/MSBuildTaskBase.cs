032ae6f5aadedf8e4908729dcb15e8dae37a372d
Missy Messa <mjanecke@microsoft.com>
2020-12-21 16:12:33 -0800

    commenting all the things

--- src/Microsoft.Arcade.Common/src/Microsoft.Arcade.Common/MSBuildTaskBase.cs
+++ src/Microsoft.Arcade.Common/src/Microsoft.Arcade.Common/MSBuildTaskBase.cs
@@ -4,6 +4,7 @@
 using MSBuild = Microsoft.Build.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 using System.Reflection;
 using System.Linq;
 
@@ -15,10 +16,17 @@ namespace Microsoft.Arcade.Common
         protected const string AssetsVirtualDir = "assets/";
         #endregion
 
+        /// <summary>
+        /// This is the name of the method on the implementation of this class that behaves as "Execute" would in a basic
+        /// impelmentation of an MSBuild Task. 
+        /// </summary>
         private const string ExecuteMethodName = "ExecuteTask";
 
-        // TODO: comment all the things in this class :)  
-
+        /// <summary>
+        /// An MSBuild Task uses the Execute method as main method as an entry point into the task's functionality. 
+        /// It is extended in this abstract class, behind the scenes, to configure dependency injection. 
+        /// </summary>
+        /// <returns></returns>
         public override sealed bool Execute()
         {
             ServiceCollection collection = new();
@@ -27,27 +35,58 @@ namespace Microsoft.Arcade.Common
             return InvokeExecute(provider);
         }
 
+        /// <summary>
+        /// A helper method that takes the service provider contains and extrapolates the dependencies referenced in the
+        /// ExecuteTask method and then subsequently invokes the ExecuteTask method. 
+        /// 
+        /// This method can also be called from tests so that the service provider can contain any mocks that will be used
+        /// in the test. 
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
         public bool InvokeExecute(ServiceProvider provider)
         {
             return (bool)GetExecuteMethod().Invoke(this, GetExecuteArguments(provider));
         }
 
+        /// <summary>
+        /// Default service configuration. This method can be overridden in the impelmented class so that 
+        /// it can configure the dependencies that the implemented task requires. 
+        /// 
+        /// This method can also be called from the tests to complete the service collection with any dependencies 
+        /// that aren't configured for the tests. 
+        /// </summary>
+        /// <param name="collection"></param>
         public virtual void ConfigureServices(IServiceCollection collection)
         {
             collection.TryAddSingleton<IFileSystem, FileSystem>();
             collection.TryAddSingleton(Log);
         }
 
-        private ParameterInfo[] GetExecuteParameterTypes()
+        /// <summary>
+        /// Uses some reflection magic to look up the types of depenencies that to be injected into the implemented task. 
+        /// </summary>
+        /// <returns></returns>
+        private Type[] GetExecuteParameterTypes()
         {
-            return GetType().GetMethod(ExecuteMethodName).GetParameters();
+            return GetType().GetMethod(ExecuteMethodName).GetParameters().Select(p => p.ParameterType).ToArray();
         }
 
+        /// <summary>
+        /// Looks up all the dependencies from the service provider that are defined as parameters in ExecuteTask
+        /// so they can be injected into the ExecuteTask method when it's called. 
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
         private object[] GetExecuteArguments(ServiceProvider serviceProvider)
         {
-            return GetExecuteParameterTypes().Select(t => serviceProvider.GetRequiredService(t.ParameterType)).ToArray();
+            return GetExecuteParameterTypes().Select(t => serviceProvider.GetRequiredService(t)).ToArray();
         }
 
+        /// <summary>
+        /// Gets the ExecuteTask method that is in the implementation of the MSBuild Task. 
+        /// </summary>
+        /// <returns></returns>
         private MethodInfo GetExecuteMethod()
         {
             return GetType().GetMethod(ExecuteMethodName);
