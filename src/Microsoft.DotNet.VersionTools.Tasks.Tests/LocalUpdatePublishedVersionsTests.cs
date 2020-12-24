2b52f2e114f7fd28fac6f435b11cb9c4b19fe020
Missy Messa <mjanecke@microsoft.com>
2020-12-23 16:18:18 -0800

    Let's see if this fixed the tests

--- src/Microsoft.DotNet.VersionTools.Tasks.Tests/LocalUpdatePublishedVersionsTests.cs
+++ src/Microsoft.DotNet.VersionTools.Tasks.Tests/LocalUpdatePublishedVersionsTests.cs
@@ -1,6 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-/*
+
 using FluentAssertions;
 using Microsoft.DotNet.Build.Tasks.VersionTools;
 using Microsoft.DotNet.Internal.DependencyInjection.Testing;
@@ -37,4 +37,4 @@ namespace Microsoft.DotNet.VersionTools.Tasks.Tests
                 .BeTrue(message);
         }
     }
-}*/
+}
