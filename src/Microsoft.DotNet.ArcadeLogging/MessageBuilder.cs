0d830f88ef9651de7a7fdce7a4ce2b9ecd35f55c
Aaron <tohron@hotmail.com>
2021-01-05 10:39:38 -0800

    New ArcadeLogging library

--- src/Microsoft.DotNet.ArcadeLogging/MessageBuilder.cs
+++ src/Microsoft.DotNet.ArcadeLogging/MessageBuilder.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Microsoft.DotNet.Arcade.Sdk
+namespace Microsoft.DotNet.ArcadeLogging
 {
     public sealed class MessageBuilder
     {
