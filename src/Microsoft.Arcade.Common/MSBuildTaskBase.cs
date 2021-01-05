36db6266ea0398c851d03c31aba9e5665f152e1d
Missy Messa <mjanecke@microsoft.com>
2021-01-05 12:43:59 -0800

    Changes per code review feedback

--- src/Microsoft.Arcade.Common/MSBuildTaskBase.cs
+++ src/Microsoft.Arcade.Common/MSBuildTaskBase.cs
@@ -34,8 +34,7 @@ namespace Microsoft.Arcade.Common
             using (var provider = collection.BuildServiceProvider())
             {
                 return InvokeExecute(provider);
-            }
-            
+            }            
         }
 
         /// <summary>
