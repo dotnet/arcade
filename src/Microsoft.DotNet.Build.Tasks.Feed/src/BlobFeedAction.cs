1ea776d8a5b084998fce87873f2ea5591d9b9d88
Epsitha Ananth <epananth@microsoft.com>
2021-01-06 19:36:08 -0800

    Publish to multiple channels

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/BlobFeedAction.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/BlobFeedAction.cs
@@ -190,8 +190,9 @@ namespace Microsoft.DotNet.Build.Tasks.Feed
                 }
                 else
                 {
+                    FileStream stream = new FileStream(item.ItemSpec, FileMode.Open, FileAccess.Read, FileShare.Read);
                     Log.LogMessage($"Uploading {item} to {relativeBlobPath}.");
-                    await blobUtils.UploadBlockBlobAsync(item.ItemSpec, relativeBlobPath);
+                    await blobUtils.UploadBlockBlobAsync(item.ItemSpec, relativeBlobPath, stream);
                 }
             }
             catch (Exception exc)
