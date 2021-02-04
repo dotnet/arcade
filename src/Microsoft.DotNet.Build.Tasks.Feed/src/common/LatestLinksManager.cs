6ca825c880a6594182612de08aa16da0aa51eee1
Michelle McDaniel <adiaaida@gmail.com>
2021-02-04 13:06:43 -0800

    Undo the flattening for now, fix the constants

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/common/LatestLinksManager.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/common/LatestLinksManager.cs
@@ -92,7 +92,7 @@ namespace Microsoft.DotNet.Build.Tasks.Feed
         /// <remarks>
         public string GetLatestShortUrlForBlob(TargetFeedConfig feedConfig, BlobArtifactModel blob)
         {
-            string blobIdWithoutVersions = Path.GetFileName(blob.Id);
+            string blobIdWithoutVersions = VersionIdentifier.RemoveVersions(blob.Id);
 
             return Path.Combine(feedConfig.LatestLinkShortUrlPrefix, blobIdWithoutVersions).Replace("\\", "/");
         }
