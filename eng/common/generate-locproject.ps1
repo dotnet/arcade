34dbbce65b7c005826b2e19a3eace0c9bdf83fd1
Jon Fortescue <jofortes@microsoft.com>
2021-02-08 10:59:05 -0800

    Add exclusions

--- eng/common/generate-locproject.ps1
+++ eng/common/generate-locproject.ps1
@@ -5,6 +5,7 @@ $ErrorActionPreference = "Stop"
 
 Push-Location "$env:BUILD_SOURCESDIRECTORY" # push location for Resolve-Path -Relative to work
 $resxFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.resx"
+$exclusions = Get-Content "$env:BUILD_SOURCESDIRECTORY\Localize\LocExclusions.json" | ConvertFrom-Json
 
 $locJson = @{
     Projects = @(
@@ -12,10 +13,21 @@ $locJson = @{
             LanguageSet = "Azure_Languages"
             LocItems = @(
                 $resxFiles | ForEach-Object {
-                    @{
-                        SourceFile = $_.FullName
-                        CopyOption = "LangIDOnName"
-                        OutputPath = "Localize\$(($_.DirectoryName | Resolve-Path -Relative).Substring(2) + "\" + $_.Name)"
+                    $outputPath = "Localize\$(($_.DirectoryName | Resolve-Path -Relative).Substring(2) + "\" + $_.Name)" 
+                    $continue = $true
+                    $exclusions.Exclusions | ForEach-Object {
+                        if ($outputPath.Contains($_))
+                        {
+                            $continue = $false
+                        }
+                    }
+                    if ($continue)
+                    {
+                        @{
+                            SourceFile = $_.FullName
+                            CopyOption = "LangIDOnName"
+                            OutputPath = $outputPath
+                        }
                     }
                 }
             )
