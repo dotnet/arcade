a3b0c4a6e86670ea1af2358d0713ca0ddec1d389
Jakub Stilec <jastilec@microsoft.com>
2021-02-10 18:17:22 +0100

    included tools.ps1 and added back Write-PipelineTelemetryError

--- eng/common/sdl/init-sdl.ps1
+++ eng/common/sdl/init-sdl.ps1
@@ -11,6 +11,12 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 $LASTEXITCODE = 0
 
+# `tools.ps1` checks $ci to perform some actions. Since the SDL
+# scripts don't necessarily execute in the same agent that run the
+# build.ps1/sh script this variable isn't automatically set.
+$ci = $true
+. $PSScriptRoot\..\tools.ps1
+
 # Don't display the console progress UI - it's a huge perf hit
 $ProgressPreference = 'SilentlyContinue'
 
@@ -42,5 +48,6 @@ try {
 }
 catch {
   Write-Host $_.ScriptStackTrace
+  Write-PipelineTelemetryError -Force -Category 'Sdl' -Message $_
   ExitWithExitCode 1
 }
