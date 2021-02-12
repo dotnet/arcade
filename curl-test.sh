a8080a3c856e4de4b8963ce31c3ee3730f2f99a5
Alex Perovich <alperovi@microsoft.com>
2021-02-12 14:36:47 -0800

    Fix url

--- curl-test.sh
+++ curl-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-install_script_url="https://dot.net/$dotnetInstallScriptVersion/dotnet-install.sh"
+install_script_url="https://dot.net/v1/dotnet-install.sh"
 curl "$install_script_url" -sSL --verbose --retry 10 --create-dirs -o "dotnet-install.sh"
