c57912614b9b0d3ac50f8ef924d6e4c23fcad7d7
Jon Fortescue <jofortes@microsoft.com>
2021-03-08 14:24:47 -0800

    Update scripts and templates

--- eng/common/generate-locproject.ps1
+++ eng/common/generate-locproject.ps1
@@ -1,23 +1,9 @@
+Param(
+    [string] $LanguageSet = 'VS_Main_Languages'
+)
+
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = "Stop"
-
-Push-Location "$env:BUILD_SOURCESDIRECTORY" # push location for Resolve-Path -Relative to work
-$resxFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.resx"
-$xlfFiles = @()
-
-$allXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.xlf"
-$langXlfFiles = @()
-if ($allXlfFiles -ne $null -And $allXlfFiles.Length -gt 0) {
-    $allXlfFiles[0].FullName -Match "\.([\w-]+)\.xlf"
-    $firstLangCode = $Matches.1
-    $langXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.$firstLangCode.xlf"
-}
-$langXlfFiles | ForEach-Object {
-    $_.Name -Match "([^.]+)\.[\w-]+\.xlf"
-    $xlfFiles += Copy-Item "$($_.FullName)" -Destination "$($_.Directory.FullName)\$($Matches.1).xlf" -PassThru
-}
-
-$locFiles = $resxFiles + $xlfFiles
 
 $exclusionsFilePath = "$env:BUILD_SOURCESDIRECTORY\Localize\LocExclusions.json"
 $exclusions = @{ Exclusions = @() }
@@ -26,14 +12,31 @@ if (Test-Path -Path $exclusionsFilePath)
     $exclusions = Get-Content "$env:BUILD_SOURCESDIRECTORY\Localize\LocExclusions.json" | ConvertFrom-Json
 }
 
+Push-Location "$env:BUILD_SOURCESDIRECTORY" # push location for Resolve-Path -Relative to work
+$jsonFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY" | where { $_.FullName -Match "en\\strings.json" }
+$xlfFiles = @()
+
+$allXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.xlf"
+$langXlfFiles = @()
+if ($allXlfFiles.Length -gt 0) {
+    $isMatch = $allXlfFiles[0].FullName -Match "\.([\w-]+)\.xlf"
+    $firstLangCode = $Matches.1
+    $langXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.$firstLangCode.xlf"
+}
+$langXlfFiles | ForEach-Object {
+    $isMatch = $_.Name -Match "([^.]+)\.[\w-]+\.xlf"
+    $xlfFiles += Copy-Item "$($_.FullName)" -Destination "$($_.Directory.FullName)\$($Matches.1).xlf" -PassThru
+}
+
+$locFiles = $jsonFiles + $xlfFiles
 
 $locJson = @{
     Projects = @(
         @{
-            LanguageSet = "Azure_Languages"
+            LanguageSet = $LanguageSet
             LocItems = @(
                 $locFiles | ForEach-Object {
-                    $outputPath = "Localize\$(($_.DirectoryName | Resolve-Path -Relative).Substring(2) + "\" + $_.Name)" 
+                    $outputPath = "Localize\$(($_.DirectoryName | Resolve-Path -Relative).Substring(2) + "\")" 
                     $continue = $true
                     $exclusions.Exclusions | ForEach-Object {
                         if ($outputPath.Contains($_))
@@ -61,3 +64,4 @@ Pop-Location
 
 New-Item "$env:BUILD_SOURCESDIRECTORY\Localize\LocProject.json" -Force
 Set-Content "$env:BUILD_SOURCESDIRECTORY\Localize\LocProject.json" $json
+Copy-Item "$env:BUILD_SOURCESDIRECTORY\Localize\LocProject.json" "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\loc"
