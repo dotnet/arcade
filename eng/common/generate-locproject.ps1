29d406a1154e84a68c2c81d9f63a4d9c7ea44bbf
Jon Fortescue <jofortes@microsoft.com>
2021-03-17 14:55:19 -0700

    Add telemetry to generate-locproject.ps1

--- eng/common/generate-locproject.ps1
+++ eng/common/generate-locproject.ps1
@@ -91,7 +91,7 @@ else {
     Set-Content "$SourcesDirectory\Localize\LocProject-generated.json" $json
 
     if ((Get-FileHash "$SourcesDirectory\Localize\LocProject-generated.json").Hash -ne (Get-FileHash "$SourcesDirectory\Localize\LocProject.json").Hash) {
-        Write-PipelineTaskError -Type "warning" -Message "Existing LocProject.json differs from generated LocProject.json. Download LocProject-generated.json and compare them."
+        Write-PipelineTelemetryError -Category "OneLocBuild" -Message "Existing LocProject.json differs from generated LocProject.json. Download LocProject-generated.json and compare them."
         
         exit 1
     }
