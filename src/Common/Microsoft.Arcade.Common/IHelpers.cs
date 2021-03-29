fd5f55c64d48b7894516cc841fba1253b2e79ffd
Matt Galbraith <MattGal@users.noreply.github.com>
2021-03-29 16:53:21 -0700

    Revert "Add support for tvOS devices in XHarness Helix SDK (#7135)" (#7162)
    This reverts commit 12a2bc501ce756c9522588d88583c0256297bd70.

--- src/Common/Microsoft.Arcade.Common/IHelpers.cs
+++ src/Common/Microsoft.Arcade.Common/IHelpers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.Arcade.Common
@@ -6,15 +6,10 @@ namespace Microsoft.Arcade.Common
     public interface IHelpers
     {
         string ComputeSha256Hash(string normalizedPath);
-
-        T MutexExec<T>(Func<T> function, string mutexName);
-        T MutexExec<T>(Func<Task<T>> function, string mutexName);
-        void MutexExec(Func<Task> function, string mutexName);
-
         T DirectoryMutexExec<T>(Func<T> function, string path);
         T DirectoryMutexExec<T>(Func<Task<T>> function, string path);
-        void DirectoryMutexExec(Func<Task> function, string path);
-
+        T MutexExec<T>(Func<T> function, string mutexName);
+        T MutexExec<T>(Func<Task<T>> function, string mutexName);
         string RemoveTrailingSlash(string directoryPath);
     }
 }
