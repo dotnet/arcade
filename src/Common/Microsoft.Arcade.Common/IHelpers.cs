43a491a459ea6f5c636a220009d5b1482f2b8189
Přemek Vysoký <premek.vysoky@microsoft.com>
2021-03-30 17:27:34 +0200

    Add support for tvOS devices in XHarness Helix SDK (#7163)

--- src/Common/Microsoft.Arcade.Common/IHelpers.cs
+++ src/Common/Microsoft.Arcade.Common/IHelpers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.Arcade.Common
@@ -6,10 +6,15 @@ namespace Microsoft.Arcade.Common
     public interface IHelpers
     {
         string ComputeSha256Hash(string normalizedPath);
-        T DirectoryMutexExec<T>(Func<T> function, string path);
-        T DirectoryMutexExec<T>(Func<Task<T>> function, string path);
+
         T MutexExec<T>(Func<T> function, string mutexName);
         T MutexExec<T>(Func<Task<T>> function, string mutexName);
+        void MutexExec(Func<Task> function, string mutexName);
+
+        T DirectoryMutexExec<T>(Func<T> function, string path);
+        T DirectoryMutexExec<T>(Func<Task<T>> function, string path);
+        void DirectoryMutexExec(Func<Task> function, string path);
+
         string RemoveTrailingSlash(string directoryPath);
     }
 }
