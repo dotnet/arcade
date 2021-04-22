81903822efade07ca4a674d5c75d60e68bd9ee4c
Jon Fortescue <jofortes@microsoft.com>
2021-04-22 13:17:11 -0700

    Continuing

--- src/Microsoft.DotNet.Arcade.Sdk.Tests/PostOneLocBuildToGitHubTests.cs
+++ src/Microsoft.DotNet.Arcade.Sdk.Tests/PostOneLocBuildToGitHubTests.cs
@@ -1,7 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using FluentAssertions;
 using Microsoft.Arcade.Common;
+using Microsoft.Arcade.Test.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
@@ -17,6 +19,77 @@ namespace Microsoft.DotNet.Arcade.Sdk.Tests
 {
     public class PostOneLocBuildToGitHubTests
     {
+        private readonly Mock<IGitHubClient> _gitHubClientMock;
+        private readonly Mock<IFileSystem> _fileSystemMock;
+        private readonly Mock<IHelpers> _helpersMock;
 
+        private readonly string _locFilesDir = @"C:\a\loc";
+        private readonly string _gitHubOrg = "dotnet";
+        private readonly string _gitHubRepo = "arcade";
+
+        private readonly PostOneLocBuildToGitHub _task;
+
+        public PostOneLocBuildToGitHubTests()
+        {
+            _fileSystemMock = new Mock<IFileSystem>();
+            _gitHubClientMock = new Mock<IGitHubClient>();
+
+            bool succeeded = false;
+
+            _helpersMock = new Mock<IHelpers>();
+            _helpersMock
+                .Setup(x => x.DirectoryMutexExec(It.IsAny<Func<bool>>(), It.IsAny<string>()))
+                .Callback<Func<bool>, string>((function, path) => {
+                    succeeded = function();
+                })
+                .Returns(() => succeeded);
+
+
+            _task = new PostOneLocBuildToGitHub
+            {
+                LocFilesDirectory = _locFilesDir,
+                GitHubPat = "fake",
+                GitHubOrg = _gitHubOrg,
+                GitHubRepo = _gitHubRepo,
+            };
+        }
+
+        [Fact]
+        public async Task ReturnsExistingOneLocPr()
+        {
+            // Setup
+            IReadOnlyList<PullRequest> prs = new List<PullRequest>
+            {
+                new TestPullRequest { TestTitle = $"A nonsense PR", TestState = ItemState.Open },
+                new TestPullRequest { TestTitle = $"{PostOneLocBuildToGitHub.PrPrefix}04/21/2021", TestState = ItemState.Open },
+                new TestPullRequest { TestTitle = $"A different nonsense PR", TestState = ItemState.Open },
+                new TestPullRequest { TestTitle = $"[DO NOT MERGE] Just testing", TestState = ItemState.Open },
+            };
+
+            _gitHubClientMock
+                .Setup(g => g.PullRequest.GetAllForRepository(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<PullRequestRequest>()))
+                .Returns(Task.FromResult(prs));
+
+            // Act
+            PullRequest foundPr = await _task.FindExistingOneLocPr(_gitHubClientMock.Object);
+
+            // Verify
+            foundPr.Title.Should().Be($"{PostOneLocBuildToGitHub.PrPrefix}04/21/2021");
+        }
+
+        private IServiceCollection CreateMockServiceCollection()
+        {
+            var collection = new ServiceCollection();
+            collection.AddSingleton(_gitHubClientMock.Object);
+            collection.AddSingleton(_fileSystemMock.Object);
+            collection.AddSingleton(_helpersMock.Object);
+            return collection;
+        }
+
+        private class TestPullRequest : PullRequest
+        {
+            public string TestTitle { get { return Title; } set { Title = value; } }
+            public StringEnum<ItemState> TestState { get { return State; } set { State = value; } }
+        }
     }
 }
--- src/Microsoft.DotNet.Arcade.Sdk/src/PostOneLocBuildToGitHub.cs
+++ src/Microsoft.DotNet.Arcade.Sdk/src/PostOneLocBuildToGitHub.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Arcade.Common;
+using Microsoft.Build.Framework;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Octokit;
@@ -15,7 +16,19 @@ namespace Microsoft.DotNet.Arcade.Sdk
 {
     public class PostOneLocBuildToGitHub : MSBuildTaskBase
     {
+        [Required]
+        public string LocFilesDirectory { get; set; }
 
+        [Required]
+        public string GitHubPat { get; set; }
+
+        [Required]
+        public string GitHubOrg { get; set; }
+
+        [Required]
+        public string GitHubRepo { get; set; }
+
+        public static string PrPrefix = "Localized files from OneLocBuild for ";
 
         public override void ConfigureServices(IServiceCollection collection)
         {
@@ -27,11 +40,27 @@ namespace Microsoft.DotNet.Arcade.Sdk
 
         public bool ExecuteTask(IGitHubClient gitHubClient, IFileSystem fileSystem, IHelpers helpers)
         {
-            
+            gitHubClient.Connection.Credentials = new Credentials("dnbot", GitHubPat);
+
+
 
             return true;
         }
 
+        public async Task<PullRequest> FindExistingOneLocPr(IGitHubClient gitHubClient)
+        {
+            PullRequestRequest filter = new PullRequestRequest { State = ItemStateFilter.Open };
+            var prs = (await gitHubClient.PullRequest.GetAllForRepository(GitHubOrg, GitHubRepo, filter))
+                .Where(pr => pr.Title.StartsWith(PrPrefix));
 
+            if (prs.Count() > 0)
+            {
+                return prs.First();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
