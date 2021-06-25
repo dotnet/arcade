dotnet/arcade

commit 13040ff54ecdb347b70f51d75b0f3f84a348c6b0

[main] Add option to prefer 64bit `msbuild` (#7559)
- cherry-pick of a5dbede4615c / #7531

eng/common/tools.ps1
@@ -378,7 +378,16 @@ function InitializeVisualStudioMSBuild([bool]$install, [object]$vsRequirements =
   }
 
   $msbuildVersionDir = if ([int]$vsMajorVersion -lt 16) { "$vsMajorVersion.0" } else { "Current" }
-  return $global:_MSBuildExe = Join-Path $vsInstallDir "MSBuild\$msbuildVersionDir\Bin\msbuild.exe"
+
+  $local:BinFolder = Join-Path $vsInstallDir "MSBuild\$msbuildVersionDir\Bin"
+  $local:Prefer64bit = if ($vsRequirements.Prefer64bit) { $vsRequirements.Prefer64bit } else { $false }
+  if ($local:Prefer64bit -and (Test-Path(Join-Path $local:BinFolder "amd64"))) {
+    $global:_MSBuildExe = Join-Path $local:BinFolder "amd64\msbuild.exe"
+  } else {
+    $global:_MSBuildExe = Join-Path $local:BinFolder "msbuild.exe"
+  }
+
+  return $global:_MSBuildExe
 }
 
 function InitializeVisualStudioEnvironmentVariables([string] $vsInstallDir, [string] $vsMajorVersion) {

commit 3a18154fdc531f2a9bf28f15c02131c77e9218c7

[main] Fix strict-mode break - use `Get-Member` instead of assuming `$null` property value - fixes build break I caused in 13040ff54ecd

eng/common/tools.ps1
@@ -380,7 +380,7 @@ function InitializeVisualStudioMSBuild([bool]$install, [object]$vsRequirements =
   $msbuildVersionDir = if ([int]$vsMajorVersion -lt 16) { "$vsMajorVersion.0" } else { "Current" }
 
   $local:BinFolder = Join-Path $vsInstallDir "MSBuild\$msbuildVersionDir\Bin"
-  $local:Prefer64bit = if ($vsRequirements.Prefer64bit) { $vsRequirements.Prefer64bit } else { $false }
+  $local:Prefer64bit = if (Get-Member -InputObject $vsRequirements -Name 'Prefer64bit') { $vsRequirements.Prefer64bit } else { $false }
   if ($local:Prefer64bit -and (Test-Path(Join-Path $local:BinFolder "amd64"))) {
     $global:_MSBuildExe = Join-Path $local:BinFolder "amd64\msbuild.exe"
   } else {