5c6e9d63b1be20bcd50e218fd5cb90e716c45b23
Premek Vysoky <premek.vysoky@microsoft.com>
2021-07-21 14:30:18 +0200

    Fix retry/reboot on Android windows

--- src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/xharness-helix-job.android.ps1
+++ src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/xharness-helix-job.android.ps1
@@ -39,6 +39,7 @@ $exit_code=$LASTEXITCODE
 
 # ADB_DEVICE_ENUMERATION_FAILURE
 if ($exit_code -eq 85) {
+    $ErrorActionPreference="Continue"
     Write-Error "Encountered ADB_DEVICE_ENUMERATION_FAILURE.  This is typically not a failure of the work item.  We will run it again and reboot this computer to help its devices"
     Write-Error "If this occurs repeatedly, please check for architectural mismatch, e.g. sending x86 or x86_64 APKs to an arm64_v8a-only queue."
     & "$Env:HELIX_PYTHONPATH" -c "from helix.workitemutil import request_infra_retry; request_infra_retry('Retrying because we could not enumerate all Android devices')"
