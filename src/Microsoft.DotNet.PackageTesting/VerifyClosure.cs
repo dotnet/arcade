dotnet/arcade

commit a50c27610c6a060c677090b801636cd7149cd591

Move VerifyClosure tasks into M.D.PackageTesting (#7692)
As we plan to deprecate the M.D.B.Tasks.Packaging project in the future,
moving the tasks into the PackageTesting project.

Fixes https://github.com/dotnet/arcade/issues/7474

src/Microsoft.DotNet.PackageTesting/VerifyClosure.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Build.Framework;
+using Microsoft.DotNet.Build.Tasks;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,7 +13,7 @@ using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Xml.Linq;
 
-namespace Microsoft.DotNet.Build.Tasks.Packaging
+namespace Microsoft.DotNet.PackageTesting
 {
     /// <summary>
     /// Verifies the closure of a set of DLLs, making sure all files are present and no cycles exist
@@ -125,7 +126,7 @@ namespace Microsoft.DotNet.Build.Tasks.Packaging
 
         private void LoadIgnoredReferences()
         {
-            foreach (var ignoredReference in IgnoredReferences.NullAsEmpty())
+            foreach (var ignoredReference in IgnoredReferences.DefaultIfEmpty())
             {
                 var name = ignoredReference.ItemSpec;
                 var versionString = ignoredReference.GetMetadata("Version");
@@ -175,7 +176,7 @@ namespace Microsoft.DotNet.Build.Tasks.Packaging
             // check module references
             if (assm.State == CheckState.Unchecked && CheckModuleReferences)
             {
-                foreach(var moduleReference in assm.ModuleReferences.NullAsEmpty())
+                foreach(var moduleReference in assm.ModuleReferences.DefaultIfEmpty())
                 {
                     if (ShouldIgnore(moduleReference))
                     {

src/Microsoft.DotNet.Build.Tasks.Packaging/src/Microsoft.DotNet.Build.Tasks.Packaging.csproj
@@ -38,9 +38,7 @@
 - SplitDependenciesBySupport
 - SplitReferences
 - UpdatePackageIndex
-- ValidationTask
-- VerifyClosure
-- VerifyTypes</PackageDescription>
+- ValidationTask</PackageDescription>
 
     <DefaultItemExcludes Condition="'$(TargetFramework)' != 'net472'">**/*.Desktop.*</DefaultItemExcludes>
     <BeforePack>$(BeforePack);AddRuntimeJson</BeforePack>

src/Microsoft.DotNet.Build.Tasks.Packaging/src/build/Packaging.common.targets
@@ -44,7 +44,5 @@
   <UsingTask TaskName="UpdatePackageIndex" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
   <UsingTask TaskName="ValidatePackage" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
   <UsingTask TaskName="ValidateFrameworkPackage" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
-  <UsingTask TaskName="VerifyClosure" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
-  <UsingTask TaskName="VerifyTypes" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
   <UsingTask TaskName="GetSupportedPackagesFromPackageReports" AssemblyFile="$(PackagingTaskDir)Microsoft.DotNet.Build.Tasks.Packaging.dll"/>
 </Project>

src/Microsoft.DotNet.PackageTesting.Tests/Microsoft.DotNet.PackageTesting.Tests.csproj
@@ -9,6 +9,8 @@
     <PackageReference Include="Microsoft.Build.Utilities.Core" Version="$(MicrosoftBuildUtilitiesCoreVersion)" />
     <PackageReference Include="NuGet.Frameworks" Version="$(NuGetVersion)" />
     <PackageReference Include="NuGet.Packaging" Version="$(NuGetVersion)" />
+    <!-- Remove when targeting >= net5.0. -->
+    <PackageReference Include="System.Reflection.Metadata" Version="5.0.0" />
   </ItemGroup>
 
   <ItemGroup>

src/Microsoft.DotNet.PackageTesting/GetCompatiblePackageTargetFrameworks.cs
@@ -13,7 +13,7 @@ namespace Microsoft.DotNet.PackageTesting
 {
     public class GetCompatiblePackageTargetFrameworks : BuildTask
     {
-        private static List<NuGetFramework> allTargetFrameworks = allTargetFrameworks = new();
+        private static List<NuGetFramework> allTargetFrameworks = new();
         private static Dictionary<NuGetFramework, HashSet<NuGetFramework>> packageTfmMapping = new();
 
         [Required]
@@ -27,12 +27,15 @@ namespace Microsoft.DotNet.PackageTesting
 
         public override bool Execute()
         {
-            bool result = true;
-            List<ITaskItem> testProjects = new List<ITaskItem>();
+            List<ITaskItem> testProjects = new();
+
             try
             {
                 Initialize(SupportedTestFrameworks);
-                string minDotnetTargetFramework = allTargetFrameworks.Where(t => t.Framework == ".NETCoreApp").OrderBy(t => t.Version).FirstOrDefault()?.GetShortFolderName();
+                string minDotnetTargetFramework = allTargetFrameworks.Where(t => t.Framework == ".NETCoreApp")
+                    .OrderBy(t => t.Version)
+                    .FirstOrDefault()?
+                    .GetShortFolderName();
 
                 foreach (var packagePath in PackagePaths)
                 {
@@ -50,25 +53,30 @@ namespace Microsoft.DotNet.PackageTesting
                 Log.LogErrorFromException(e, showStackTrace: false);
             }
 
-            return result && !Log.HasLoggedErrors;
+            return !Log.HasLoggedErrors;
         }
 
         public static IEnumerable<NuGetFramework> GetTestFrameworks(Package package, string minDotnetTargetFramework)
         {
-            List<NuGetFramework> frameworksToTest= new List<NuGetFramework>();
+            List<NuGetFramework> frameworksToTest= new();
             IEnumerable<NuGetFramework> packageTargetFrameworks = package.FrameworksInPackage;
 
             // Testing the package installation on all tfms linked with package targetframeworks.
             foreach (var item in packageTargetFrameworks)
             {
                 if (packageTfmMapping.ContainsKey(item))
+                {
                     frameworksToTest.AddRange(packageTfmMapping[item]);
+                }
+
                 // Adding the frameworks in the packages to the test matrix.
                 frameworksToTest.Add(item);
             }
 
             if (!string.IsNullOrEmpty(minDotnetTargetFramework) && frameworksToTest.Any(t => t.Framework == ".NETStandard"))
+            {
                 frameworksToTest.Add(NuGetFramework.Parse(minDotnetTargetFramework));
+            }
 
             return frameworksToTest.Where(tfm => allTargetFrameworks.Contains(tfm)).Distinct();
         }
@@ -97,12 +105,12 @@ namespace Microsoft.DotNet.PackageTesting
             }
         }
 
-        public List<ITaskItem> CreateItemFromTestFramework(string packageId, string version, IEnumerable<NuGetFramework> testFrameworks)
+        private static List<ITaskItem> CreateItemFromTestFramework(string packageId, string version, IEnumerable<NuGetFramework> testFrameworks)
         {
-            List<ITaskItem> testprojects = new List<ITaskItem>();
+            List<ITaskItem> testprojects = new();
             foreach (var framework in testFrameworks)
             {
-                var supportedPackage = new TaskItem(packageId);
+                TaskItem supportedPackage = new(packageId);
                 supportedPackage.SetMetadata("Version", version);
                 supportedPackage.SetMetadata("TargetFramework", framework.ToString());
                 supportedPackage.SetMetadata("TargetFrameworkShort", framework.GetShortFolderName());

src/Microsoft.DotNet.PackageTesting/Microsoft.DotNet.PackageTesting.csproj
@@ -2,7 +2,6 @@
 
   <PropertyGroup>
     <TargetFrameworks>netcoreapp3.1;net472</TargetFrameworks>
-    <ExcludeFromSourceBuild>false</ExcludeFromSourceBuild>
     <PackageType>MSBuildSdk</PackageType>
     <IncludeBuildOutput>false</IncludeBuildOutput>
     <IsPackable>true</IsPackable>

src/Microsoft.DotNet.PackageTesting/NupkgParser.cs
@@ -2,24 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Linq;
+using System.Collections.Generic;
 using NuGet.Frameworks;
 using NuGet.Packaging;
 
 namespace Microsoft.DotNet.PackageTesting
 {
-    public class NupkgParser
+    class NupkgParser
     {
         public static Package CreatePackageObject(string packagePath)
         {
-            PackageArchiveReader nupkgReader = new PackageArchiveReader(packagePath);
+            PackageArchiveReader nupkgReader = new(packagePath);
             NuspecReader nuspecReader = nupkgReader.NuspecReader;
 
             string packageId = nuspecReader.GetId();
             string version = nuspecReader.GetVersion().ToString();
 
-            NuGetFramework[] dependencyFrameworks = nuspecReader.GetDependencyGroups().Select(dg => dg.TargetFramework).Where(tfm => tfm != null).ToArray();
+            NuGetFramework[] dependencyFrameworks = nuspecReader.GetDependencyGroups()
+                .Select(dg => dg.TargetFramework)
+                .Where(tfm => tfm != null)
+                .ToArray();
+            IEnumerable<string> files = nupkgReader.GetFiles()?.Where(t => t.EndsWith(packageId + ".dll"));
 
-            return new Package(packageId, version, nupkgReader.GetFiles()?.Where(t => t.EndsWith(packageId + ".dll")), dependencyFrameworks);
+            return new Package(packageId, version, files, dependencyFrameworks);
         }
     }
 }

src/Microsoft.DotNet.PackageTesting/Package.cs
@@ -11,6 +11,10 @@ namespace Microsoft.DotNet.PackageTesting
 {
     public class Package
     {
+        public IEnumerable<NuGetFramework> FrameworksInPackage { get; }
+        public string PackageId { get; }
+        public string Version { get; }
+
         public Package(string packageId, string version, IEnumerable<string> packageAssetPaths, IEnumerable<NuGetFramework> dependencyFrameworks)
         {
             PackageId = packageId;
@@ -18,7 +22,7 @@ namespace Microsoft.DotNet.PackageTesting
 
             ContentItemCollection packageAssets = new();
             packageAssets.Load(packageAssetPaths);
-            ManagedCodeConventions conventions = new ManagedCodeConventions(null);
+            ManagedCodeConventions conventions = new(null);
 
             IEnumerable<ContentItem> RefAssets = packageAssets.FindItems(conventions.Patterns.CompileRefAssemblies);
             IEnumerable<ContentItem> LibAssets = packageAssets.FindItems(conventions.Patterns.CompileLibAssemblies);
@@ -30,9 +34,5 @@ namespace Microsoft.DotNet.PackageTesting
             FrameworksInPackageList.AddRange(dependencyFrameworks);
             FrameworksInPackage = FrameworksInPackageList.Distinct();
         }
-
-        public string PackageId { get; set; }
-        public string Version { get; set; }
-        public IEnumerable<NuGetFramework> FrameworksInPackage { get; private set; }
     }
 }

commit 59d147a19dc8dee36f49ba2bebcaaadf8950e7ca

Add null checks back in Verify* types
Regressed with https://github.com/dotnet/arcade/commit/a50c27610c6a060c677090b801636cd7149cd591 as the code paths don't protect anymore against null.

src/Microsoft.DotNet.PackageTesting/VerifyClosure.cs
@@ -126,7 +126,9 @@ namespace Microsoft.DotNet.PackageTesting
 
         private void LoadIgnoredReferences()
         {
-            foreach (var ignoredReference in IgnoredReferences.DefaultIfEmpty())
+            if (IgnoredReferences == null or IgnoredReferences.Length == 0) return;
+        
+            foreach (var ignoredReference in IgnoredReferences)
             {
                 var name = ignoredReference.ItemSpec;
                 var versionString = ignoredReference.GetMetadata("Version");