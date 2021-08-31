90a404027591736ca363151a3cf9784eaa2fa66a
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-31 10:50:46 +0200

    Fix updating zip archives II

--- src/Common/Microsoft.Arcade.Common/ZipArchiveManager.cs
+++ src/Common/Microsoft.Arcade.Common/ZipArchiveManager.cs
@@ -24,14 +24,18 @@ namespace Microsoft.Arcade.Common
         public void ArchiveFile(string filePath, string archivePath)
         {
             bool archiveExists = File.Exists(archivePath);
+            string entryName = Path.GetFileName(filePath);
 
-            using (FileStream fs = File.OpenWrite(archivePath))
-            using (var zip = new ZipArchive(fs, archiveExists ? ZipArchiveMode.Update : ZipArchiveMode.Create, false))
+            using FileStream fs = File.OpenWrite(archivePath);
+            using ZipArchive zip = new(fs, archiveExists ? ZipArchiveMode.Update : ZipArchiveMode.Create, false);
+                
+            // Overwrite previous file, if any (otherwise dual records were created)
+            if (archiveExists)
             {
-                string entryName = Path.GetFileName(filePath);
                 zip.Entries.FirstOrDefault(e => e.FullName == entryName)?.Delete();
-                zip.CreateEntryFromFile(filePath, entryName);
             }
+
+            zip.CreateEntryFromFile(filePath, entryName);
         }
 
         public Task AddContentToArchive(string archivePath, string targetFilename, string content)
@@ -39,9 +43,12 @@ namespace Microsoft.Arcade.Common
 
         public async Task AddContentToArchive(string archivePath, string targetFilename, Stream content)
         {
-            using var archiveStream = new FileStream(archivePath, FileMode.Open);
-            using var archive = new ZipArchive(archiveStream, ZipArchiveMode.Update);
+            using FileStream archiveStream = new(archivePath, FileMode.Open);
+            using ZipArchive archive = new(archiveStream, ZipArchiveMode.Update);
+
+            // Overwrite previous file, if any (otherwise dual records were created)
             archive.Entries.FirstOrDefault(e => e.FullName == targetFilename)?.Delete();
+
             ZipArchiveEntry entry = archive.CreateEntry(targetFilename);
             using Stream targetStream = entry.Open();
             await content.CopyToAsync(targetStream);
