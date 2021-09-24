fe22dcedcf662873bbcf860ed06cab283b3f68a6
Adeel <3840695+am11@users.noreply.github.com>
2021-09-24 21:45:18 +0300

    Set AllowMultiple to true in SkipOnCoreClrAttribute

--- src/Microsoft.DotNet.XUnitExtensions/src/Attributes/SkipOnCoreClrAttribute.cs
+++ src/Microsoft.DotNet.XUnitExtensions/src/Attributes/SkipOnCoreClrAttribute.cs
@@ -7,7 +7,7 @@ using Xunit.Sdk;
 namespace Xunit
 {
     [TraitDiscoverer("Microsoft.DotNet.XUnitExtensions.SkipOnCoreClrDiscoverer", "Microsoft.DotNet.XUnitExtensions")]
-    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class SkipOnCoreClrAttribute : Attribute, ITraitAttribute
     {
         internal SkipOnCoreClrAttribute() { }
