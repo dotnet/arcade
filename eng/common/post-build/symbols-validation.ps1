d02116b2a402ae88d316b6079f9fdfa8bc87f1ff
Epsitha Ananth <epananth@microsoft.com>
2021-10-18 12:44:38 -0700

    updated

--- eng/common/post-build/symbols-validation.ps1
+++ eng/common/post-build/symbols-validation.ps1
@@ -5,9 +5,10 @@ param(
   [Parameter(Mandatory = $false)][switch] $CheckForWindowsPdbs, # If we should check for the existence of windows pdbs in addition to portable PDBs
   [Parameter(Mandatory = $false)][switch] $ContinueOnError, # If we should keep checking symbols after an error
   [Parameter(Mandatory = $false)][switch] $Clean,           # Clean extracted symbols directory after checking symbols
-  [Parameter(Mandatory = $false)][switch] $SymbolExclusionFile  # Exclude the symbols in the file from publishing to symbol server
+  [Parameter(Mandatory = $false)][string] $SymbolExclusionFile  # Exclude the symbols in the file from publishing to symbol server
 )
 
+. $PSScriptRoot\..\tools.ps1
 # Maximum number of jobs to run in parallel
 $MaxParallelJobs = 16
 
@@ -27,11 +28,15 @@ if ($CheckForWindowsPdbs) {
 }
 
 $ExclusionSet = New-Object System.Collections.Generic.HashSet[string];
-$FileRelativePath = $FileName.Replace("$ExtractPath\", "")
+
+if (!$InputPath -or !(Test-Path $InputPath)){
+  Write-Host "No symbols to validate."
+  ExitWithExitCode 0
+}
 
 #Check if the path exists
-if(Test-path $SymbolExclusionFile){
-  $Exclusions = Get-Content "$SymbolExclusionFile" | foreach-object {} 
+if ($SymbolExclusionFile -and (Test-Path $SymbolExclusionFile)){
+  [string[]]$Exclusions = Get-Content "$SymbolExclusionFile"
   $Exclusions | foreach { if($_ -and $_.Trim()){$ExclusionSet.Add($_)} }
 }
 else{
@@ -43,8 +48,6 @@ $CountMissingSymbols = {
     [string] $PackagePath, # Path to a NuGet package
     [string] $WindowsPdbVerificationParam # If we should check for the existence of windows pdbs in addition to portable PDBs
   )
-
-  . $using:PSScriptRoot\..\tools.ps1
 
   Add-Type -AssemblyName System.IO.Compression.FileSystem
 
@@ -155,11 +158,12 @@ $CountMissingSymbols = {
       return $null
     }
 
-    if (($($using:ExclusionSet) -ne $null) -and ($($using:ExclusionSet).Contains($FileRelativePath) -or ($($using:ExclusionSet).Contains($FileRelativePath.Replace("\", "/")))))
-    {
-      Write-Host "Skipping $SymbolPath from symbol validation"
+    $FileRelativePath = $FileName.Replace("$ExtractPath\", "")
+    if (($($using:ExclusionSet) -ne $null) -and ($($using:ExclusionSet).Contains($FileRelativePath) -or ($($using:ExclusionSet).Contains($FileRelativePath.Replace("\", "/"))))){
+      Write-Host "Skipping $FileName from symbol validation"
     }
-    else{
+
+    else {
       $FileGuid = New-Guid
       $ExpandedSymbolsPath = Join-Path -Path $SymbolsPath -ChildPath $FileGuid
 
