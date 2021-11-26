50014ff58b5f56490d5f8e188c856067163e56fc
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-11-26 15:21:37 +0100

    Don't output stdout from lld check
    This caused an issue in https://github.com/dotnet/runtime/pull/61668 because https://github.com/dotnet/runtime/blob/c00b06826ca4d333ef69d51e523ef7bd309b8631/src/libraries/System.Runtime.InteropServices/tests/TestAssets/NativeExports/NativeExports.csproj#L33 sources the init-compiler.sh and uses the stdout from it and we were outputting the stdout of the lld check which broke this.
    
    Before:
    
    ```
    # bash -c 'source "eng/common/native/init-compiler.sh" "eng/common/native/" x64 clang && echo $CC'
    LLD 10.0.1 (compatible with GNU linkers)
    /usr/bin/clang-10
    ```
    
    After:
    
    ```
    # bash -c 'source "eng/common/native/init-compiler.sh" "eng/common/native/" x64 clang && echo $CC'
    /usr/bin/clang-10
    ```

--- eng/common/native/init-compiler.sh
+++ eng/common/native/init-compiler.sh
@@ -113,7 +113,7 @@ fi
 
 # Only lld version >= 9 can be considered stable
 if [[ "$compiler" == "clang" && "$majorVersion" -ge 9 ]]; then
-    if "$CC" -fuse-ld=lld -Wl,--version 2>/dev/null; then
+    if "$CC" -fuse-ld=lld -Wl,--version >/dev/null 2>&1; then
         LDFLAGS="-fuse-ld=lld"
     fi
 fi
