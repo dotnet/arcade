2750ca44a900d516733d38388575c50ecbe1bce9
Matt Galbraith <mattgal@microsoft.com>
2021-12-27 15:48:41 -0800

    Add Helix sdk warnings for expiring queues (https://github.com/dotnet/core-eng/issues/15080).  Fix up for changes falling out from swagger regeneration

--- src/Microsoft.DotNet.Helix/Sdk/CheckHelixJobStatus.cs
+++ src/Microsoft.DotNet.Helix/Sdk/CheckHelixJobStatus.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.Helix.Sdk
 {
@@ -23,106 +20,33 @@ namespace Microsoft.DotNet.Helix.Sdk
 
         public bool FailOnMissionControlTestFailure { get; set; } = false;
 
-        protected override async Task ExecuteCore(CancellationToken cancellationToken)
+        protected override Task ExecuteCore(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            var jobNames = Jobs.Select(j => j.GetMetadata("Identity")).ToList();
 
             if (FailOnWorkItemFailure)
             {
-                string accessTokenSuffix = string.IsNullOrEmpty(this.AccessToken) ? String.Empty : "?access_token={Get this from helix.dot.net}";
-                foreach (ITaskItem failedWorkItem in FailedWorkItems)
+                string accessTokenSuffix = string.IsNullOrEmpty(AccessToken) ? "" : "?access_token={Get this from helix.dot.net}";
+                foreach (ITaskItem workItem in FailedWorkItems)
                 {
-                    var jobName = failedWorkItem.GetMetadata("JobName");
-                    var workItemName = failedWorkItem.GetMetadata("WorkItemName");
-                    var consoleUri = failedWorkItem.GetMetadata("ConsoleOutputUri");
+                    var failed = workItem.GetMetadata("Failed");
+                    if (failed == "true")
+                    {
+                        var jobName = workItem.GetMetadata("JobName");
+                        var workItemName = workItem.GetMetadata("WorkItemName");
+                        var consoleUri = workItem.GetMetadata("ConsoleOutputUri");
 
-                    Log.LogError($"Work item {failedWorkItem} in job {jobName} has failed.");
-                    Log.LogError($"Failure log: {consoleUri}{accessTokenSuffix} .");
+                        Log.LogError($"Work item {workItemName} in job {jobName} has failed.\nFailure log: {consoleUri}{accessTokenSuffix}");
+                    }
                 }
             }
 
             if (FailOnMissionControlTestFailure)
             {
-                await Task.WhenAll(jobNames.Select(n => CheckMissionControlTestFailuresAsync(n, cancellationToken)));
-            }
-        }
-
-        private async Task CheckMissionControlTestFailuresAsync(string jobName, CancellationToken cancellationToken)
-        {
-            await Task.Yield();
-            cancellationToken.ThrowIfCancellationRequested();
-
-
-            Log.LogMessage($"Checking mission control test status of job {jobName}");
-            for (; ; await Task.Delay(10000, cancellationToken)) // delay every time this loop repeats
-            {
-                if (await MissionControlTestProcessingDoneAsync(jobName, cancellationToken))
-                {
-                    break;
-                }
-
-                Log.LogMessage($"Job {jobName} is still processing xunit results.");
-                cancellationToken.ThrowIfCancellationRequested();
-            }
-        }
-
-        private async Task<bool> MissionControlTestProcessingDoneAsync(string jobName,
-            CancellationToken cancellationToken)
-        {
-            var results = await HelixApi.Aggregate.JobSummaryAsync(
-                groupBy: ImmutableList.Create("job.name"),
-                maxResultSets: 1,
-                filterName: jobName,
-                cancellationToken: cancellationToken);
-
-            if (results.Count != 1)
-            {
-                Log.LogError($"Not exactly 1 result from aggregate api for job '{jobName}': {JsonConvert.SerializeObject(results)}");
-                return true;
+                Log.LogMessage($"Mission Control is deprecated. Please set FailOnMissionControlTestFailure to false.");
             }
 
-            var data = results[0].Data;
-            if (data == null)
-            {
-                Log.LogError($"No data found in first result for job '{jobName}'.");
-                return true;
-            }
-
-            if (data.WorkItemStatus.ContainsKey("fail"))
-            {
-                Log.LogError($"Job '{jobName}' has {data.WorkItemStatus["fail"]} failed work items.");
-                return true;
-            }
-
-            if (data.WorkItemStatus.ContainsKey("none"))
-            {
-                return false;
-            }
-
-            var analysis = data.Analysis;
-            if (analysis.Any())
-            {
-                var xunitAnalysis = analysis.FirstOrDefault(a => a.Name == "xunit");
-                if (xunitAnalysis == null)
-                {
-                    Log.LogError($"Job '{jobName}' has no xunit analysis.");
-                    return true;
-                }
-
-                var pass = xunitAnalysis.Status.GetValueOrDefault("pass", 0);
-                var fail = xunitAnalysis.Status.GetValueOrDefault("fail", 0);
-                var skip = xunitAnalysis.Status.GetValueOrDefault("skip", 0);
-                var total = pass + fail + skip;
-
-                if (fail > 0)
-                {
-                    Log.LogError($"Job '{jobName}' failed {fail} out of {total} tests.");
-                }
-                return true;
-            }
-
-            return false;
+            return Task.CompletedTask;
         }
     }
 }
