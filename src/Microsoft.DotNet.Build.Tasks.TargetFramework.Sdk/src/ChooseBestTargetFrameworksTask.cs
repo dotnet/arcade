742b573497ec375cdd36232c57daae9253745399
Viktor Hofer <viktor.hofer@microsoft.com>
2022-01-19 20:11:21 +0100

    Update ChooseBestTargetFrameworksTask.cs

--- src/Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk/src/ChooseBestTargetFrameworksTask.cs
+++ src/Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk/src/ChooseBestTargetFrameworksTask.cs
@@ -4,7 +4,6 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Microsoft.DotNet.Build.Tasks.TargetFramework.Sdk
 {
