6010d2fd369964e833669858aaa7b28a6d6f05d7
Epsitha Ananth <epananth@microsoft.com>
2022-02-09 12:39:29 -0800

    review comments

---
+++ eng/common/generate-sbom-prep.ps1
@@ -0,0 +1,16 @@
+Param(
+    [Parameter(Mandatory=$true)][string] $ManifestDirPath    # Manifest directory where sbom will be placed
+)
+
+Write-Host "Creating dir $ManifestDirPath"
+#create directory for sbom manifest to be placed
+if (!(Test-Path -path $ManifestDirPath))
+{
+  New-Item -ItemType Directory -path $ManifestDirPath
+  Write-Host "Successfully created directory $ManifestDirPath"
+}
+
+Write-Host "Updating artifact name"
+$artifact_name = "$env:SYSTEM_STAGENAME$env:AGENT_JOBNAME _SBOM" -replace '["/:<>\\|?@*"() ]', '_'
+Write-Host "Artifact name $artifact_name"
+Write-Host "##vso[task.setvariable variable=ARTIFACT_NAME]$artifact_name"
