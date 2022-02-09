bd71d27e93b9ec5ef617d036ca251cf468c4aab1
Epsitha Ananth <epananth@microsoft.com>
2022-02-08 17:24:00 -0800

    Review comments

--- eng/common/generate-sbom-prep.sh
+++ eng/common/generate-sbom-prep.sh
@@ -7,14 +7,16 @@ manifest_dir=$1
 if [ ! -d "$manifest_dir" ] ; then
   mkdir -p "$manifest_dir"
   echo "Sbom directory created." $manifest_dir
+else
+  Write-PipelineTelemetryError -category 'Build'  "Unable to create sbom folder."
 fi
 
 artifact_name=$SYSTEM_STAGENAME$AGENT_JOBNAME"_SBOM"
 echo "Artifact name before : "$artifact_name
 #replace all special characters with _, some builds use special characters like : in Agent.Jobname, that is not a permissible name while uploading artifacts.
-result="${artifact_name//["/:<>\\|?@*$" ]/_}"
-echo "Artifact name after : "$result
-export ARTIFACT_NAME=$result
-echo "##vso[task.setvariable variable=ARTIFACT_NAME]$result"
+safe_artifact_name="${artifact_name//["/:<>\\|?@*$" ]/_}"
+echo "Artifact name after : "$safe_artifact_name
+export ARTIFACT_NAME=$safe_artifact_name
+echo "##vso[task.setvariable variable=ARTIFACT_NAME]$safe_artifact_name"
 
 exit 0
