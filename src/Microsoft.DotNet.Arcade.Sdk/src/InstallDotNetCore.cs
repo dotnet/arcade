29c15984f8828186466281120e4fdca0d88125df
Andrii Kurdiumov <kant2002@gmail.com>
2022-04-27 01:18:55 +0600

    Augment C# task to MSBuild This is for WinForms repository

--- src/Microsoft.DotNet.Arcade.Sdk/src/InstallDotNetCore.cs
+++ src/Microsoft.DotNet.Arcade.Sdk/src/InstallDotNetCore.cs
@@ -35,6 +35,11 @@ namespace Microsoft.DotNet.Arcade.Sdk
         
         public string RuntimeSourceFeedKey { get; set; }
 
+        /// <summary>
+        /// Optional timeout parameter in seconds.
+        /// </summary>
+        public string DownloadTimeout { get; set; }
+
         public override bool Execute()
         {
             if (!File.Exists(GlobalJsonPath))
@@ -131,6 +136,11 @@ namespace Microsoft.DotNet.Arcade.Sdk
                                         if (!string.IsNullOrWhiteSpace(RuntimeSourceFeed) && !string.IsNullOrWhiteSpace(RuntimeSourceFeedKey))
                                         {
                                             arguments += $" -runtimeSourceFeedKey {RuntimeSourceFeedKey}";
+                                        }
+
+                                        if (!string.IsNullOrWhiteSpace(DownloadTimeout))
+                                        {
+                                            arguments += $" -downloadTimeout {DownloadTimeout}";
                                         }
 
                                         Log.LogMessage(MessageImportance.Low, $"Executing: {DotNetInstallScript} {arguments}");
