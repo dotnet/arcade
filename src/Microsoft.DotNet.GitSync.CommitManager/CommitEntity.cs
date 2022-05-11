856c129fb7008fc7c7b02a0721ad92490e50845a
Alitzel Mendez <alichel_6288@hotmail.com>
2022-05-11 10:44:39 -0700

    Governance compliance 5.0

--- src/Microsoft.DotNet.GitSync.CommitManager/CommitEntity.cs
+++ src/Microsoft.DotNet.GitSync.CommitManager/CommitEntity.cs
@@ -1,6 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using Azure;
+using Azure.Data.Tables;
+
 namespace Microsoft.DotNet.GitSync.CommitManager
 {
     public class CommitEntity : ITableEntity
