21be2459b202a37fca5b7c76d756568cb3a9396c
Přemek Vysoký <premek.vysoky@microsoft.com>
2022-06-03 17:55:48 +0200

    Remove `testResults.xml` when infra retry requested from CWD (#9563)

--- src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/xharness-event-processor.py
+++ src/Microsoft.DotNet.Helix/Sdk/tools/xharness-runner/xharness-event-processor.py
@@ -355,9 +355,13 @@ if retry:
     # TODO https://github.com/dotnet/core-eng/issues/15059
     # We need to remove testResults.xml so that it is not uploaded since this run will be discarded
     # This is a workaround until we make AzDO reporter not upload test results
-    test_results = os.path.join(output_directory, "testResults.xml")
+    file_name = "testResults.xml"
+    test_results = os.path.join(output_directory, file_name)
     if os.path.exists(test_results):
         os.remove(test_results)
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
 
 if reboot:
     send_metric(REBOOT_METRIC_NAME, reboot_exit_code, reboot_dimensions, event_type=EVENT_TYPE)
