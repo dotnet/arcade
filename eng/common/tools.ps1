21b6156f47e9c2ac09d3b98767e37d78017d874d
Jon Fortescue <jofortes@microsoft.com>
2022-06-30 14:17:00 -0700

    Update with latest arcade changes

--- eng/common/tools.ps1
+++ eng/common/tools.ps1
@@ -604,7 +604,7 @@ function InitializeNativeTools() {
         InstallDirectory = "$ToolsDir"
       }
     }
-    if (Test-Path variable:NativeToolsOnMachine) {
+    if ($env:NativeToolsOnMachine) {
       Write-Host "Variable NativeToolsOnMachine detected, enabling native tool path promotion..."
       $nativeArgs += @{ PathPromotion = $true }
     }
