16e1ff602ce8b6c21ef75b1bbbe4fdea110c0135
Michael Stuckey <Michael.Stuckey@microsoft.com>
2022-07-17 22:28:22 -0700

    Make powershell more arcadey

--- eng/common/sdl/sdl.ps1
+++ eng/common/sdl/sdl.ps1
@@ -7,9 +7,21 @@ function Install-Gdn {
         [string]$Version
     )
 
+    $ErrorActionPreference = 'Stop'
+    Set-StrictMode -Version 2.0
+    $disableConfigureToolsetImport = $true
+    $global:LASTEXITCODE = 0
+
+    # `tools.ps1` checks $ci to perform some actions. Since the SDL
+    # scripts don't necessarily execute in the same agent that run the
+    # build.ps1/sh script this variable isn't automatically set.
+    $ci = $true
+    . $PSScriptRoot\..\tools.ps1
+
     if ($Version) {
         Start-Process nuget -Verbose -ArgumentList "install", "Microsoft.Guardian.Cli", "-Version $Version", "-Source https://securitytools.pkgs.visualstudio.com/_packaging/Guardian/nuget/v3/index.json", "-OutputDirectory $Path", "-NonInteractive", "-NoCache" -NoNewWindow -Wait
-    } else {
+    }
+    else {
         Start-Process nuget -Verbose -ArgumentList "install", "Microsoft.Guardian.Cli", "-Source https://securitytools.pkgs.visualstudio.com/_packaging/Guardian/nuget/v3/index.json", "-OutputDirectory $Path", "-NonInteractive", "-NoCache" -NoNewWindow -Wait
     }
 
