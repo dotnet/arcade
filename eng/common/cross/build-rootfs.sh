0a39b8f4851f8b4cd87997eb02f0895e1cd84d2a
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2022-07-28 12:12:46 +0300

    Apply suggestions from code review

--- eng/common/cross/build-rootfs.sh
+++ eng/common/cross/build-rootfs.sh
@@ -113,7 +113,7 @@ while :; do
             __LLDB_Package="liblldb-6.0-dev"
             __Keyring="--keyring /usr/share/keyrings/raspbian-archive-keyring.gpg"
 
-            if [[ ! -e "$__Keyring" ]]; then
+            if [[ ! -e “ /usr/share/keyrings/raspbian-archive-keyring.gpg" ]]; then
                 __Keyring=
             fi
             ;;
