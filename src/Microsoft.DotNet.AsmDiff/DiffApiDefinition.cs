ac6b57b804bcec47f1fd42a37210155d9cf0063b
secopsintern <125271987+secopsintern@users.noreply.github.com>
2023-02-13 17:41:44 +0530

    Update DiffApiDefinition.cs

--- src/Microsoft.DotNet.AsmDiff/DiffApiDefinition.cs
+++ src/Microsoft.DotNet.AsmDiff/DiffApiDefinition.cs
@@ -1,12 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+
 using Microsoft.Cci;
 using Microsoft.Cci.Differs;
 using Microsoft.Cci.Extensions;
+
+
+
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace Microsoft.DotNet.AsmDiff
 {
