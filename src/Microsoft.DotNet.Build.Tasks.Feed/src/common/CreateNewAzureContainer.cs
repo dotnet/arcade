26dece352e7604a1bcb28388e0acb0473e1863d5
MilenaHristova <mhristova@microsoft.com>
2023-06-26 13:36:45 +0200

    add documentation

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/common/CreateNewAzureContainer.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/common/CreateNewAzureContainer.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.DotNet.Build.CloudTestTasks;
 
 namespace Microsoft.DotNet.Build.Tasks.Feed
-{ 
+{
+    /// <summary>
+    /// Creates an Azure blob storage container with a unique name. If there is already a container named [ContainerName] will
+    /// try creating a container [ContainerName]-1, [ContainerName]-2 and so on until the name is unique.
+    /// The final name is saved in ContainerName.
+    /// </summary>
     public class CreateNewAzureContainer : CreateAzureContainer
     {
         public override async Task<AzureStorageUtils> GetBlobStorageUtilsAsync(string accountName, string accountKey, string containerName)
