bdbba2eef1b23da313d6b03632efa4ca9b6a39b9
Jacques Eloff <joeloff@users.noreply.github.com>
2023-07-20 09:22:18 -0700

    Replace MD5 with SHA2 (#13907)
    * Replace MD5 with SHA2
    
    * PR feedback

--- src/Microsoft.DotNet.Build.Tasks.Workloads/src/Utils.cs
+++ src/Microsoft.DotNet.Build.Tasks.Workloads/src/Utils.cs
@@ -14,12 +14,12 @@ namespace Microsoft.DotNet.Build.Tasks.Workloads
     public class Utils
     {
         /// <summary>
-        /// Generate a hash for a string value using a given hash algorithm.
+        /// Generate a truncated hash (first 16 bytes) for a string value using a given hash algorithm.
         /// </summary>
         /// <param name="value">The value to hash.</param>
         /// <param name="hashName">The name of the <see cref="HashAlgorithm"/> to use.</param>
         /// <returns>A string containing the hash result.</returns>
-        public static string GetHash(string value, HashAlgorithmName hashName)
+        public static string GetTruncatedHash(string value, HashAlgorithmName hashName)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(value);
             HashAlgorithm algorithm = HashAlgorithm.Create(hashName.Name);
@@ -30,7 +30,9 @@ namespace Microsoft.DotNet.Build.Tasks.Workloads
                 sb.Append(b.ToString("x2"));
             }
 
-            return sb.ToString();
+            string result = sb.ToString();
+
+            return result.Substring(0, 32);
         }
 
         /// <summary>
