4cfded97a32d92ee582a263fc1ecdf6dcb0eeb79
Matt Mitchell <mmitche@microsoft.com>
2023-08-15 08:51:24 -0700

    Update preview flow scripts with better names and help docs (#13970)

--- scripts/flip-preview-flow.ps1
+++ scripts/flip-preview-flow.ps1
@@ -1,10 +1,34 @@
+<#
+.SYNOPSIS
+Flips the flow of a preview/rc between public flow and internal flow. Internal subscriptions and default channel must exist (see create-preview-flow.ps1)
+
+.PARAMETER RuntimeChannel
+Public runtime channel 
+
+.PARAMETER SdkChannel
+Public sdk channel
+
+.PARAMETER InternalRuntimeChannel
+Internal runtime channel
+
+.PARAMETER InternalSdkChannel
+Internal sdk channel
+
+.PARAMETER FlowType
+What type of flow to use.
+
+.PARAMETER TriggerSubscriptions
+If true, triggers subscriptions after the switch
+
+#>
+
 param
 (
     [Parameter(Mandatory=$true)][string]$RuntimeChannel,
     [Parameter(Mandatory=$true)][string]$SdkChannel,
     [Parameter(Mandatory=$true)][string]$InternalRuntimeChannel,
     [Parameter(Mandatory=$true)][string]$InternalSdkChannel,
-    [Parameter(Mandatory=$true)][ValidateSet('Preview','Preview+')][string]$FlowType,
+    [Parameter(Mandatory=$true)][ValidateSet('Public','Internal')][string]$FlowType,
     [switch]$TriggerSubscriptions
 )
 
@@ -12,7 +36,7 @@ Write-Host "Switching flow to $FlowType"
 
 switch ( $FlowType )
 {
-    'Preview' {
+    'Public' {
         # Switching to public flow is easy. Enable all the subscriptions in the public runtime and SDK channels
         # and disable any on the internal channels
         & darc subscription-status --disable --channel "$InternalRuntimeChannel" --quiet --exact
@@ -26,7 +50,7 @@ switch ( $FlowType )
             & darc trigger-subscriptions --channel "$SdkChannel" --quiet --exact
         }
     }
-    'Preview+' {
+    'Internal' {
         # Switching to internal flow is a little more subtle. Enable the internal subscriptions,
         # then disable the corresponding public subscriptions, which is a subset of the full public channel
         & darc subscription-status --enable --channel "$InternalRuntimeChannel" --quiet --exact
