88748bb12d18cb8e6117f104ab368f0a7948fe3a
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2023-08-25 16:30:43 -0700

    [release/6.0] Bump a few package versions - small part of https://github.com/dotnet/dnceng/issues/345 - bump Azure.Data.Tables to v12.8.1 - replace Microsoft.IdentityModel.Clients.ActiveDirectory with Microsoft.Identity.Client   - part of https://github.com/dotnet/dnceng/issues/629 - bump NuGet.* to v6.7.0   - part of https://dev.azure.com/dnceng/internal/_workitems/edit/5128   - part of https://dev.azure.com/dnceng/internal/_workitems/edit/5129   - left both `$(NuGetVersioningVersion)` and `$(NuGetVersion)` to minimize disruption     - see #12333 change in 'main' for a counter-example   - react to deprecation of `ContentItemCollection.FindItemGroups(...)`   - react to removal of `SignatureVerificationProviderFactory`   - react to `VerifySignaturesResult` API changes (`Valid` to `IsValid`) - bump NewtonSoft.Json to v13.0.3 - nits:   - remove unused `$(...Version)` properties   - remove a bit of dead code and unused `using`s in changed files

--- src/SignCheck/Microsoft.SignCheck/Verification/NupkgVerifier.cs
+++ src/SignCheck/Microsoft.SignCheck/Verification/NupkgVerifier.cs
@@ -1,12 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SignCheck.Logging;
-using NuGet.Common;
 using NuGet.Packaging;
 using NuGet.Packaging.Signing;
 
@@ -14,12 +12,12 @@ namespace Microsoft.SignCheck.Verification
 {
     public class NupkgVerifier : ArchiveVerifier
     {
-        public NupkgVerifier(Log log, Exclusions exclusions, SignatureVerificationOptions options) : base(log, exclusions, options, fileExtension: ".nupkg")
+        public NupkgVerifier(Log log, Exclusions exclusions, SignatureVerificationOptions options) :
+            base(log, exclusions, options, fileExtension: ".nupkg")
         {
-
         }
 
-        public override SignatureVerificationResult VerifySignature(string path, string parent, string virtualPath) 
+        public override SignatureVerificationResult VerifySignature(string path, string parent, string virtualPath)
         {
             SignatureVerificationResult svr = new SignatureVerificationResult(path, parent, virtualPath);
             string fullPath = svr.FullPath;
@@ -31,20 +29,25 @@ namespace Microsoft.SignCheck.Verification
             return svr;
         }
 
+        // This method and SignatureVerificationResult.IsSigned are slightly misnamed. Signature validity is just as
+        // important as signature existence. The new VerifySignatureResult.IsSigned property would _not_ be correct
+        // to use here.
         private bool IsSigned(string path)
         {
-            IEnumerable<ISignatureVerificationProvider> providers = SignatureVerificationProviderFactory.GetSignatureVerificationProviders();
+            IEnumerable<ISignatureVerificationProvider> providers = new ISignatureVerificationProvider[] {
+                new IntegrityVerificationProvider(),
+                new SignatureTrustAndValidityVerificationProvider(allowUntrustedRootList: null),
+                new AllowListVerificationProvider(allowList: null),
+            };
             var packageSignatureVerifier = new PackageSignatureVerifier(providers);
 
             var verifierSettings = SignedPackageVerifierSettings.GetVerifyCommandDefaultPolicy();
-            IEnumerable<ISignatureVerificationProvider> verificationProviders = SignatureVerificationProviderFactory.GetSignatureVerificationProviders();
-            var verifier = new PackageSignatureVerifier(verificationProviders);
 
             using (var pr = new PackageArchiveReader(path))
             {
                 Task<VerifySignaturesResult> verifySignatureResult = packageSignatureVerifier.VerifySignaturesAsync(pr, verifierSettings, CancellationToken.None);
 
-                return verifySignatureResult.Result.Valid;
+                return verifySignatureResult.Result.IsValid;
             }
         }
     }
