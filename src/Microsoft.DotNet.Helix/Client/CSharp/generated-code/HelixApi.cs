0326de29b29476d52e23df311a6c61f50d24e603
Viktor Hofer <viktor.hofer@microsoft.com>
2023-12-28 15:56:19 +0000

    Update Arcade TFMs according to TFM docs
    - Use "Tool" floating TFM properties for msbuild tasks and tools,
      i.e. Microsoft.DotNet.Arcade.Sdk.
    - Use "Minimum" floating TFM properties for libraries that support
      previous framework versions, i.e. XUnitExtensions.
    - Use "Current" floating TFM properties for libraries that support
      only the current framework version.
    
    Remove "NetToolMinimum" which isn't used and not necessary as our
    tools/tasks don't multi-target.

--- src/Microsoft.DotNet.Helix/Client/CSharp/generated-code/HelixApi.cs
+++ src/Microsoft.DotNet.Helix/Client/CSharp/generated-code/HelixApi.cs
@@ -333,6 +333,9 @@ namespace Microsoft.DotNet.Helix.Client
             Response = new ResponseWrapper(response, responseContent);
         }
 
+#if NET
+        [Obsolete]
+#endif
         protected RestApiException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
@@ -342,6 +345,9 @@ namespace Microsoft.DotNet.Helix.Client
             Response = JsonConvert.DeserializeObject<ResponseWrapper>(responseString, SerializerSettings);
         }
 
+#if NET
+        [Obsolete]
+#endif
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             if (info == null)
@@ -369,12 +375,18 @@ namespace Microsoft.DotNet.Helix.Client
             Body = body;
         }
 
+#if NET
+        [Obsolete]
+#endif
         protected RestApiException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
             Body = JsonConvert.DeserializeObject<T>(info.GetString("Body"));
         }
 
+#if NET
+        [Obsolete]
+#endif
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             if (info == null)
