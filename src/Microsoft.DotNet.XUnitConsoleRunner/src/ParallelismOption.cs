f26f437bc1993e1d1ec13de7d95f836b511333c6
Viktor Hofer <viktor.hofer@microsoft.com>
2024-01-02 12:48:19 +0100

    Enable license header validation for C# files (#14330)

--- src/Microsoft.DotNet.XUnitConsoleRunner/src/ParallelismOption.cs
+++ src/Microsoft.DotNet.XUnitConsoleRunner/src/ParallelismOption.cs
@@ -1,4 +1,7 @@
-﻿namespace Xunit.ConsoleClient
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Xunit.ConsoleClient
 {
     public enum ParallelismOption
     {
