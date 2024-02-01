618c372abb8f173f028a8a82a74b99f44ce5aee1
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2024-02-01 15:48:31 -0800

    Diffing lines

---
+++ src/PackageDiff/PackageDiff.Tasks/DiffPackages.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Build.Framework;
+
+public class PackageDiffTask : Microsoft.Build.Utilities.ToolTask
+{
+    [Required]
+    public string BaselinePackage {get; set;} = "";
+
+    [Required]
+    public string TestPackage {get; set;} = "";
+
+    protected override string ToolName { get; } = $"PackageDiff" + (System.Environment.OSVersion.Platform == PlatformID.Unix ? "" : ".exe");
+
+    protected override string GenerateFullPathToTool()
+    {
+        return Path.Combine(typeof(PackageDiffTask).Assembly.Location, ToolName);
+    }
+
+    protected override string GenerateCommandLineCommands()
+    {
+        return $"\"{BaselinePackage}\" \"{TestPackage}\"";
+    }
+}
