23669004b6f150c0382c1ba3819575ade209fccb
Matt Mitchell (.NET) <mmitche@microsoft.com>
2024-06-05 16:22:50 -0700

    Enable use of the default azure credential for storage publishing When a SAS is not supplied for a given storage endpoint, the default azure credential is used.

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/AzureStorageContainerAssetTokenCredentialPublisher.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/AzureStorageContainerAssetTokenCredentialPublisher.cs
@@ -2,27 +2,27 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using Azure.Core;
 using Azure.Storage.Blobs;
-using Azure;
 using Microsoft.Build.Utilities;
 
 namespace Microsoft.DotNet.Build.Tasks.Feed
 {
-    public class AzureStorageContainerAssetPublisher : AzureStorageAssetPublisher
+    public class AzureStorageContainerAssetTokenCredentialPublisher : AzureStorageAssetPublisher
     {
         private readonly Uri _containerUri;
-        private readonly Uri _sasUri;
+        private readonly TokenCredential _tokenCredential;
 
-        public AzureStorageContainerAssetPublisher(Uri containerUri, Uri sasUri, TaskLoggingHelper log) : base(log)
+        public AzureStorageContainerAssetTokenCredentialPublisher(Uri containerUri, TokenCredential tokenCredential, TaskLoggingHelper log) : base(log)
         {
             _containerUri = containerUri;
-            _sasUri = sasUri;
+            _tokenCredential = tokenCredential;
         }
 
         public override BlobClient CreateBlobClient(string blobPath)
         {
             // When creating the blob client from the URI, only utilize the query parameters for the SAS uri (excluding the leading ?)
-            var containerClient = new BlobContainerClient(_containerUri, new AzureSasCredential(_sasUri.Query));
+            var containerClient = new BlobContainerClient(_containerUri, _tokenCredential);
             return containerClient.GetBlobClient(blobPath);
         }
     }
