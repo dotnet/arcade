0f83e751fd5b3ce4e72b12a7e7d42e21a5d0feff
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-11-06 09:22:11 -0800

    Check for aarch64 instead of arm64 in CreateRpmPackage

--- src/Microsoft.DotNet.Build.Tasks.Installers/src/CreateRpmPackage.cs
+++ src/Microsoft.DotNet.Build.Tasks.Installers/src/CreateRpmPackage.cs
@@ -69,7 +69,7 @@ namespace Microsoft.DotNet.Build.Tasks.Installers.src
                 "x86" => Architecture.X86,
                 "x64" => Architecture.X64,
                 "arm" => Architecture.Arm,
-                "arm64" => Architecture.Arm64,
+                "aarch64" => Architecture.Arm64,
 #if NET
                 "armv6" => Architecture.Armv6,
                 "s390x" => Architecture.S390x,
