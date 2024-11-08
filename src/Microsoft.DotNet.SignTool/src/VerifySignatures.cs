24c7de55ddd62836a87612771ec615535245b3a9
Ella Hathaway <ellahathaway@microsoft.com>
2024-11-08 18:58:42 +0000

    Nit

--- src/Microsoft.DotNet.SignTool/src/VerifySignatures.cs
+++ src/Microsoft.DotNet.SignTool/src/VerifySignatures.cs
@@ -17,7 +17,6 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
-using System.Runtime.Remoting;
 
 namespace Microsoft.DotNet.SignTool
 {
