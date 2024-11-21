d60063859ee8d98e9f7225d0c95dd90b7a3d925e
Ella Hathaway <ellahathaway@microsoft.com>
2024-11-21 22:20:12 +0000

    Make Utilities.cs internal and methods public

--- src/Microsoft.DotNet.MacOsPkg/Utilities.cs
+++ src/Microsoft.DotNet.MacOsPkg/Utilities.cs
@@ -6,21 +6,21 @@ using System.Linq;
 
 namespace Microsoft.DotNet.MacOsPkg
 {
-    public static class Utilities
+    internal static class Utilities
     {
-        internal static bool IsPkg(string path) =>
+        public static bool IsPkg(string path) =>
             Path.GetExtension(path).Equals(".pkg");
 
-        internal static bool IsAppBundle(string path) =>
+        public static bool IsAppBundle(string path) =>
             Path.GetExtension(path).Equals(".app");
 
-        internal static string? FindInPath(string name, string path, bool isDirectory, SearchOption searchOption = SearchOption.AllDirectories)
+        public static string? FindInPath(string name, string path, bool isDirectory, SearchOption searchOption = SearchOption.AllDirectories)
         {
             string[] results = isDirectory ? Directory.GetDirectories(path, name, searchOption) : Directory.GetFiles(path, name, searchOption);
             return results.FirstOrDefault();
         }
 
-        internal static void CleanupPath(string path)
+        public static void CleanupPath(string path)
         {
             if (Directory.Exists(path))
             {
@@ -32,7 +32,7 @@ namespace Microsoft.DotNet.MacOsPkg
             }
         }
 
-        internal static void CreateParentDirectory(string path)
+        public static void CreateParentDirectory(string path)
         {
             string? parent = Path.GetDirectoryName(path);
             if (!string.IsNullOrEmpty(parent))
