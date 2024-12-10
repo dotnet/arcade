d5c8bb8726b46b5867961f5d8f56f1b13e72dcb9
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-12-10 11:08:32 +0100

    Fix typo in certificate name in SignToolTests.cs (#15322)

--- src/Microsoft.DotNet.SignTool.Tests/SignToolTests.cs
+++ src/Microsoft.DotNet.SignTool.Tests/SignToolTests.cs
@@ -38,7 +38,7 @@ namespace Microsoft.DotNet.SignTool.Tests
             {".vsix",  new List<SignInfo>{ new SignInfo("VsixSHA2") } },
             {".zip",  new List<SignInfo>{ SignInfo.Ignore } },
             {".tgz",  new List<SignInfo>{ SignInfo.Ignore } },
-            {".py",  new List<SignInfo>{ new SignInfo("Microsof400") } },
+            {".py",  new List<SignInfo>{ new SignInfo("Microsoft400") } }, // lgtm [cs/common-default-passwords] Safe, these are certificate names
             {".nupkg",  new List<SignInfo>{ new SignInfo("NuGet") } },
             {".symbols.nupkg",  new List<SignInfo>{ SignInfo.Ignore } },
         };
