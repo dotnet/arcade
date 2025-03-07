8b6565cd3b39f954218ac0883a83f5bff90dad40
Matt Mitchell <mmitche@microsoft.com>
2025-03-07 11:09:56 -0800

    Support pipeline artifacts when using streaming publishing (#15595)

--- src/Common/Microsoft.Arcade.Test.Common/FakeHttpClient.cs
+++ src/Common/Microsoft.Arcade.Test.Common/FakeHttpClient.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,13 +15,17 @@ namespace Microsoft.DotNet.Arcade.Test.Common
         
         public static HttpClient WithResponses(params HttpResponseMessage[] responses)
             => new HttpClient( // lgtm [cs/httpclient-checkcertrevlist-disabled] HttpClient used in unit tests
-                new FakeHttpMessageHandler(responses));
+                new FakeResponseOnlyHttpMessageHandler(responses));
 
-        private class FakeHttpMessageHandler : HttpMessageHandler
+        public static HttpClient WithResponsesGivenUris(Dictionary<string, IEnumerable<HttpResponseMessage>> FakeHttpMessageHandler)
+            => new HttpClient( // lgtm [cs/httpclient-checkcertrevlist-disabled] HttpClient used in unit tests
+                new FakeHttpMessageHandler(FakeHttpMessageHandler));
+
+        public class FakeResponseOnlyHttpMessageHandler : HttpMessageHandler
         {
             private readonly IEnumerator<HttpResponseMessage> _responseEnumerator;
 
-            public FakeHttpMessageHandler(IEnumerable<HttpResponseMessage> responses)
+            public FakeResponseOnlyHttpMessageHandler(IEnumerable<HttpResponseMessage> responses)
             {
                 _responseEnumerator = responses.GetEnumerator();
             }
@@ -39,6 +44,43 @@ namespace Microsoft.DotNet.Arcade.Test.Common
                     throw new InvalidOperationException($"Unexpected end of response sequence. Number of predefined responses should be at least equal to number of requests invoked by HttpClient.");
 
                 return Task.FromResult(_responseEnumerator.Current);
+            }
+        }
+
+        public class FakeHttpMessageHandler : HttpMessageHandler
+        {
+            private readonly Dictionary<string, IEnumerator<HttpResponseMessage>> _responseEnumerators;
+
+            public FakeHttpMessageHandler(Dictionary<string, IEnumerable<HttpResponseMessage>> responses)
+            {
+                _responseEnumerators = responses.Select(kvp =>
+                    new KeyValuePair<string, IEnumerator<HttpResponseMessage>>(kvp.Key, kvp.Value.GetEnumerator()))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    foreach (var e in _responseEnumerators.Values)
+                    {
+                        e.Dispose();
+                    }
+                }
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if (!_responseEnumerators.TryGetValue(request.RequestUri.ToString(), out var responseEnumerator))
+                {
+                    if (request.RequestUri != null)
+                        throw new InvalidOperationException($"Unexpected request URI: {request.RequestUri}.");
+                }
+                if (!responseEnumerator.MoveNext())
+                    throw new InvalidOperationException($"Unexpected end of response sequence for uri '{request.RequestUri}'. " +
+                        $"Number of predefined responses should be at least equal to number of requests invoked by HttpClient.");
+
+                return Task.FromResult(responseEnumerator.Current);
             }
         }
     }
