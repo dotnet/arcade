7a1e9892c0d2a3121fabcb70fdc220d983f9d295
Jacques Eloff <joeloff@users.noreply.github.com>
2025-08-13 08:50:01 -0700

    Fix using statements

--- src/Microsoft.DotNet.Build.Tasks.Workloads/src/Msi/MsiUtils.wix.cs
+++ src/Microsoft.DotNet.Build.Tasks.Workloads/src/Msi/MsiUtils.wix.cs
@@ -4,8 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Deployment.WindowsInstaller;
-using Microsoft.Deployment.WindowsInstaller.Package;
+using WixToolset.Dtf.WindowsInstaller;
+using WixToolset.Dtf.WindowsInstaller.Package;
 
 namespace Microsoft.DotNet.Build.Tasks.Workloads.Msi
 {
