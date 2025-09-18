583effe8471a0e08a6422d0d2799cb99e9a3d38f
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-09-18 11:03:39 +0200

    [release/8.0] Add 11.0 feeds (#16149)

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/model/PublishingConstants.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/model/PublishingConstants.cs
@@ -113,6 +113,10 @@ namespace Microsoft.DotNet.Build.Tasks.Feed.Model
         private const string FeedDotNet10InternalShipping = "https://pkgs.dev.azure.com/dnceng/internal/_packaging/dotnet10-internal/nuget/v3/index.json";
         private const string FeedDotNet10InternalTransport = "https://pkgs.dev.azure.com/dnceng/internal/_packaging/dotnet10-internal-transport/nuget/v3/index.json";
 
+        private const string FeedDotNet11Shipping = "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet11/nuget/v3/index.json";
+        private const string FeedDotNet11Transport = "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet11-transport/nuget/v3/index.json";
+        private const string FeedDotNet11Workloads = "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet11-workloads/nuget/v3/index.json";
+
         private const string FeedDotNetLibrariesShipping = "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-libraries/nuget/v3/index.json";
         private const string FeedDotNetLibrariesTransport = "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-libraries-transport/nuget/v3/index.json";
 
@@ -228,6 +232,30 @@ namespace Microsoft.DotNet.Build.Tasks.Feed.Model
         {
             (TargetFeedContentType.Package, FeedDotNet10Workloads, AssetSelection.ShippingOnly),
             (TargetFeedContentType.Package, FeedDotNet10Workloads, AssetSelection.NonShippingOnly),
+            (InstallersAndSymbols, FeedStagingForInstallers),
+            (TargetFeedContentType.Checksum, FeedStagingForChecksums),
+        };
+        
+        private static TargetFeedSpecification[] DotNet11Feeds =
+        {
+            (TargetFeedContentType.Package, FeedDotNet11Shipping, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNet11Transport, AssetSelection.NonShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetEng, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetEng, AssetSelection.NonShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNet11Shipping, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNet11Transport, AssetSelection.NonShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetLibrariesShipping, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetLibrariesTransport, AssetSelection.NonShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetTools, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNetTools, AssetSelection.NonShippingOnly),
+            (InstallersAndSymbols, FeedStagingForInstallers),
+            (TargetFeedContentType.Checksum, FeedStagingForChecksums),
+        };
+        
+        private static TargetFeedSpecification[] DotNet11WorkloadFeeds =
+        {
+            (TargetFeedContentType.Package, FeedDotNet11Workloads, AssetSelection.ShippingOnly),
+            (TargetFeedContentType.Package, FeedDotNet11Workloads, AssetSelection.NonShippingOnly),
             (InstallersAndSymbols, FeedStagingForInstallers),
             (TargetFeedContentType.Checksum, FeedStagingForChecksums),
         };
@@ -988,6 +1016,33 @@ namespace Microsoft.DotNet.Build.Tasks.Feed.Model
                 targetFeeds: DotNet10Feeds,
                 symbolTargetType: PublicAndInternalSymbolTargets,
                 filenamesToExclude: FilenamesToExclude),
+
+            // .NET 11,
+            new TargetChannelConfig(
+                id: 8297,
+                isInternal: false,
+                publishingInfraVersion: PublishingInfraVersion.Latest,
+                akaMSChannelNames: ["11.0"],
+                targetFeeds: DotNet11Feeds,
+                symbolTargetType: PublicAndInternalSymbolTargets),
+
+            // .NET 11 Workload Release,
+            new TargetChannelConfig(
+                id: 8299,
+                isInternal: false,
+                publishingInfraVersion: PublishingInfraVersion.Latest,
+                akaMSChannelNames: [ "11.0-workloads" ],
+                targetFeeds: DotNet11WorkloadFeeds,
+                symbolTargetType: PublicAndInternalSymbolTargets),
+
+            // .NET 11.0.1xx SDK,
+            new TargetChannelConfig(
+                id: 8298,
+                isInternal: false,
+                publishingInfraVersion: PublishingInfraVersion.Latest,
+                akaMSChannelNames: [ "11.0.1xx", "11.0" ],
+                targetFeeds: DotNet11Feeds,
+                symbolTargetType: PublicAndInternalSymbolTargets),
 
             // .NET Core Experimental,
             new TargetChannelConfig(
