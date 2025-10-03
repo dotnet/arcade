b7f59f708c24a7217c3bc23f2388736a28f981d7
Jan Kotas <jkotas@microsoft.com>
2025-10-03 15:51:56 -0700

    Enable ConditonalFact and ConditionalTheory for XUnit v3
    dotnet/runtime has many thousands of tests that use these. We do not want to be changing all of them to alternatives.

--- src/Microsoft.DotNet.XUnitExtensions.Shared/Attributes/ConditionalFactAttribute.cs
+++ src/Microsoft.DotNet.XUnitExtensions.Shared/Attributes/ConditionalFactAttribute.cs
@@ -1,14 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-
-// Not adding the support for xunit.v3.
-// Still keeping the logic inside compatible with xunit.v3 in case we decided to add it.
-// For cases that used to do [ConditionalFact] in xunit.v2, they can now call Assert.Skip instead of throwing SkipTestException
-// In this case, [Fact] will just work because Assert.Skip is natively supported in xunit.v3
-// TODO: Evaluate whether or not we want to still expose this attribute in xunit.v3 for usages of CalleeType and ConditionMemberNames?
-#if !USES_XUNIT_3
-
 using System;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.DotNet.XUnitExtensions;
@@ -43,4 +35,3 @@ namespace Xunit
         }
     }
 }
-#endif
