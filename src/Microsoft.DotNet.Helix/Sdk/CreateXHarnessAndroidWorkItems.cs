b8d45b8a6b4ab33eea8287f35a9c6f9e464c02de
Milos Kotlar <kotlarmilos@gmail.com>
2025-10-14 14:00:46 +0200

    Set DOTNET_CI environment variable for Android and Apple work item commands

--- src/Microsoft.DotNet.Helix/Sdk/CreateXHarnessAndroidWorkItems.cs
+++ src/Microsoft.DotNet.Helix/Sdk/CreateXHarnessAndroidWorkItems.cs
@@ -157,7 +157,9 @@ namespace Microsoft.DotNet.Helix.Sdk
                 "--timeout \"$timeout\" " +
                 "--package-name \"$package_name\" " +
                 " -v " +
-                $"{ devOutArg } { instrumentationArg } { exitCodeArg } { extraArguments } { passthroughArgs }";
+                $"{ devOutArg }  { instrumentationArg } { exitCodeArg } { extraArguments } " +
+                "-e DOTNET_CI true " +
+                $"{ passthroughArgs }";
         }
 
         private string GetHelixCommand(
