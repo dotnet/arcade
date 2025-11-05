790219e6cf87f7ce6b6d822f91038d6ba097fa56
Premek Vysoky <premek.vysoky@microsoft.com>
2025-11-05 10:30:46 +0100

    [release/9.0] Update vulnerable MSBuild packages

--- src/Microsoft.DotNet.SignTool.Tests/FakeSignTool.cs
+++ src/Microsoft.DotNet.SignTool.Tests/FakeSignTool.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Packaging;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
