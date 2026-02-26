f4c335a7e3e9bb8fa600b423b64c187f8e52a3be
Noah Gilson <noahgilson@microsoft.com>
2026-02-26 12:22:18 -0800

    Improve Helix Error Reporting
    # Motivators
    When helix fails, there are several core problems that make the build analysis tab no longer helpful.
    
    1. Failures do not appear, and instead this error is displayed from `dotnet-helix-service`: This is a helix work item crash with status: `BadExit`.
    
    2. Work Item failures simply say `Test has failed. Check the log: log_link` when they could actually provide the error, so we don't have to click and read the log.
    
    3. When I click the link on '[Console]' it takes me here: https://dev.azure.com/dnceng-public/public/_build/results?buildId=1310620&view=ms.vss-test-web.build-test-results-tab&runId=36607218&resultId=113379&paneView=debug
    
    This view is not helpful - here is what it shows me.
    "The Helix Work Item failed. Often this is due to a test crash. Please see the 'Artifacts' tab above for additional logs." The failures for a "Work Item" are not helpful and provide nothing actionable. What I really want to see is a specific test failure link for that build, https://dev.azure.com/dnceng-public/public/_build/results?buildId=1310620&view=ms.vss-test-web.build-test-results-tab&runId=36607090&resultId=100720&paneView=debug such as this link, which shows ''Expected string to be <null> or empty because Expected command to not output to stderr but it was not:'
    
    # Fixes
    
    Adds more error details from the log into the output messages.
    Note, I have not worked in the Arcade repo so I'm not familiar with this logic. It may also be controversial to create a longer string in  the output by showing more error details.
    
    My goal is to speed up diagnostic time and reduce the number of clicks it takes to see what failed in a PR.

--- src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/test_results_reader/__init__.py
+++ src/Microsoft.DotNet.Helix/Sdk/tools/azure-pipelines/reporter/test_results_reader/__init__.py
@@ -13,8 +13,65 @@ def __no_results_result():
     work_item_name = get_env("HELIX_WORKITEM_FRIENDLYNAME")
     
     if exitCode != "0":
-        # if we have a catastrophic failure, we want to create the fake test result with attached dump files and logs (if available)
-        return
+        # Work item crashed / exited non-zero without producing result files.
+        # Try to capture useful error information from available sources.
+        failure_message = "Work item exited with code {}. No test result files were produced.".format(exitCode)
+        attachments = []
+
+        # Try to capture console output from the test process
+        # The stdout/stderr of the test command is available in the work item's console log
+        # but we can also check for any error output files
+        upload_root = get_env("HELIX_WORKITEM_UPLOAD_ROOT")
+        workitem_root = get_env("HELIX_WORKITEM_ROOT")
+
+        # Look for blame sequence files that indicate which test was running when crash occurred
+        error_details = []
+        for search_dir in [os.getcwd(), upload_root, workitem_root]:
+            if not search_dir or not os.path.isdir(search_dir):
+                continue
+            for root, dirs, files in os.walk(search_dir):
+                for f in files:
+                    fpath = os.path.join(root, f)
+                    try:
+                        # Blame sequence file tells us which test was running
+                        if f == "Sequence.xml" or f.endswith("_Sequence.xml"):
+                            with open(fpath, 'r', errors='replace') as seq_file:
+                                content = seq_file.read()
+                                error_details.append("Blame sequence (last test running at crash):\n" + content[-2000:])
+                                attachments.append(TestResultAttachment(
+                                    name=u"Sequence.xml",
+                                    text=content,
+                                ))
+                        # Capture any .log files that might have error details
+                        elif f.endswith("TestLog.log") or f == "dotnetTestLog.log":
+                            with open(fpath, 'r', errors='replace') as log_file:
+                                content = log_file.read()
+                                # Get last 3000 chars which likely contain the error
+                                tail = content[-3000:] if len(content) > 3000 else content
+                                error_details.append("Test log tail:\n" + tail)
+                                attachments.append(TestResultAttachment(
+                                    name=f,
+                                    text=content,
+                                ))
+                    except Exception as e:
+                        log.warning("Failed to read {}: {}".format(fpath, e))
+
+        if error_details:
+            failure_message += "\n\n" + "\n\n".join(error_details)
+
+        return TestResult(
+            name=u'{}.WorkItemExecution'.format(work_item_name),
+            kind=u'unknown',
+            type_name=u'{}'.format(work_item_name),
+            method=u'WorkItemExecution',
+            duration=1,
+            result=u'Fail',
+            exception_type=None,
+            failure_message=failure_message,
+            stack_trace=None,
+            skip_reason=None,
+            attachments=attachments,
+        )
     else:
         result = 'Pass'
         failure_message = None
@@ -53,4 +110,6 @@ def read_results(dirs_to_check: List[str]) -> Iterable[TestResult]:
 
     if not found:
         log.warn('No results file found in any of the following formats: {}'.format(', '.join((f.name for f in all_formats))))
-        yield __no_results_result()
+        result = __no_results_result()
+        if result is not None:
+            yield result
