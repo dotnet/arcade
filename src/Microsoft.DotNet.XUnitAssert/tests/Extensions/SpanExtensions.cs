becd3c825f3dfa77cf1428a755500bf1733eefa9
Andy Gocke <andy@commentout.net>
2026-04-21 03:55:58 +0000

    Remove Microsoft.DotNet.XUnitAssert
    The Microsoft.DotNet.XUnitAssert package was an xunit fork designed for
    AOT compatibility. Now that XUnit publishes an 'official' package designed
    for AOT compatibility, that's no longer necessary. We can just use the official
    package.

--- src/Microsoft.DotNet.XUnitAssert/tests/Extensions/SpanExtensions.cs
+++
@@ -1,8 +0,0 @@
-internal static class SpanExtensions
-{
-	public static Span<T> Spanify<T>(this T[]? values) =>
-		new(values);
-
-	public static Span<char> Spanify(this string? value) =>
-		new((value ?? string.Empty).ToCharArray());
-}
--- src/Microsoft.DotNet.XUnitAssert/tests/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-global using System.Diagnostics.CodeAnalysis;
-global using Xunit.Internal;
