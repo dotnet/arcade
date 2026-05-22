55c2f2a59eeb2d8d5f7ddccb6d398f4c012bf0f8
Premek Vysoky <premek.vysoky@microsoft.com>
2026-05-22 14:41:46 +0200

    Fix when a work item is deemed failed

--- src/Microsoft.DotNet.Helix/JobMonitor/JobMonitorRunner.cs
+++ src/Microsoft.DotNet.Helix/JobMonitor/JobMonitorRunner.cs
@@ -1008,6 +1008,27 @@ static file class WorkItemExtensions
 {
     extension(WorkItemSummary workItem)
     {
-        public bool IsFailed => workItem.ExitCode != null && workItem.ExitCode != 0;
+        public bool IsFailed
+        {
+            get
+            {
+                if (workItem.ExitCode == null)
+                {
+                    return false;
+                }
+
+                if (workItem.ExitCode != 0)
+                {
+                    return true;
+                }
+
+                if (!workItem.State.Equals("Finished", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
     }
 }
